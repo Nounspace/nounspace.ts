66f9fa6ce28cf1e440bc998d1007db000025029f
Julian Early <jearly@berkeley.edu>
2024-05-01 13:26:49 -0700

    refactor: port the rest of pages/api to app

---
+++ src/app/api/auth/confirm/route.ts
@@ -0,0 +1,33 @@
+import { type EmailOtpType } from '@supabase/supabase-js'
+
+import createClient from '@/common/data/api/supabase/api'
+import { NextRequest, NextResponse } from 'next/server';
+import { get } from 'lodash';
+
+function stringOrFirstString(item: string | string[] | undefined) {
+  return Array.isArray(item) ? item[0] : item
+}
+
+export default async function POST(req: NextRequest) {
+  const queryParams = req.nextUrl.searchParams;
+  const token_hash = stringOrFirstString(get(queryParams, "token_hash", ""));
+  const type = stringOrFirstString(get(queryParams, "type", ""));
+
+  const resp = NextResponse.redirect("/error"); 
+
+  if (token_hash && type) {
+    const supabase = createClient(req, resp)
+    const { error } = await supabase.auth.verifyOtp({
+      type: type as EmailOtpType,
+      token_hash,
+    })
+    if (error) {
+      console.error(error)
+    } else {
+      // When no error, change redirect to the "next" query param or to root
+      resp.headers.set("Location", stringOrFirstString(get(queryParams, "next")) || '/');
+    }
+  }
+
+  return resp;
+}
