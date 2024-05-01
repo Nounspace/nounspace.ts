adacc8ecd9d733b22ebfb86e372e16e223525cbc
Julian Early <jearly@berkeley.edu>
2024-05-01 13:26:49 -0700

    refactor: port the rest of pages/api to app

--- src/common/data/api/supabase/api.ts
+++ src/common/data/api/supabase/api.ts
@@ -1,20 +1,20 @@
-import { createServerClient, type CookieOptions, serialize } from '@supabase/ssr'
-import { type NextApiRequest, type NextApiResponse } from 'next'
+import { createServerClient } from '@supabase/ssr'
+import { type NextRequest, type NextResponse } from 'next/server'
 
-export default function createClient(req: NextApiRequest, res: NextApiResponse) {
+export default function createClient(req: NextRequest, res: NextResponse) {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return req.cookies[name]
+          return req.cookies.get(name)?.value;
         },
-        set(name: string, value: string, options: CookieOptions) {
-          res.appendHeader('Set-Cookie', serialize(name, value, options))
+        set(name: string, value: string) {
+          res.cookies.set(name, value);
         },
-        remove(name: string, options: CookieOptions) {
-          res.appendHeader('Set-Cookie', serialize(name, '', options))
+        remove(name: string) {
+          res.cookies.delete(name);
         },
       },
     }
