8162af20700889d0bb7422b41e64e08f9e6ee4a2
Jesse Paterson <jesse.r.pat@gmail.com>
2024-05-03 15:54:51 -0700

    reworked homebase and home to move grid layout further down the chian

--- src/common/ui/templates/space/index.tsx
+++ src/common/ui/templates/space/index.tsx
@@ -1,4 +1,6 @@
-
+import DefaultGrid from "@/fidgets/layout/grids/defaultGrid";
+import Gallery from "@/fidgets/ui/gallery";
+import Feed from "@/pages/feed";
 
 type SpaceArgs = {
   config: {
@@ -15,9 +17,15 @@ type SpaceArgs = {
 
 const Space = ({ config, isEditable }: SpaceArgs) => {
   return (
-    <>
-      
-    </>
+    <DefaultGrid>
+      <Feed/>
+      <Gallery />
+      <div className="p-4 bg-slate-300 row-span-2 col-span-2 rounded-md flex items-center justify-center"></div>
+      <div className="p-4 bg-slate-300 row-span-2 col-span-2 rounded-md flex items-center justify-center"></div>
+      <div className="p-4 bg-slate-300 row-span-2 col-span-4 rounded-md flex items-center justify-center"></div>
+      <div className="p-4 bg-slate-300 row-span-2 col-span-4 rounded-md flex items-center justify-center"></div>
+      <div className="p-4 bg-slate-300 row-span-2 col-span-2 rounded-md flex items-center justify-center"></div>
+    </DefaultGrid>
   );
 };
 
