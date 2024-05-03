cbd63c37c68c842e596e68b33e26cdb4e12ec960
Jesse Paterson <jesse.r.pat@gmail.com>
2024-05-03 13:05:01 -0700

    minor changes

--- src/pages/homebase/homebase.tsx
+++ src/pages/homebase/homebase.tsx
@@ -2,7 +2,7 @@ import Space from "@/common/ui/templates/space";
 import { useAccountStore } from "@/common/data/stores/useAccountStore";
 
 function retrieveConfig(user, space){
-    const fidgets = {};
+    const fidgetConfigs = {};
     const layoutConfig = {}
     const layoutID = "";
 
