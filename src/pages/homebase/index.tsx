549109c8103ac5cef289358f1685004fb596db89
Jesse Paterson <jesse.r.pat@gmail.com>
2024-05-03 15:55:28 -0700

    including a couple missing files

---
+++ src/pages/homebase/index.tsx
@@ -0,0 +1,21 @@
+import Space from "@/common/ui/templates/space";
+import Feed from "../feed";
+import { useAccountStore } from "@/common/data/stores/useAccountStore";
+
+function retrieveConfig(user, space){
+    const fidgetConfigs = {};
+    const layoutConfig = {}
+    const layoutID = "";
+
+    return ({fidgetConfigs, layoutConfig, layoutID})
+}
+
+export default function Homebase(spaceID) {
+
+   //const { getCurrentUser } = useAccountStore();
+    const user = useAccountStore.getState().accounts[0];
+
+    return (
+        <Space config={retrieveConfig(user, spaceID)} isEditable={true}></Space>
+    )
+}
