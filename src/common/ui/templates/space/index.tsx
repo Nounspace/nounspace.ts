beffc8a07641dbaf18559618f3313bc9d701144c
Jesse Paterson <jesse.r.pat@gmail.com>
2024-05-04 16:53:35 -0700

    Edit mode working, but resets locations when turned off

--- src/common/ui/templates/space/index.tsx
+++ src/common/ui/templates/space/index.tsx
@@ -17,45 +17,16 @@ type SpaceArgs = {
     }
   };
   isEditable: boolean;
-}
+} 
 
 const ReactGridLayout = WidthProvider(RGL);
 
 export default function Space({ config, isEditable }: SpaceArgs){
-  function generateDOM() {
-    // Generate items with properties from the layout, rather than pass the layout directly
-    const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se", "ne"];
-    const layout = [
-      {
-        x: 0,
-        y: 0,
-        w: 6,
-        h: 20,
-        f: <Feed/>,
-        resizeHandles: isEditable ? availableHandles : [],
-        minW: 4,
-        maxW: 8,
-        minH: 10,
-        maxH: 20
-      },
-      {
-        f: <Gallery/>,
-        resizeHandles: isEditable ? availableHandles : [],
-        x: 6,
-        y: 0,
-        w: 4,
-        minW: 2,
-        maxW: 4,
-        h: 9,
-        minH: 3,
-        maxH: 12
-      },
-    ];
-    
+  function generateDOM() {    
     return _.map(_.range(config.layoutConfig.items), function(i) {
       return (
-        <div key={i} data-grid={layout[i]} className="overflow-hidden rounded-md m4">
-          {layout[i].f}
+        <div key={i} data-grid={config.fidgetConfigs[i]} className="overflow-hidden rounded-md m4">
+          {config.fidgetConfigs[i].f}
         </div>
       );
     });
@@ -71,7 +42,3 @@ export default function Space({ config, isEditable }: SpaceArgs){
     </ReactGridLayout>
   );
 }
-
-// if (process.env.STATIC_EXAMPLES === true) {
-//   import("../test-hook.jsx").then(fn => fn.default(ResizableHandles));
-// }
