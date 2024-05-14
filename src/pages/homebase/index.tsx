248e2849c1773d99f07f2ace6a02ea0b09b96c08
henry <pye.henry@protonmail.com>
2024-05-14 17:32:34 -0400

    Revert lint pages on homebase

--- src/pages/homebase/index.tsx
+++ src/pages/homebase/index.tsx
@@ -1,42 +1,97 @@
-import { useAccountStore } from "@/common/data/stores/useAccountStore";
-import Space from "@/common/ui/templates/space";
-import ChannelFidget from "@/fidgets/example";
+import { LayoutFidgetDetails } from "@/common/fidgets";
+import Space, { SpaceConfig } from "@/common/ui/templates/Space";
+import { GridLayout, ResizeDirections } from "@/fidgets/layout/Grid";
 import { useState } from "react";
 import { RiPencilFill } from "react-icons/ri";
 
-export default function Homebase(spaceID) {
+export default function Homebase() {
   const [editMode, setMode] = useState(false);
 
-  //const { getCurrentUser } = useAccountStore();
-  const user = useAccountStore.getState().accounts[0];
+  const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se", "ne"] as ResizeDirections[];
+  const defaultLayoutData = [
+    {
+      i: "gallery",
+      resizeHandles: availableHandles,
+      x: 0,
+      y: 0,
+      w: 6,
+      minW: 1,
+      maxW: 12,
+      h: 10,
+      minH: 1,
+      maxH: 12,
+    },
+    {
+      i: "frame",
+      resizeHandles: availableHandles,
+      x: 6,
+      y: 0,
+      w: 3,
+      minW: 2,
+      maxW: 4,
+      h: 6,
+      minH: 3,
+      maxH: 12,
+    },
+  ];
 
-  const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se", "ne"];
-
-  const [fidgetConfigs, setFidgetConfigs] = useState([
-    { f: <ChannelFidget channel="bright-moments" />, resizeHandles: availableHandles, x: 0, y: 0, w: 4, minW: 1, h: 8, minH: 1 },
-    { f: <ChannelFidget channel="invaders" />, resizeHandles: availableHandles, x: 4, y: 0, w: 4, minW: 1, h: 8, minH: 1 },
-  ]);
+  const fidgets = {
+    gallery: {
+      fidgetName: "gallery",
+      id: "gallery",
+      instanceConfig: {
+        editable: false,
+        settings: {
+          imageUrl: "https://storage.googleapis.com/papyrus_images/d467b07030969fab95a8f44b1de596ab.png",
+        },
+      },
+    },
+    frame: {
+      fidgetName: "frame",
+      id: "frame",
+      instanceConfig: {
+        settings: {
+          url: "https://altumbase.com/degen/4888/dIVWKaIQZR",
+        },
+        editable: false,
+      },
+    },
+  };
 
   function switchMode() {
     setMode(!editMode);
   }
 
-  function retrieveConfig(user, space) {
-    const layoutConfig = {
-      isDraggable: editMode,
-      isResizable: editMode,
-      items: 2,
-      cols: 12,
-      rowHeight: 70,
-      onLayoutChange: function () {},
-      // This turns off compaction so you can place items wherever.
-      compactType: null,
-      // This turns off rearrangement so items will not be pushed arround.
-      preventCollision: true,
-    };
-    const layoutID = "";
+  const gridDetails: GridLayout = {
+    isDraggable: false,
+    isResizable: false,
+    items: 4,
+    cols: 12,
+    rowHeight: 70,
+    onLayoutChange: () => {},
+    onDrop: () => {},
+    // This turns off compaction so you can place items wherever.
+    compactType: null,
+    // This turns off rearrangement so items will not be pushed arround.
+    preventCollision: true,
+    maxRows: 9,
+    layout: defaultLayoutData,
+  };
+  const layoutID = "";
+  const layoutDetails: LayoutFidgetDetails = {
+    layoutConfig: gridDetails,
+    layoutFidget: "grid",
+  };
 
-    return { fidgetConfigs, layoutConfig, layoutID };
+  const [spaceConfig, setSpaceConfig] = useState<SpaceConfig>({
+    layoutID,
+    layoutDetails,
+    fidgetConfigs: fidgets,
+  });
+
+  async function saveConfig(config: SpaceConfig) {
+    setSpaceConfig(config);
+    return true;
   }
 
   return (
@@ -46,19 +101,19 @@ export default function Homebase(spaceID) {
         onClick={switchMode}
         className={
           editMode
-            ? "rounded-full bg-white size-12 absolute top-6 right-4 z-10 flex opacity-90 hover:opacity-100 duration-500"
-            : "rounded-full bg-white size-12 absolute top-6 right-4 z-10 flex opacity-50 hover:opacity-100 duration-500"
+            ? "opacity-90 rounded-full bg-white size-12 absolute top-6 right-4 z-10 flex hover:opacity-100 duration-500"
+            : "opacity-50 rounded-full bg-white size-12 absolute top-6 right-4 z-10 flex hover:opacity-100 duration-500"
         }
       >
         <RiPencilFill
           className={
             editMode
               ? "text-slate-900 font-semibold text-2xl absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              : "x  text-gray-700 font-semibold text-2xl absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              : "text-gray-700 font-semibold text-2xl absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
           }
         />
       </button>
-      <Space config={retrieveConfig(user, spaceID)} isEditable={editMode}></Space>
+      <Space config={spaceConfig} isEditable={editMode} saveConfig={saveConfig} />
     </div>
   );
 }
