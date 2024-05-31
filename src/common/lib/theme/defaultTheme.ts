520da59c189454361d9f97a6bf898a2875a581de
Nounspace Ryan <ryan@nounspace.com>
2024-05-31 14:10:47 -0500

    Add default theme

---
+++ src/common/lib/theme/defaultTheme.ts
@@ -0,0 +1,12 @@
+import { GlobalTheme } from "@/common/lib/theme";
+
+export const DEFAULT_THEME: GlobalTheme = {
+  id: 'default',
+  name: 'Default',
+  properties: {
+    font: 'Inter',
+    background: '#ffffff'
+  }
+};
+
+export default DEFAULT_THEME;
