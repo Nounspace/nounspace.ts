c4ec53bcb04dfa18e0c27df252814e5be9119c21
Julian Early <julian.is.early@gmail.com>
2024-05-30 17:52:22 -0700

    feat: add signer request backend

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,4 @@
+import { mnemonicToAccount } from "viem/accounts";
 export const WEBSITE_URL = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}` : process.env.NEXT_PUBLIC_URL;
 export const APP_FID = process.env.NEXT_PUBLIC_APP_FID ? Number(process.env.NEXT_PUBLIC_APP_FID) : undefined;
-export const APP_MNENOMIC = process.env.APP_MNENOMIC;
+export const AppSigner = mnemonicToAccount(process.env.APP_MNENOMIC!);
