fbf16c3de3a1113d4a81db3e87f9f984be337491
Nounspace Ryan <ryan@nounspace.com>
2024-05-31 14:10:42 -0500

    Add initial theme typedefs

---
+++ src/common/lib/theme/index.d.ts
@@ -0,0 +1,69 @@
+export interface ThemeSettings {
+  id: string;
+  name: string;
+  properties: {
+    [key: string]: Color | CSSSize | FontFamily;
+  }
+}
+
+export interface GlobalTheme extends ThemeSettings {
+  properties: {
+    font: FontFamily,
+    background: Color
+  }
+}
+
+export interface ThemeConfig {
+  id: string;
+  name: string;
+  fontPrimary: FontFamily;
+  fontSecondary: FontFamily;
+  background: ColorBackground | ImageBackground | VideoBackground | CodeBackground;
+}
+
+// Sizes
+export type CSSUnit = 'cm' | 'mm' | 'in' | 'px' | 'pt' | 'pc' | 'em' | 'rem' | 'vw' | 'vh' | 'vmin' | 'vmax' | '%';
+export type CSSSize = `${number}${CSSUnit}`;
+
+// Typography
+export type FontFamily = string;
+
+// Colors
+export type Color = HexColor | RGBColor | RGBAColor | HSLColor | HSLAColor | NamedColor;
+export type HexColor = `#${string}`;
+export type RGBColor = `rgb\\s*\\(\\s*${number}\\s*,\\s*${number}\\s*,\\s*${number}\\s*\\)\\s*|\\s*rgb\\s*\\(\\s*${number}%\\s*,\\s*${number}%\\s*,\\s*${number}%\\s*\\)`;
+export type RGBAColor = `rgba\\s*\\(\\s*${number}\\s*,\\s*${number}\\s*,\\s*${number}\\s*,\\s*${number | `${number}%`}\\s*\\)\\s*|\\s*rgba\\s*\\(\\s*${number}%\\s*,\\s*${number}%\\s*,\\s*${number}%\\s*,\\s*${number | `${number}%`}\\s*\\)`;
+export type HSLColor = `hsl\\s*\\(\\s*${number}\\s*,\\s*${number}%\\s*,\\s*${number}%\\s*\\)`;
+export type HSLAColor = `hsla\\s*\\(\\s*${number}\\s*,\\s*${number}%\\s*,\\s*${number}%\\s*,\\s*${number | `${number}%`}\\s*\\)`;
+export type NamedColor = (
+  'aliceblue' | 'antiquewhite' | 'aqua' | 'aquamarine' | 'azure'
+  | 'beige' | 'bisque' | 'black' | 'blanchedalmond' | 'blue'
+  | 'blueviolet' | 'brown' | 'burlywood' | 'cadetblue' | 'chartreuse'
+  | 'chocolate' | 'coral' | 'cornflowerblue' | 'cornsilk' | 'crimson'
+  | 'cyan' | 'darkblue' | 'darkcyan' | 'darkgoldenrod' | 'darkgray'
+  | 'darkgreen' | 'darkgrey' | 'darkkhaki' | 'darkmagenta' | 'darkolivegreen'
+  | 'darkorange' | 'darkorchid' | 'darkred' | 'darksalmon' | 'darkseagreen'
+  | 'darkslateblue' | 'darkslategray' | 'darkslategrey' | 'darkturquoise' | 'darkviolet'
+  | 'deeppink' | 'deepskyblue' | 'dimgray' | 'dimgrey' | 'dodgerblue'
+  | 'firebrick' | 'floralwhite' | 'forestgreen' | 'fuchsia' | 'gainsboro'
+  | 'ghostwhite' | 'gold' | 'goldenrod' | 'gray' | 'green'
+  | 'greenyellow' | 'grey' | 'honeydew' | 'hotpink' | 'indianred'
+  | 'indigo' | 'ivory' | 'khaki' | 'lavender' | 'lavenderblush'
+  | 'lawngreen' | 'lemonchiffon' | 'lightblue' | 'lightcoral' | 'lightcyan'
+  | 'lightgoldenrodyellow' | 'lightgray' | 'lightgreen' | 'lightgrey' | 'lightpink'
+  | 'lightsalmon' | 'lightseagreen' | 'lightskyblue' | 'lightslategray' | 'lightslategrey'
+  | 'lightsteelblue' | 'lightyellow' | 'lime' | 'limegreen' | 'linen'
+  | 'magenta' | 'maroon' | 'mediumaquamarine' | 'mediumblue' | 'mediumorchid'
+  | 'mediumpurple' | 'mediumseagreen' | 'mediumslateblue' | 'mediumspringgreen' | 'mediumturquoise'
+  | 'mediumvioletred' | 'midnightblue' | 'mintcream' | 'mistyrose' | 'moccasin'
+  | 'navajowhite' | 'navy' | 'oldlace' | 'olive' | 'olivedrab'
+  | 'orange' | 'orangered' | 'orchid' | 'palegoldenrod' | 'palegreen'
+  | 'paleturquoise' | 'palevioletred' | 'papayawhip' | 'peachpuff' | 'peru'
+  | 'pink' | 'plum' | 'powderblue' | 'purple' | 'red'
+  | 'rosybrown' | 'royalblue' | 'saddlebrown' | 'salmon' | 'sandybrown'
+  | 'seagreen' | 'seashell' | 'sienna' | 'silver' | 'skyblue'
+  | 'slateblue' | 'slategray' | 'slategrey' | 'snow' | 'springgreen'
+  | 'steelblue' | 'tan' | 'teal' | 'thistle' | 'tomato'
+  | 'turquoise' | 'violet' | 'wheat' | 'white' | 'whitesmoke'
+  | 'yellow' | 'yellowgreen' | 'transparent' | 'currentColor'
+);
