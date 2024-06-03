1ef93d309beca1d0a2965893bb059f40e24ec8e8
Julian Early <julian.is.early@gmail.com>
2024-06-03 12:02:40 -0700

    refactor: remove extra console.log

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,3 @@
-console.log()
 export const WEBSITE_URL = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}` : process.env.NEXT_PUBLIC_URL;
 export const APP_FID = process.env.NEXT_PUBLIC_APP_FID ? Number(process.env.NEXT_PUBLIC_APP_FID) : undefined;
 export const APP_MNENOMIC = process.env.APP_MNENOMIC;
