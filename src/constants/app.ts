3a93885fa28893030a034763fd9ae39f366dbccc
Julian Early <julian.is.early@gmail.com>
2024-06-03 11:59:05 -0700

    fix: change to use NEXT_PUBLIC_VERCEL_ENV

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,4 @@
-export const WEBSITE_URL = process.env.VERCEL_ENV === "preview" ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}` : process.env.NEXT_PUBLIC_URL;
+console.log()
+export const WEBSITE_URL = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}` : process.env.NEXT_PUBLIC_URL;
 export const APP_FID = process.env.NEXT_PUBLIC_APP_FID ? Number(process.env.NEXT_PUBLIC_APP_FID) : undefined;
 export const APP_MNENOMIC = process.env.APP_MNENOMIC;
