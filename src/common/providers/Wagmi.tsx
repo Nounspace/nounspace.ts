d46c391b790078865042a61003459e076e739203
Julian Early <julian.is.early@gmail.com>
2024-06-04 19:09:15 -0700

    feat: update constants to include base chain
    Also moved ALCHEMY_API_KEY into next public

--- src/common/providers/Wagmi.tsx
+++ src/common/providers/Wagmi.tsx
@@ -3,14 +3,15 @@ import { Chain, Transport } from "viem";
 import { createConfig } from "wagmi";
 import { WagmiProvider } from '@privy-io/wagmi';
 import type { CreateConfigParameters } from "wagmi";
-import { mainnetHttp, optimismHttp } from "@/constants/chains";
-import { mainnet, optimism } from "viem/chains";
+import { baseHttp, mainnetHttp, optimismHttp } from "@/constants/chains";
+import { base, mainnet, optimism } from "viem/chains";
 
 const wagmiParams: CreateConfigParameters<readonly [Chain, ...Chain[]], Record<number, Transport>> = {
-  chains: [optimism, mainnet],
+  chains: [optimism, mainnet, base],
   transports: {
     [optimism.id]: optimismHttp,
     [mainnet.id]: mainnetHttp,
+    [base.id]: baseHttp,
   },
 };
 
