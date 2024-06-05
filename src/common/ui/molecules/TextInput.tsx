33c149af67aa2c86fd12b20b2a28b4cf30ec90fd
Nounspace Ryan <ryan@nounspace.com>
2024-06-05 13:39:10 -0500

    Fidget settings configuration

--- src/common/ui/molecules/TextInput.tsx
+++ src/common/ui/molecules/TextInput.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import { Input } from "../atoms/input";
+import React, { forwardRef, useCallback } from "react";
+import { Input, InputProps } from "@/common/ui/atoms/input";
 
-const TextInput: React.FC = (props, ref) => {
+export interface TextInputProps extends Omit<InputProps, 'onChange'> {
+  value: string;
+  onChange?: (value: string) => void;
+}
+
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+  const onChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      props.onChange?.(event.target.value);
+    },
+    [props.onChange]
+  );
+
   return (
     <Input
       {...props}
       ref={ref}
-    ></Input>
+      onChange={onChange}
+    />
   );
-};
+});
 
 export default TextInput;
---
+++ src/common/ui/organisms/FidgetSettingsEditor.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { FidgetSettings, FidgetEditConfig, FidgetFieldConfig } from "@/common/fidgets";
+import { reduce } from "lodash";
+
+export type FidgetSettingsEditorProps = {
+  readonly editConfig: FidgetEditConfig;
+  settings: FidgetSettings;
+  onSave: (settings: FidgetSettings) => void;
+};
+
+type FidgetSettingsRowProps = {
+  field: FidgetFieldConfig;
+  value: any;
+  onChange: (value: any) => void;
+}
+
+const FidgetSettingsRow: React.FC<FidgetSettingsRowProps> = ({ field, value, onChange }) => {
+  const InputComponent = field.inputSelector
+
+  return (
+    <div className="mb-5">
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{ field.fieldName }</label>
+      {
+        <InputComponent
+          value={value}
+          onChange={onChange}
+          className="h-9"
+        />
+      }
+    </div>
+  )
+}
+
+// Merges defaults and saved settings
+const getInitialSettings = (
+  settings: FidgetSettings,
+  config: FidgetEditConfig
+): FidgetSettings => {
+  return reduce(
+    config.fields,
+    (acc, f) => ({
+      ...acc,
+      [f.fieldName]: f.fieldName in settings ? settings[f.fieldName] : (f.default || null),
+    }),
+    {},
+  )
+}
+
+export const FidgetSettingsEditor: React.FC<FidgetSettingsEditorProps> = ({
+  editConfig,
+  settings,
+  onSave,
+}) => {
+  const [state, setState] = useState<FidgetSettings>(getInitialSettings(settings, editConfig))
+
+  const _onSave = (e) => {
+    e.preventDefault()
+    onSave(state)
+  }
+
+  return (
+    <form className="" onSubmit={_onSave}>
+      {
+        editConfig.fields.map((field, i) => (
+          <FidgetSettingsRow
+            field={field}
+            key={i}
+            value={state[field.fieldName]}
+            onChange={(val) => {
+              setState({
+                ...state,
+                [field.fieldName]: val
+              })
+            }}
+          />
+        ))
+      }
+      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+    </form>
+  )
+}
+
+export default FidgetSettingsEditor;
