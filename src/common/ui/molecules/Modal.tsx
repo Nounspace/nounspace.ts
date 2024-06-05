0fc9d3816f3321d09db730d8b3eac592f3d29554
Nounspace Ryan <ryan@nounspace.com>
2024-06-05 11:10:15 -0500

    Update global styles

--- src/common/ui/molecules/Modal.tsx
+++ src/common/ui/molecules/Modal.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Cross2Icon } from '@radix-ui/react-icons';
 
-type ModalProps = {
+export type ModalProps = {
   open: boolean;
   setOpen: (open: boolean) => void;
   title?: string;
   description?: string;
   children?: React.ReactNode;
+  overlay?: boolean;
+
+  // The modality of the dialog. When set to true, interaction with outside elements
+  // will be disabled and only dialog content will be visible to screen readers.
   focusMode?: boolean;
 }
 
-const Modal = ({ open, setOpen, title, description, children, focusMode }: ModalProps) => (
+const Modal = ({ open, setOpen, title, description, children, focusMode, overlay = true }: ModalProps) => (
   <Dialog.Root open={open} onOpenChange={setOpen} modal={focusMode || true}>
     <Dialog.Portal>
-      <Dialog.Overlay className="bg-muted/95 data-[state=open]:animate-overlayShow fixed inset-0" />
+      {overlay && <Dialog.Overlay className="bg-muted/95 data-[state=open]:animate-overlayShow fixed inset-0" />}
       <Dialog.Content className="data-[state=open]:animate-contentShow fixed bg-background top-[40%] left-[50%] w-[100vw] max-w-[600px] translate-x-[-50%] translate-y-[-40%] rounded-[6px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
         {title && (
           <Dialog.Title className="text-card-foreground m-0 text-[17px] font-medium">
