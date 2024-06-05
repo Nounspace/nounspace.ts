99fd776effb103aca008e436110194b0d46ed45c
Julian Early <julian.is.early@gmail.com>
2024-06-04 19:14:16 -0700

    Change Signer to use Warpcast + sponsor nOGs holders sign up (#53)
    * feat: update constants to include base chain
    
    Also moved ALCHEMY_API_KEY into next public
    
    * feat: change backend to use Warpcast instead of Neynar + sponsor sign ups for nOGs holders

--- src/pages/api/signerRequests.ts
+++ src/pages/api/signerRequests.ts
@@ -1,13 +1,15 @@
 import { APP_FID } from "@/constants/app";
 import { AppSigner } from "@/constants/app-server-side";
+import { NOGS_CONTRACT_ADDR } from "@/constants/nogs";
+import { ALCHEMY_API, WARPCAST_API } from "@/constants/urls";
 import { SIGNED_KEY_REQUEST_TYPE, SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN } from "@farcaster/core";
-import { NeynarAPIClient } from "@neynar/nodejs-sdk";
-import { DeveloperManagedSigner } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import axios from "axios";
 import { isArray, isUndefined } from "lodash";
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type CreateSignerRequest = {
   publicKey: `0x${string}`;
+  requestingWallet?: `0x${string}`;
 };
 
 export type SignerResponse<D> = {
@@ -18,8 +20,40 @@ export type SignerResponse<D> = {
   value?: D;
 };
 
-async function handlePost(req: NextApiRequest, res: NextApiResponse<SignerResponse<DeveloperManagedSigner>>) {
-  const { publicKey } = req.body as CreateSignerRequest;
+type SignedKeyRequestSponsorship = {
+  sponsorFid: number;
+  signature: string; // sponsorship signature by sponsorFid
+}
+
+type SignedKeyRequestBody = {
+  key: string;
+  requestFid: number;
+  deadline: number;
+  signature: string; // key request signature by requestFid
+  sponsorship?: SignedKeyRequestSponsorship;
+}
+
+export type SignedKeyRequestResponse = {
+  token: string;
+  deeplinkUrl: string;
+  isSponsored: boolean;
+  key: `0x${string}`;
+  state: "pending" | "completed" | "approved";
+  requestFid: number;
+};
+
+type SignedKeyRequestResponseBody = {
+  result: {
+    signedKeyRequest: SignedKeyRequestResponse;
+  };
+};
+
+type AlchemyIsHolderOfContract = {
+  isHolderOfContract: boolean;
+};
+
+async function handlePost(req: NextApiRequest, res: NextApiResponse<SignerResponse<SignedKeyRequestResponse>>) {
+  const { publicKey, requestingWallet } = req.body as CreateSignerRequest;
 
   if (isUndefined(publicKey)) {
     return res.status(400).json({
@@ -30,7 +64,6 @@ async function handlePost(req: NextApiRequest, res: NextApiResponse<SignerRespon
     });
   }
 
-  const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
   const deadline = Math.floor(Date.now() / 1000) + 86400; // 24 hours from now
   const signature = await AppSigner.signTypedData({
     domain: SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN,
@@ -45,14 +78,46 @@ async function handlePost(req: NextApiRequest, res: NextApiResponse<SignerRespon
     },
   });
 
+  let shouldSponsor = false;
+  if (!isUndefined(requestingWallet)) {
+    try {
+      const { data } = await axios.get<AlchemyIsHolderOfContract>(
+        `${ALCHEMY_API("base")}nft/v3/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}/isHolderOfContract`,
+        {
+          params: {
+            wallet: requestingWallet,
+            contractAddress: NOGS_CONTRACT_ADDR,
+          }
+        }
+      );
+      shouldSponsor = data.isHolderOfContract;
+    } catch (e) {
+      console.error(e);
+      console.log("Failed to check ")
+    }
+  }
+
+  let sponsorship: SignedKeyRequestSponsorship | undefined = undefined;
+  if (shouldSponsor) {
+    sponsorship = {
+      sponsorFid: APP_FID!,
+      signature: await AppSigner.signMessage({ message: { raw: signature }}),
+    };
+  }
+
   try {
-    const result = await client.registerSignedKeyForDeveloperManagedSigner(
-      publicKey, signature, APP_FID!, deadline,
-    );
+    const requestArgs: SignedKeyRequestBody = {
+      key: publicKey,
+      requestFid: APP_FID!,
+      signature,
+      deadline,
+      sponsorship,
+    };
+    const { data } = await axios.post<SignedKeyRequestResponseBody>(`${WARPCAST_API}/v2/signed-key-requests`, requestArgs);
 
     return res.status(200).json({
       result: "success",
-      value: result,
+      value: data.result.signedKeyRequest,
     });
   } catch (e) {
     console.error(e);
@@ -65,40 +130,42 @@ async function handlePost(req: NextApiRequest, res: NextApiResponse<SignerRespon
   }
 }
 
-async function handleGet(req: NextApiRequest, res: NextApiResponse) {
-  const publicKey = req.query.publicKey;
+async function handleGet(req: NextApiRequest, res: NextApiResponse<SignerResponse<SignedKeyRequestResponse>>) {
+  const token = req.query.token;
 
-  if (isUndefined(publicKey)) {
+  if (isUndefined(token)) {
     return res.status(400).json({
       result: "error",
       error: {
-        message: "No Public Key Provided",
+        message: "No token Provided",
       },
     });
   }
 
-  if (isArray(publicKey)) {
+  if (isArray(token)) {
     return res.status(400).json({
       result: "error",
       error: {
-        message: "Only provide a single public key",
+        message: "Only provide a single token",
       },
     });
   }
-
-  const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
 
   try {
-    const result = await client.lookupDeveloperManagedSigner(publicKey);
+    const { data } = await axios.get<SignedKeyRequestResponseBody>(`${WARPCAST_API}/v2/signed-key-request`, {
+      params: {
+        token,
+      },
+    });
 
     return res.status(200).json({
       result: "success",
-      value: result,
+      value: data.result.signedKeyRequest,
     });
   } catch (e) {
     console.error(e);
     return res.status(500).json({
-      status: "error",
+      result: "error",
       error: {
         message: "An error occurred registering the signer with Neynar",
       },
@@ -108,7 +175,7 @@ async function handleGet(req: NextApiRequest, res: NextApiResponse) {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<SignerResponse<DeveloperManagedSigner>>
+  res: NextApiResponse<SignerResponse<SignedKeyRequestResponse>>
 ) {
   if (req.method === "POST") {
     return handlePost(req, res);
