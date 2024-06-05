a0a9d4244d97524d406353818364a533f97bc9c0
Julian Early <julian.is.early@gmail.com>
2024-06-04 19:14:16 -0700

    Change Signer to use Warpcast + sponsor nOGs holders sign up (#53)
    * feat: update constants to include base chain
    
    Also moved ALCHEMY_API_KEY into next public
    
    * feat: change backend to use Warpcast instead of Neynar + sponsor sign ups for nOGs holders

---
+++ src/constants/urls.ts
@@ -0,0 +1,18 @@
+
+export const WARPCAST_API = "https://api.warpcast.com";
+
+
+const optimismUrl = `https://opt-mainnet.g.alchemy.com/`
+const mainnetUrl = `https://eth-mainnet.g.alchemy.com/`;
+const baseUrl = `https://base-mainnet.g.alchemy.com/`;
+export function ALCHEMY_API(network: "base" | "opt" | "eth") {
+  let url: string;
+  if (network === "base") {
+    url = baseUrl;
+  } else if (network === "opt") {
+    url = optimismUrl;
+  } else {
+    url = mainnetUrl;
+  }
+  return `${url}`;
+}
