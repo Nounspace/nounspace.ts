a40663794a5aca9dff02c9700c7bd547ad1bf9c8
Julian Early <julian.is.early@gmail.com>
2024-06-07 16:28:51 -0700

    fix: img to next/js Image

--- src/common/ui/organisms/Navigation.tsx
+++ src/common/ui/organisms/Navigation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { mergeClasses } from "@/common/lib/utils/mergeClasses";
+import Image from "next/image";
 
 const HomeIcon = () => {
   return (
@@ -62,7 +63,7 @@ const ShareIcon = () => {
 const BrandHeader = () => {
   return (
     <Link href="/homebase" className="flex items-center ps-2.5 mb-4">
-      <img src="/images/noggles.svg" className="h-13 me-3" alt="Nounspace Logo" />
+      <Image src="/images/noggles.svg" className="h-13 me-3" alt="Nounspace Logo" />
       {false && <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Nounspace</span>}
     </Link>
   )
