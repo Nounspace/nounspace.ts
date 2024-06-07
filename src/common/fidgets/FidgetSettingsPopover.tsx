284383ae0b77688845c67754f1ed061ea143a29b
Nounspace Ryan <ryan@nounspace.com>
2024-06-07 16:16:06 -0500

    Change fidget settings modal to popover

---
+++ src/common/fidgets/FidgetSettingsPopover.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Popover, PopoverContent, PopoverAnchor } from "@/common/ui/atoms/popover";
+import { FidgetSettings, FidgetEditConfig, FidgetFieldConfig } from "@/common/fidgets";
+import FidgetSettingsEditor, { FidgetSettingsEditorProps } from "@/common/ui/organisms/FidgetSettingsEditor";
+
+type FidgetSettingsPopoverProps = FidgetSettingsEditorProps & {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  onSave: (newSettings: FidgetSettings) => void;
+}
+
+export const FidgetSettingsPopover: React.FC<FidgetSettingsPopoverProps> = ({
+  open,
+  setOpen,
+  onSave,
+  editConfig,
+  settings,
+}) =>  {
+  return (
+    <Popover
+      open={open}
+      onOpenChange={setOpen}
+      modal={false}
+    >
+      <PopoverAnchor />
+      <PopoverContent
+        align="center"
+        side="bottom"
+        onMouseDown={e => e.stopPropagation()}
+        className=""
+      >
+        <FidgetSettingsEditor
+          editConfig={editConfig}
+          settings={settings}
+          onSave={onSave}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default FidgetSettingsPopover;
