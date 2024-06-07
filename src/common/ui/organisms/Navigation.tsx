cc8b3220d8d81ded3e4142b264a2dc0427c0270d
Nounspace Ryan <ryan@nounspace.com>
2024-06-07 16:20:19 -0500

    Add Navigation component

---
+++ src/common/ui/organisms/Navigation.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { classNames } from "@/styles/utils/css";
+
+const HomeIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
+    </svg>
+  )
+}
+
+const FeedIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 9H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h6m0-6v6m0-6 5.419-3.87A1 1 0 0 1 18 5.942v12.114a1 1 0 0 1-1.581.814L11 15m7 0a3 3 0 0 0 0-6M6 15h3v5H6v-5Z"/>
+    </svg>
+  )
+}
+
+const ExploreIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+    </svg>
+  )
+}
+
+const ChannelsIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7h1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h11.5M7 14h6m-6 3h6m0-10h.5m-.5 3h.5M7 7h3v3H7V7Z"/>
+    </svg>
+  )
+}
+
+const BookmarkIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m17 21-5-4-5 4V3.889a.92.92 0 0 1 .244-.629.808.808 0 0 1 .59-.26h8.333a.81.81 0 0 1 .589.26.92.92 0 0 1 .244.63V21Z"/>
+    </svg>
+  )
+}
+
+const SettingsIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+    </svg>
+  )
+}
+
+const ShareIcon = () => {
+  return (
+    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M7.926 10.898 15 7.727m-7.074 5.39L15 16.29M8 12a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm12 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm0-11a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
+    </svg>
+  )
+}
+
+const BrandHeader = () => {
+  return (
+    <a href="/homebase" className="flex items-center ps-2.5 mb-4">
+      <img src="/images/noggles.svg" className="h-13 me-3" alt="Nounspace Logo" />
+      {false && <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Nounspace</span>}
+    </a>
+  )
+}
+
+const NavItem = ({ label, active, Icon }) => {
+  return (
+    <li>
+      <a href="#" className={
+        classNames(
+          "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
+          active && 'bg-gray-100'
+        )
+      }>
+        <Icon
+          className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+          aria-hidden="true"
+        />
+        <span className="ms-2">{label}</span>
+      </a>
+    </li>
+  );
+}
+
+const Navigation = () => {
+  return (
+    <aside id="logo-sidebar" className="absolute top-12 bottom-12 z-8 w-[270px] transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+      <div className="h-full px-4 py-4 overflow-y-auto border border-blue-100 rounded-xl relative bg-card">
+        <BrandHeader />
+        <div className="text-lg font-medium">
+          <ul className="space-y-2">
+            <NavItem label="Homebase" Icon={HomeIcon} active={true} />
+            <NavItem label="Feed" Icon={FeedIcon} />
+            <NavItem label="Explore" Icon={ExploreIcon} />
+            <NavItem label="Channels" Icon={ChannelsIcon} />
+            <NavItem label="Bookmark" Icon={BookmarkIcon} />
+          </ul>
+          <div className="absolute bottom-0 left-0 right-0 px-4 py-3">
+            <div className="mt-5 pt-2 border-t border-gray-200 dark:border-gray-700">
+              <a href="#" className="flex items-center justify-between p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <div className="flex items-center">
+                  <SettingsIcon />
+                  <span className="ms-2">Settings</span>
+                </div>
+                <div className="border-l border-gray-200 dark:border-gray-700 ps-3">
+                  <ShareIcon />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default Navigation;
