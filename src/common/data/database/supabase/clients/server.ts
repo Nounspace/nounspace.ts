a82f55122e38acca901fb0dfb012b0697ce42510
Julian Early <julian.is.early@gmail.com>
2024-06-13 16:43:24 -0700

    feat: add types for supabase DB

--- src/common/data/database/supabase/clients/server.ts
+++ src/common/data/database/supabase/clients/server.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database as SupabaseDataBaseType } from "@/supabase/database";
 
-const supabaseClient = createClient(
+const supabaseClient = createClient<SupabaseDataBaseType>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
 );
