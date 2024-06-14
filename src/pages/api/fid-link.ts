9ad81ab30c9b3e6403bc09e44dc519f1f43d7cc7
Julian Early <julian.is.early@gmail.com>
2024-06-14 13:55:43 -0700

    feat: add end point to register identities to fids

---
+++ src/pages/api/fid-link.ts
@@ -0,0 +1,154 @@
+import requestHandler, {
+  NounspaceResponse,
+} from "@/common/data/api/requestHandler";
+import { isSignable, validateSignable } from "@/common/lib/signedFiles";
+import { NextApiRequest, NextApiResponse } from "next";
+import neynar from "@/common/data/api/neynar";
+import supabaseClient from "@/common/data/database/supabase/clients/server";
+import moment from "moment";
+import { first } from "lodash";
+
+export type FidLinkToIdentityRequest = {
+  fid: number;
+  identityPublicKey: string;
+  timestamp: string;
+  signature: string;
+  signingPublicKey: string;
+};
+
+function isFidLinkToIdentityRequest(
+  maybe: unknown,
+): maybe is FidLinkToIdentityRequest {
+  if (!isSignable(maybe, "signingPublicKey")) {
+    return false;
+  }
+  return (
+    typeof maybe["fid"] === "number" &&
+    typeof maybe["timestamp"] === "number" &&
+    typeof maybe["identityPublicKey"] === "string"
+  );
+}
+
+export type FidLinkToIdentityResponse = NounspaceResponse<{
+  fid: number;
+  identityPublicKey: string;
+  created: string;
+  signature: string;
+  signingPublicKey: string;
+  isSigningKeyValid: boolean;
+}>;
+
+async function checkSigningKeyValidForFid(fid: number, signingKey: string) {
+  try {
+    const result = await neynar.lookupDeveloperManagedSigner(signingKey);
+    return result.fid === fid && result.status === "approved";
+  } catch {
+    return false;
+  }
+}
+
+async function linkFidToIdentity(
+  req: NextApiRequest,
+  res: NextApiResponse<FidLinkToIdentityResponse>,
+) {
+  const reqBody = req.body;
+  if (!isFidLinkToIdentityRequest(reqBody)) {
+    res.status(400).json({
+      result: "error",
+      error: {
+        message:
+          "Registration request requires fid, timestamp, identityPublicKey, signingPublicKey, and a signature",
+      },
+    });
+    return;
+  }
+  if (!validateSignable(reqBody)) {
+    res.status(400).json({
+      result: "error",
+      error: {
+        message: "Invalid signature",
+      },
+    });
+    return;
+  }
+  if (!checkSigningKeyValidForFid) {
+    res.status(400).json({
+      result: "error",
+      error: {
+        message: `Signing key ${reqBody.signingPublicKey} is not valid for fid ${reqBody.fid}`,
+      },
+    });
+  }
+  const { data: checkExistsData } = await supabaseClient
+    .from("fidRegistrations")
+    .select("fid, created")
+    .eq("fid", reqBody.fid);
+  if (checkExistsData && checkExistsData.length > 0) {
+    const currentRecord = first(checkExistsData);
+    if (moment(currentRecord?.created).isAfter(reqBody.timestamp)) {
+      res.status(400).json({
+        result: "error",
+        error: {
+          message:
+            "New registration is less recent than current registration, try again",
+        },
+      });
+      return;
+    }
+    const { data, error } = await supabaseClient
+      .from("fidRegistrations")
+      .update({
+        created: reqBody.timestamp,
+        identityPublicKey: reqBody.identityPublicKey,
+        isSigningKeyValid: true,
+        signature: reqBody.signature,
+        signingKeyLastValidatedAt: moment().toISOString(),
+        signingPublicKey: reqBody.signingPublicKey,
+      })
+      .eq("fid", reqBody.fid)
+      .select();
+    if (error !== null) {
+      res.status(500).json({
+        result: "error",
+        error: {
+          message: error.message,
+        },
+      });
+      return;
+    }
+    res.status(200).json({
+      result: "success",
+      value: first(data),
+    });
+  } else {
+    const { data, error } = await supabaseClient
+      .from("fidRegistrations")
+      .insert({
+        fid: reqBody.fid,
+        created: reqBody.timestamp,
+        identityPublicKey: reqBody.identityPublicKey,
+        isSigningKeyValid: true,
+        signature: reqBody.signature,
+        signingKeyLastValidatedAt: moment().toISOString(),
+        signingPublicKey: reqBody.signingPublicKey,
+      })
+      .select();
+    if (error !== null) {
+      res.status(500).json({
+        result: "error",
+        error: {
+          message: error.message,
+        },
+      });
+      return;
+    }
+    res.status(200).json({
+      result: "success",
+      value: first(data),
+    });
+  }
+}
+
+export default requestHandler({
+  post: linkFidToIdentity,
+});
