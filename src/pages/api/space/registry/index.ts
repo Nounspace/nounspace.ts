99c9648aaec22c4a2c7c9afe2bf270f6dbd31361
Julian Early <julian.is.early@gmail.com>
2024-06-14 14:36:22 -0700

    feat: add space registration handler

---
+++ src/pages/api/space/registry/index.ts
@@ -0,0 +1,107 @@
+import requestHandler, {
+  NounspaceResponse,
+} from "@/common/data/api/requestHandler";
+import supabaseClient from "@/common/data/database/supabase/clients/server";
+import { isSignable, validateSignable } from "@/common/lib/signedFiles";
+import { first } from "lodash";
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+type SpaceRegistration = {
+  spaceName: string;
+  identityPublicKey: string;
+  fid: number;
+  signature: string;
+  timestamp: string;
+};
+
+type SpaceInfo = SpaceRegistration & {
+  spaceId: string;
+};
+
+function isSpaceRegistration(maybe: unknown): maybe is SpaceRegistration {
+  if (!isSignable(maybe, "identityPublicKey")) {
+    return false;
+  }
+  return (
+    typeof maybe["spaceName"] === "string" &&
+    typeof maybe["fid"] === "string" &&
+    typeof maybe["timestamp"] === "string"
+  );
+}
+
+export type RegisterNewSpaceResponse = NounspaceResponse<SpaceInfo>;
+
+export type ModifiableSpacesResponse = NounspaceResponse<{
+  identity: string;
+  spaces: SpaceInfo[];
+}>;
+
+// Handles the registration of a new space name to requesting identity
+// Checks that the identity is allowed to register more than one name
+async function registerNewSpace(
+  req: NextApiRequest,
+  res: NextApiResponse<RegisterNewSpaceResponse>,
+) {
+  const registration = req.body;
+  if (!isSpaceRegistration(registration)) {
+    res.status(400).json({
+      result: "error",
+      error: {
+        message:
+          "Registration of a new space requires spaceName, fid, timestamp, identityPublicKey, and signature",
+      },
+    });
+    return;
+  }
+  if (!validateSignable(registration)) {
+    res.status(400).json({
+      result: "error",
+      error: {
+        message: "Invalid signature",
+      },
+    });
+    return;
+  }
+  const { data } = await supabaseClient
+    .from("spaceRegistrations")
+    .select("spaceId")
+    .eq("fid", registration.fid)
+    .eq("spaceName", registration.spaceName);
+  if (data && data.length > 0) {
+    res.status(400).json({
+      result: "error",
+      error: {
+        message: `fid ${registration.fid} already has a space named ${registration.spaceName}`,
+      },
+    });
+  }
+  const { data: result, error } = await supabaseClient
+    .from("spaceRegistrations")
+    .insert([{ ...registration, isDefault: false }])
+    .select();
+  if (error) {
+    res.status(500).json({
+      result: "error",
+      error: {
+        message: error.message,
+      },
+    });
+    return;
+  }
+  res.status(200).json({
+    result: "success",
+    value: first(result),
+  });
+}
+
+// Returns a list of the spaces that the requesting identity
+// owns and can modify, including updates and renaming
+async function listModifiableSpaces(
+  req: NextApiRequest,
+  res: NextApiResponse<ModifiableSpacesResponse>,
+) {}
+
+export default requestHandler({
+  get: listModifiableSpaces,
+  post: registerNewSpace,
+});
