58ad2992ad5d9bcb766a910a9184d0465b8f5500
Julian Early <julian.is.early@gmail.com>
2024-06-19 10:14:48 -0700

    debug: add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import bundlerAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundlerAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA).
@@ -29,4 +35,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withBundleAnalyzer(nextConfig);
