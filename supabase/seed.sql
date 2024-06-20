8fd71754ef771944e2e4527752ed85c22d715c4d
Julian Early <julian.is.early@gmail.com>
2024-06-20 14:32:59 -0700

    hotfix: supabase seed change double quote to single to fix issue with sql seed command

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2,4 +2,5 @@
 INSERT INTO storage.buckets 
   ("id", "name", "created_at", "updated_at", "public", "avif_autodetection")
 VALUES
-  ('private', 'private', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', true, false);
+  ('private', 'private', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', true, false),
+  ('spaces', 'spaces', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', true, false);
