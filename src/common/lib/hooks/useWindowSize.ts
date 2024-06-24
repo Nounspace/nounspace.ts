0db4574180c588d7eb7a6e184354863b9995ecd3
Jesse Paterson <jesse.r.pat@gmail.com>
2024-06-23 19:46:11 -0500

    clean up

--- src/common/lib/hooks/useWindowSize.ts
+++ src/common/lib/hooks/useWindowSize.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function useWindowDimensions() {
+export default function useWindowSize() {
   const hasWindow = typeof window !== "undefined";
 
   function getWindowDimensions() {
@@ -16,12 +16,12 @@ export default function useWindowDimensions() {
     getWindowDimensions(),
   );
 
+  function handleResize() {
+    setWindowDimensions(getWindowDimensions());
+  }
+
   useEffect(() => {
     if (hasWindow) {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     }
