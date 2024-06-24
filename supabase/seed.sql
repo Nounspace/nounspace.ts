36b6d3c50ec8efaf939e38833e8f3e765818d49c
Julian Early <julian.is.early@gmail.com>
2024-06-24 11:22:36 -0700

    hotfix: update seed migrations to ignore bucket conflicts

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -4,4 +4,5 @@ INSERT INTO storage.buckets
 VALUES
   ('explore', 'explore', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', false, false),
   ('private', 'private', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', true, false),
-  ('spaces', 'spaces', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', true, false);
+  ('spaces', 'spaces', '2024-05-21 23:43:16.762796+00', '2024-05-21 23:43:16.762796+00', true, false)
+ON CONFLICT ("id") DO NOTHING;
