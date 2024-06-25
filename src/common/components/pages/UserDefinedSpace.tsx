65b9d99fbe4bba3abea5ad8aa686ecd5816f49f7
Julian Early <julian.is.early@gmail.com>
2024-06-25 13:28:45 -0700

    feat: inital versions of person spaces

---
+++ src/common/components/pages/UserDefinedSpace.tsx
@@ -0,0 +1,86 @@
+import { findIndex, isNil } from "lodash";
+import React, { useEffect, useMemo, useState } from "react";
+import { useAuthenticatorManager } from "@/authenticators/AuthenticatorManager";
+import { useAppStore } from "@/common/data/stores";
+import INITIAL_PERSONAL_SPACE_CONFIG from "@/constants/initialPersonSpace";
+import SpaceWithLoader from "../templates/SpaceWithLoader";
+import { SpaceConfig } from "../templates/Space";
+
+const FARCASTER_NOUNSPACE_AUTHENTICATOR_NAME = "farcaster:nounspace";
+
+export default async function UserDefinedSpace({
+  spaceId: providedSpaceId,
+  fid,
+}: {
+  spaceId: string | null;
+  fid: number;
+}) {
+  const authenticatorManager = useAuthenticatorManager();
+  const {
+    editableSpaces,
+    loadSpace,
+    remoteSpaces,
+    localSpaces,
+    saveLocalCopy,
+    commitSpaceToDb,
+  } = useAppStore((state) => ({
+    editableSpaces: state.space.editableSpaces,
+    loadSpace: state.space.loadSpace,
+    localSpaces: state.space.localSpaces,
+    remoteSpaces: state.space.remoteSpaces,
+    saveLocalCopy: state.space.saveLocalSpace,
+    commitSpaceToDb: state.space.commitSpaceToDatabase,
+  }));
+
+  useEffect(() => {
+    if (!isNil(providedSpaceId)) {
+      loadSpace(providedSpaceId);
+    }
+  }, []);
+
+  const isSignedIntoFarcaster = await useMemo(
+    async () =>
+      findIndex(
+        await authenticatorManager.getInitializedAuthenticators(),
+        FARCASTER_NOUNSPACE_AUTHENTICATOR_NAME,
+      ) > -1,
+    [authenticatorManager],
+  );
+  const currentUserFid = await useMemo(async () => {
+    if (!isSignedIntoFarcaster) return null;
+    const authManagerResp = await authenticatorManager.callMethod(
+      "root",
+      FARCASTER_NOUNSPACE_AUTHENTICATOR_NAME,
+      "getSignerFid",
+    );
+    if (authManagerResp.result === "success") {
+      return authManagerResp.value as number;
+    }
+    return null;
+  }, [isSignedIntoFarcaster, authenticatorManager]);
+
+  const isEditable = useMemo(() => {
+    return (
+      (isNil(spaceId) && fid === currentUserFid) ||
+      (!isNil(spaceId) && spaceId in editableSpaces)
+    );
+  }, [editableSpaces, currentUserFid]);
+
+  const [spaceId, setSpaceId] = useState(providedSpaceId);
+
+  const config = isNil(spaceId)
+    ? INITIAL_PERSONAL_SPACE_CONFIG
+    : localSpaces[spaceId];
+  const saveConfig = async (spaceConfig: SpaceConfig) => {};
+  const commitConfig = async () => {};
+  const resetConfig = async () => {};
+
+  return (
+    <SpaceWithLoader
+      config={config}
+      saveConfig={saveConfig}
+      commitConfig={commitConfig}
+      resetConfig={resetConfig}
+    />
+  );
+}
---
+++ src/pages/s/[handle].tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import neynar from "@/common/data/api/neynar";
+import supabaseClient from "@/common/data/database/supabase/clients/server";
+import { useAppStore } from "@/common/data/stores";
+import { first, isArray, isNil, isNull, isUndefined } from "lodash";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useEffect } from "react";
+import { NextPageWithLayout } from "../_app";
+import LoggedInStateManager from "@/common/components/templates/LoggedInStateManager";
+import UserDefinedSpace from "@/common/components/pages/UserDefinedSpace";
+import SpaceNotFound from "@/common/components/pages/SpaceNotFound";
+
+type SpacePageProps = {
+  spaceId: string | null;
+  fid: number | null;
+  handle: string | string[] | undefined;
+};
+
+export const getServerSideProps = (async ({
+  params,
+}: GetServerSidePropsContext) => {
+  const handle =
+    isUndefined(params) || isUndefined(params.handle) || isArray(params.handle)
+      ? null
+      : params.handle;
+  if (isNull(handle)) {
+    return {
+      props: {
+        spaceId: null,
+        fid: null,
+        handle: isUndefined(params) ? params : params.handle,
+      },
+    };
+  }
+
+  try {
+    const {
+      result: { user },
+    } = await neynar.lookupUserByUsername(handle);
+
+    const { data } = await supabaseClient
+      .from("spaceRegistrations")
+      .select("spaceId")
+      .eq("fid", user.fid)
+      .eq("isDefault", true);
+
+    if (data) {
+      const spaceRegistration = first(data);
+      if (!isUndefined(spaceRegistration)) {
+        return {
+          props: {
+            spaceId: spaceRegistration.spaceId,
+            fid: user.fid,
+            handle,
+          },
+        };
+      }
+    }
+
+    return {
+      props: {
+        spaceId: null,
+        fid: user.fid,
+        handle,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        spaceId: null,
+        fid: null,
+        handle,
+      },
+    };
+  }
+}) satisfies GetServerSideProps<SpacePageProps>;
+
+const UserPrimarySpace: NextPageWithLayout = async ({
+  handle,
+  spaceId,
+  fid,
+}: SpacePageProps) => {
+  const { loadEditableSpaces } = useAppStore((state) => ({
+    loadEditableSpaces: state.space.loadEditableSpaces,
+  }));
+
+  useEffect(() => {
+    loadEditableSpaces();
+  }, []);
+
+  if (!isNil(fid)) {
+    return <UserDefinedSpace fid={fid} spaceId={spaceId} />;
+  }
+
+  return <SpaceNotFound handle={handle} />;
+};
+
+UserPrimarySpace.getLayout = (page: React.ReactElement) => {
+  return (
+    <LoggedInStateManager>
+      <div
+        className="min-h-screen max-w-screen h-screen w-screen"
+        style={{ background: "var(--user-theme-background)" }}
+      >
+        {page}
+      </div>
+    </LoggedInStateManager>
+  );
+};
+
+export default UserPrimarySpace;
