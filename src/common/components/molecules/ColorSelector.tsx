9533721b40e6d72d05844b942318f139130d0a2d
Gleb Surinov <glebovsky@gmail.com>
2024-06-26 22:48:40 +0600

    Style theme settings editor (#114)

--- src/common/components/molecules/ColorSelector.tsx
+++ src/common/components/molecules/ColorSelector.tsx
@@ -7,15 +7,20 @@ import {
 import { Button } from "@/common/components/atoms/button";
 import { Color } from "@/common/lib/theme";
 import ColorPicker from "react-best-gradient-color-picker";
+import { mergeClasses } from "@/common/lib/utils/mergeClasses";
 
 export type ColorSelectorProps = {
   value: Color;
   onChange: (value: Color) => void;
+  className?: string;
+  innerClassName?: string;
 };
 
 export const ColorSelector: React.FC<ColorSelectorProps> = ({
   value,
   onChange,
+  className,
+  innerClassName,
 }) => {
   const [isEditing, setIsEditing] = useState(false);
 
@@ -31,16 +36,25 @@ export const ColorSelector: React.FC<ColorSelectorProps> = ({
       <PopoverTrigger asChild>
         <Button
           variant="outline"
-          className="p-0.5 h-9 w-9 flex flex-shrink-0 items-stretch border cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none"
+          className={mergeClasses(
+            "p-0.5 h-9 w-9 flex flex-shrink-0 items-stretch border cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none",
+            className,
+          )}
         >
           <div
-            className="flex-1 rounded-md"
+            className={mergeClasses("flex-1 rounded-md", innerClassName)}
             style={{ backgroundColor: value }}
           />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-2" align="start">
-        <ColorPicker value={value} onChange={_onChange} />
+        <ColorPicker
+          value={value}
+          onChange={_onChange}
+          width={250}
+          height={150}
+          hideColorGuide
+        />
       </PopoverContent>
     </Popover>
   );
