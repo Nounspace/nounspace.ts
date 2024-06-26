116b712a376e55b64f0d40162b58b2d5b69c6971
Jesse Paterson <jesse.r.pat@gmail.com>
2024-06-25 23:36:26 -0500

    additional proportion fix for buttons in settings

--- src/common/components/organisms/FidgetSettingsEditor.tsx
+++ src/common/components/organisms/FidgetSettingsEditor.tsx
@@ -68,84 +68,89 @@ export const FidgetSettingsEditor: React.FC<FidgetSettingsEditorProps> = ({
   };
 
   return (
-    <form onSubmit={_onSave}>
-      <div className="flex pb-4 m-2">
-        <button onClick={unselect} className="my-auto">
-          <BackArrowIcon />
-        </button>
-        <h1 className="capitalize text-lg pl-4">
-          Edit {properties.fidgetName} Fidget
-        </h1>
+    <form onSubmit={_onSave} className="flex-col flex h-full">
+      <div className="h-5/6">
+        <div className="flex pb-4 m-2">
+          <button onClick={unselect} className="my-auto">
+            <BackArrowIcon />
+          </button>
+          <h1 className="capitalize text-lg pl-4">
+            Edit {properties.fidgetName} Fidget
+          </h1>
+        </div>
+        <div className="gap-3 flex flex-col">
+          {properties.fields.map((field, i) => (
+            <FidgetSettingsRow
+              field={field}
+              key={i}
+              value={state[field.fieldName]}
+              onChange={(val) => {
+                setState({
+                  ...state,
+                  [field.fieldName]: val,
+                });
+              }}
+            />
+          ))}
+        </div>
       </div>
-      <div className="gap-3 flex flex-col">
-        {properties.fields.map((field, i) => (
-          <FidgetSettingsRow
-            field={field}
-            key={i}
-            value={state[field.fieldName]}
-            onChange={(val) => {
-              setState({
-                ...state,
-                [field.fieldName]: val,
-              });
-            }}
-          />
-        ))}
-      </div>
-      {showConfirmCancel ? (
-        // Back Button and Exit Button (shows second)
-        <>
-          <div className="mt-20 pt-2 flex items-center justify-center">
+
+      <div className="h-1/6">
+        {showConfirmCancel ? (
+          // Back Button and Exit Button (shows second)
+          <>
+            <div className="pt-2 flex items-center justify-center">
+              <button
+                onClick={() => setShowConfirmCancel(false)}
+                className="flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-sky-100 text-[#1C64F2]"
+              >
+                <div className="flex items-center">
+                  <BackArrowIcon />
+                </div>
+              </button>
+              <button
+                onClick={() => {
+                  removeFidget(fidgetId);
+                }}
+                className="ml-4 flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-red-100 text-[#1C64F2] font-semibold"
+              >
+                <div className="ml-4 flex items-center">
+                  <FaTriangleExclamation
+                    className="h-8l shrink-0"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-4 mr-4">Delete</span>
+                </div>
+              </button>
+            </div>
+            <p className="w-full text-center text-xs pt-4 pl-16 pr-16">
+              This cannot be undone.
+            </p>
+          </>
+        ) : (
+          // X Button and Save Button (shows first)
+          <div className="pt-2 flex items-center justify-center">
             <button
-              onClick={() => setShowConfirmCancel(false)}
-              className="flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-sky-100 text-[#1C64F2]"
+              onClick={() => setShowConfirmCancel(true)}
+              className="flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-red-100 text-[#1C64F2]"
             >
-              <div className="flex items-center">
-                <BackArrowIcon />
+              <div className="flex items-center p-1">
+                <FaTrashCan className="h-8l shrink-0" aria-hidden="true" />
               </div>
             </button>
+
             <button
-              onClick={() => {
-                removeFidget(fidgetId);
-              }}
-              className="ml-4 flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-red-100 text-[#1C64F2] font-semibold"
+              type="submit"
+              className="ml-4 flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-sky-100 text-[#1C64F2] font-semibold"
             >
               <div className="ml-4 flex items-center">
-                <FaTriangleExclamation
-                  className="h-8l shrink-0"
-                  aria-hidden="true"
-                />
-                <span className="ml-4 mr-4">Delete</span>
+                <FaFloppyDisk className="h-8l shrink-0" aria-hidden="true" />
+                <span className="ml-4 mr-4">Save</span>
               </div>
             </button>
           </div>
-          <p className="w-full text-center text-xs pt-4 pl-16 pr-16">
-            This cannot be undone.
-          </p>
-        </>
-      ) : (
-        // X Button and Save Button (shows first)
-        <div className="mt-20 pt-2 flex items-center justify-center">
-          <button
-            onClick={() => setShowConfirmCancel(true)}
-            className="flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-red-100 text-[#1C64F2]"
-          >
-            <div className="flex items-center p-1">
-              <FaTrashCan className="h-8l shrink-0" aria-hidden="true" />
-            </div>
-          </button>
-
-          <button
-            type="submit"
-            className="ml-4 flex rounded-xl p-2 px-auto bg-[#F3F4F6] hover:bg-sky-100 text-[#1C64F2] font-semibold"
-          >
-            <div className="ml-4 flex items-center">
-              <FaFloppyDisk className="h-8l shrink-0" aria-hidden="true" />
-              <span className="ml-4 mr-4">Save</span>
-            </div>
-          </button>
-        </div>
-      )}
+        )}
+      </div>
     </form>
   );
 };
