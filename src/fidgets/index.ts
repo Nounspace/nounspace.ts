f22ab8426ece562a0086f8522995d2f63003684a
Julian Early <julian.is.early@gmail.com>
2024-06-27 00:02:35 -0700

    feat: add cast to CompleteFidgets

--- src/fidgets/index.ts
+++ src/fidgets/index.ts
@@ -1,5 +1,4 @@
 // TO DO: Figure out how to do this importing dynamically
-import { LayoutFidget } from "@/common/fidgets";
 import Example from "./example";
 import Frame from "./farcaster/Frame";
 import Gallery from "./ui/gallery";
@@ -7,19 +6,23 @@ import TextFidget from "./ui/Text";
 import IFrame from "./ui/IFrame";
 import Grid from "./layout/Grid";
 import NounishGovernance from "./community/nouns-dao/NounishGovernance";
+import Cast from "./farcaster/Cast";
 
 export const CompleteFidgets = {
+  //
   example:
     process.env.NEXT_PUBLIC_VERCEL_ENV === "development" ? Example : undefined,
+  // Farcaster
   frame: Frame,
+  cast: Cast,
+  // Basic UI elements
   gallery: Gallery,
   text: TextFidget,
-  governance: NounishGovernance,
   iframe: IFrame,
+  // Nouns
+  governance: NounishGovernance,
 };
 
 export const LayoutFidgets = {
   grid: Grid,
-} as {
-  [key: string]: LayoutFidget<any>;
 };
