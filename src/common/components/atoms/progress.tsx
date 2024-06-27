69dc2eb925fc709dba2339cec57eb2d4d8a9d1b5
Nounspace Ryan <ryan@nounspace.com>
2024-06-26 20:18:23 -0500

    feat: governance fidget improvements

--- src/common/components/atoms/progress.tsx
+++ src/common/components/atoms/progress.tsx
@@ -4,31 +4,33 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { mergeClasses } from "@/common/lib/utils/mergeClasses";
 
 type CustomProgressProps = {
-  indicatorClassName?: string;
+  indicatorStyles?: object;
 };
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> &
     CustomProgressProps
->(({ className, indicatorClassName, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={mergeClasses(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className,
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
+>(({ className, indicatorStyles, value, ...props }, ref) => {
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
       className={mergeClasses(
-        "h-full w-full flex-1 bg-primary transition-all",
-        indicatorClassName,
+        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+        className,
       )}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className="h-full w-full flex-1 bg-primary transition-all"
+        style={{
+          transform: `translateX(-${100 - (value || 0)}%)`,
+          ...(indicatorStyles || {}),
+        }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
