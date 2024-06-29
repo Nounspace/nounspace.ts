40d59e2fc399fc39498b93cc13ae461b367dd136
Jesse Paterson <jesse.r.pat@gmail.com>
2024-06-29 14:20:03 -0500

    clean up loading screen fidget

--- src/common/components/organisms/LoadingScreen.tsx
+++ src/common/components/organisms/LoadingScreen.tsx
@@ -7,14 +7,9 @@ const LoadingScreen: React.FC<{ text?: string }> = ({
   text?: string;
 }) => {
   return (
-    <div className="w-full max-w-full min-h-screen">
-      <div className="relative w-full h-screen flex-col items-center grid lg:max-w-none lg:grid-cols lg:px-0">
-        <div className="relative h-full flex-col bg-muted p-10 text-foreground flex">
-          <div className="absolute inset-0 bg-gradient-to-t lg:bg-gradient-to-l from-gray-900 via-gray-700 to-stone-500" />
-          <div className="relative z-1 mt-16 lg:mt-24">
-            <Loading text={text} />
-          </div>
-        </div>
+    <div className="relative h-full flex-col p-10 text-foreground flex">
+      <div className="relative z-1 mt-16 lg:mt-24 lg:mb-24">
+        <Loading text={text} />
       </div>
     </div>
   );
