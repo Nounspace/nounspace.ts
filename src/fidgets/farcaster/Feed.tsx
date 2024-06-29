657ab9555f12d60d24a7384353773a013a5419cd
Jesse Paterson <jesse.r.pat@gmail.com>
2024-06-28 19:24:28 -0500

    switched over to observer

--- src/fidgets/farcaster/Feed.tsx
+++ src/fidgets/farcaster/Feed.tsx
@@ -13,6 +13,7 @@ import { map } from "lodash";
 import { CastRow } from "./components/CastRow";
 import { useFarcasterSigner } from ".";
 import Loading from "@/common/components/molecules/Loading";
+import { useInView } from "react-intersection-observer";
 
 export type feedFidgetSettings = {
   feedType: FeedType;
@@ -69,8 +70,14 @@ const Feed: React.FC<FidgetArgs<feedFidgetSettings>> = ({
   settings: { feedType },
 }) => {
   const { fid } = useFarcasterSigner("feed");
-  const { fetchNextPage, hasNextPage, data, isLoading, error, isFetching } =
-    useGetCasts(feedType, fid);
+  const {
+    data,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    isError,
+    error,
+  } = useGetCasts(feedType, fid);
 
   // TO DO: trigger new page loading as inifite scroll
   // TO DO: Handle respons errors
@@ -79,57 +86,43 @@ const Feed: React.FC<FidgetArgs<feedFidgetSettings>> = ({
   // https://tanstack.com/query/latest/docs/framework/react/reference/useInfiniteQuery
   // https://www.radix-ui.com/primitives/docs/components/scroll-area
 
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const [loadedNextSection, setLoadedNextSection] = useState(false);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollableSection = scrollRef.current;
-      if (scrollableSection !== null) {
-        if (
-          scrollableSection.scrollTop + scrollableSection.clientHeight >=
-          scrollableSection.scrollHeight - 10
-        ) {
-          if (!isFetching && hasNextPage) {
-            fetchNextPage();
-          }
-        }
-      }
-    };
-
-    const scrollable = scrollRef.current;
-    if (scrollable) {
-      scrollable.addEventListener("scroll", handleScroll);
+    if (inView && hasNextPage) {
+      fetchNextPage();
     }
-
-    // Cleanup event listener on component unmount
-    return () => {
-      if (scrollable) {
-        scrollable.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
+  }, [inView]);
 
   return (
-    <div
-      className="h-full overflow-scroll justify-center items-center"
-      ref={scrollRef}
-    >
-      {isLoading ? (
-        <div className="bg-[#E6E6E6] h-full w-full flex flex-col justify-center items-center">
-          <Loading />
-        </div>
-      ) : (
-        <ScrollArea>
-          {map(data?.pages, (page) => {
-            return map(page.casts, (cast) => {
-              return <CastRow cast={cast} />;
-            });
-          })}
-        </ScrollArea>
-      )}
-      <div className="bg-[#E6E6E6] h-1/6 w-full flex flex-col">
-        <Loading />
+    <div className="h-full overflow-scroll justify-center items-center">
+      <div>
+        {/* data.pages: Page[] */}
+        {isError ? (
+          <div>{"Error"}</div>
+        ) : (
+          data?.pages.map((page, pageNum) => (
+            <React.Fragment key={pageNum}>
+              {page.casts.map((cast, index) => (
+                <CastRow cast={cast} key={index} />
+              ))}
+            </React.Fragment>
+          ))
+        )}
+      </div>
+      {/* Pagination Controls */}
+      <div ref={ref} className="bg-[#E6E6E6]">
+        {isFetchingNextPage ? (
+          <div className="bg-[#E6E6E6] h-3/6 w-full flex flex-col justify-center items-center">
+            <Loading />
+          </div>
+        ) : hasNextPage ? (
+          "Fetch More Data"
+        ) : (
+          <div className="bg-[#E6E6E6] h-full w-full flex flex-col justify-center items-center">
+            <Loading />
+          </div>
+        )}
       </div>
     </div>
   );
