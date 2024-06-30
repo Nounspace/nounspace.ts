77c007b119d661948dc1224da8de3cbedb86e0ec
Gleb Surinov <glebovsky@gmail.com>
2024-07-01 05:14:52 +0600

    Fidget styles (#138)

--- src/common/lib/theme/defaultTheme.ts
+++ src/common/lib/theme/defaultTheme.ts
@@ -11,6 +11,10 @@ export const defaultUserTheme: UserTheme = {
     background: "#ffffff",
     backgroundHTML: "",
     musicURL: "https://www.youtube.com/watch?v=dMXlZ4y7OK4&t=1804",
+    fidgetBackground: "#ffffff",
+    fidgetBorderWidth: "1px",
+    fidgetBorderColor: "#eeeeee",
+    fidgetShadow: "none",
   },
 };
 
