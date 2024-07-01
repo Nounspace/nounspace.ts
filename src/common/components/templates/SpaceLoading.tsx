74e241d6f837d9933a6226b03eda0ec5c8fc2a5e
Julian Early <julian.is.early@gmail.com>
2024-07-01 13:28:11 -0700

    fix: move sidebar up a layer to support middle state in loggin flow (#167)

--- src/common/components/templates/SpaceLoading.tsx
+++ src/common/components/templates/SpaceLoading.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode, useMemo } from "react";
-import LoadingSidebar from "../organisms/LoadingSidebar";
 import { isUndefined } from "lodash";
 import useWindowSize from "@/common/lib/hooks/useWindowSize";
 
@@ -23,38 +22,33 @@ export default function SpaceLoading({ profile }: { profile?: ReactNode }) {
 
   return (
     <>
-      <div className="flex w-full h-full">
-        <div className="w-3/12 flex mx-auto transition-all duration-100 ease-out max-w-96">
-          <LoadingSidebar />
-        </div>
-        <div className="w-full transition-all duration-100 ease-out h-full">
-          <div className={"h-full flex flex-col"}>
-            {!isUndefined(profile) ? <div> {profile} </div> : null}
-            <div className="flex-1 grid-container grow">
-              <div
-                className="relative grid-overlap w-full h-full opacity-50"
-                style={{
-                  transition: "background-color 1000ms linear",
-                  display: "grid",
-                  gridTemplateColumns: `repeat(${cols}, 1fr)`,
-                  gridTemplateRows: `repeat(${maxRows}, ${rowHeight}px)`,
-                  gridGap: `${margin[0]}px`,
-                  rowGap: `${margin[1]}px`,
-                  padding: `${containerPadding[0]}px`,
-                  background: "rgba(200, 227, 248, 0.3)",
-                }}
-              >
-                {[...Array(cols * maxRows)].map((_, i) => (
-                  <div
-                    className="rounded-lg"
-                    key={i}
-                    style={{
-                      backgroundColor: "rgba(200, 227, 248, 0.5)",
-                      outline: "1px dashed rgba(200, 227, 248, 0.8)",
-                    }}
-                  />
-                ))}
-              </div>
+      <div className="w-full transition-all duration-100 ease-out h-full">
+        <div className={"h-full flex flex-col"}>
+          {!isUndefined(profile) ? <div> {profile} </div> : null}
+          <div className="flex-1 grid-container grow">
+            <div
+              className="relative grid-overlap w-full h-full opacity-50"
+              style={{
+                transition: "background-color 1000ms linear",
+                display: "grid",
+                gridTemplateColumns: `repeat(${cols}, 1fr)`,
+                gridTemplateRows: `repeat(${maxRows}, ${rowHeight}px)`,
+                gridGap: `${margin[0]}px`,
+                rowGap: `${margin[1]}px`,
+                padding: `${containerPadding[0]}px`,
+                background: "rgba(200, 227, 248, 0.3)",
+              }}
+            >
+              {[...Array(cols * maxRows)].map((_, i) => (
+                <div
+                  className="rounded-lg"
+                  key={i}
+                  style={{
+                    backgroundColor: "rgba(200, 227, 248, 0.5)",
+                    outline: "1px dashed rgba(200, 227, 248, 0.8)",
+                  }}
+                />
+              ))}
             </div>
           </div>
         </div>
