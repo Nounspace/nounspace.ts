58cb70cfecc1a60c7913cf13211809cbc7c1fd7b
Jesse Paterson <jesse.r.pat@gmail.com>
2024-07-02 14:17:25 -0500

    adds an initial feed to the homebase

--- src/constants/intialHomebase.ts
+++ src/constants/intialHomebase.ts
@@ -1,18 +1,49 @@
 import { SpaceConfig } from "@/common/components/templates/Space";
 import { LayoutFidgetConfig, LayoutFidgetDetails } from "@/common/fidgets";
 import DEFAULT_THEME from "@/common/lib/theme/defaultTheme";
+import { FeedType, FilterType } from "@neynar/nodejs-sdk";
 
 const layoutID = "";
 const layoutDetails: LayoutFidgetDetails<LayoutFidgetConfig<any[]>> = {
   layoutConfig: { layout: [] },
   layoutFidget: "grid",
 };
-
 const INITIAL_HOMEBASE_CONFIG: SpaceConfig = {
   layoutID,
-  layoutDetails,
+  layoutDetails: {
+    layoutConfig: {
+      layout: [
+        {
+          w: 5,
+          h: 8,
+          x: 0,
+          y: 0,
+          i: "feed:profile",
+          minW: 4,
+          maxW: 36,
+          minH: 6,
+          maxH: 36,
+          moved: false,
+          static: false,
+        },
+      ],
+    },
+    layoutFidget: "grid",
+  },
   theme: DEFAULT_THEME,
-  fidgetInstanceDatums: {},
+  fidgetInstanceDatums: {
+    "feed:profile": {
+      config: {
+        editable: false,
+        settings: {
+          feedType: FeedType.Following,
+        },
+        data: {},
+      },
+      fidgetType: "feed",
+      id: "feed:profile",
+    },
+  },
   isEditable: true,
   fidgetTrayContents: [],
 };
