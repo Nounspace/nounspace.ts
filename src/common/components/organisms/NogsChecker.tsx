43a3f78e759201f83188c320a045d914b662fc18
Gleb Surinov <glebovsky@gmail.com>
2024-07-02 13:18:42 +0600

    No nogs screen update (#189)

--- src/common/components/organisms/NogsChecker.tsx
+++ src/common/components/organisms/NogsChecker.tsx
@@ -4,17 +4,14 @@ import React from "react";
 import { Button } from "../atoms/button";
 
 export default function NogsChecker() {
-  const {
-    setRecheckTimerLength,
-    setShouldRecheck,
-    recheckCountDown,
-    isChecking,
-  } = useAppStore((state) => ({
-    setRecheckTimerLength: state.setup.setNogsRecheckTimerLength,
-    setShouldRecheck: state.setup.setNogsShouldRecheck,
-    isChecking: state.setup.nogsIsChecking,
-    recheckCountDown: state.setup.nogsRecheckCountDown,
-  }));
+  const { setRecheckTimerLength, setShouldRecheck, isChecking } = useAppStore(
+    (state) => ({
+      setRecheckTimerLength: state.setup.setNogsRecheckTimerLength,
+      setShouldRecheck: state.setup.setNogsShouldRecheck,
+      isChecking: state.setup.nogsIsChecking,
+      recheckCountDown: state.setup.nogsRecheckCountDown,
+    }),
+  );
 
   async function userTriggeredRecheck() {
     setRecheckTimerLength(RECHECK_INITIAL_TIME);
@@ -24,11 +21,32 @@ export default function NogsChecker() {
   // TO DO: Add place to mint nOGs here
   return (
     <>
-      Waiting to see if you have gotten nOG&apos;d... Checking again in{" "}
-      {recheckCountDown} seconds
+      <p className="mb-2">
+        To customize a Space and Homebase, you must hold a nOGs NFT. Mint a pair{" "}
+        <a
+          href="https://highlight.xyz/mint/663d2717dffb7b3a490f398f"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="cursor-pointer text-blue-500 text-font-medium hover:underline hover:text-blue-500/70"
+        >
+          here
+        </a>
+        , then try again.
+      </p>
       <Button disabled={isChecking} onClick={userTriggeredRecheck}>
         {isChecking ? "Checking if you have nOGs" : "Check now?"}
       </Button>
+
+      <div
+        className="mt-2"
+        dangerouslySetInnerHTML={{
+          __html: `
+        <link rel="stylesheet" href="https://mint.highlight.xyz/assets/embed.css" />
+        <div data-widget="highlight-mint-card" data-mint-collection-id="663d2717dffb7b3a490f398f"></div>
+        <script type="module" crossorigin="true" src="https://mint.highlight.xyz/assets/embed.js?v=1"></script>
+      `,
+        }}
+      />
     </>
   );
 }
