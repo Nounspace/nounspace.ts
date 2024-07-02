7caf6ddcddd612dfbcedb8194e026d4dda12d8c5
Gleb Surinov <glebovsky@gmail.com>
2024-07-02 12:10:02 +0600

    Nogs checker screen update (mint card)

--- src/common/components/organisms/NogsChecker.tsx
+++ src/common/components/organisms/NogsChecker.tsx
@@ -24,11 +24,35 @@ export default function NogsChecker() {
   // TO DO: Add place to mint nOGs here
   return (
     <>
-      Waiting to see if you have gotten nOG&apos;d... Checking again in{" "}
-      {recheckCountDown} seconds
+      <p>
+        Waiting to see if you have gotten nOG&apos;d... Checking again in{" "}
+        {recheckCountDown} seconds.
+      </p>
+      <p className="mb-1">
+        To customize a Space and Homebase, you must hold a nOGs NFT. Mint a pair{" "}
+        <a
+          href="https://highlight.xyz/mint/663d2717dffb7b3a490f398f"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          here
+        </a>
+        , then try again.
+      </p>
       <Button disabled={isChecking} onClick={userTriggeredRecheck}>
         {isChecking ? "Checking if you have nOGs" : "Check now?"}
       </Button>
+
+      <div
+        className="mt-2"
+        dangerouslySetInnerHTML={{
+          __html: `
+        <link rel="stylesheet" href="https://mint.highlight.xyz/assets/embed.css" />
+        <div data-widget="highlight-mint-card" data-mint-collection-id="663d2717dffb7b3a490f398f"></div>
+        <script type="module" crossorigin="true" src="https://mint.highlight.xyz/assets/embed.js?v=1"></script>
+      `,
+        }}
+      />
     </>
   );
 }
