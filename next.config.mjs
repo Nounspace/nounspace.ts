2a4772823aa0f2e620a985bc9ac543568c518b46
Willy Ogorzaly <willy@ogorzaly.com>
2024-07-01 23:37:03 -0500

    Add CSP
    Add basic CSP before going to prod. note that this still allows iframes to load from any site that allows it on their end

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,22 @@ const withBundleAnalyzer = bundlerAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com https://www.youtube.com/iframe_api https://auth.privy.nounspace.com;
+    style-src 'self' 'unsafe-inline' https://i.ytimg.com;
+    img-src 'self' blob: data: https: https://i.ytimg.com;
+    font-src 'self' https:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'self';
+    frame-src 'self' https://auth.privy.nounspace.com https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com https://www.youtube.com https://*;
+    child-src 'self' https://auth.privy.nounspace.com https://verify.walletconnect.com https://verify.walletconnect.org https://www.youtube.com https://*;
+    connect-src 'self' https://auth.privy.nounspace.com wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems https://auth.privy.io https://auth.privy.io/api/v1/apps/clw9qpfkl01nnpox6rcsb5wy3 wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://*.rpc.privy.systems;
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA).
@@ -36,6 +52,19 @@ const nextConfig = {
         source: '/signatures',
         destination: 'https://docs.nounspace.com/nounspace-alpha/accounts/signatures',
         permanent: true,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
       },
     ]
   },
