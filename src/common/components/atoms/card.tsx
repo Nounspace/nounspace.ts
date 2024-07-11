5a0e7c3fc4c24a7a127a4b638a560923072799b6
Julian Early <julian.is.early@gmail.com>
2024-07-11 13:23:13 -0700

    Fix assorted small errors (#245)

--- src/common/components/atoms/card.tsx
+++ src/common/components/atoms/card.tsx
@@ -48,7 +48,7 @@ const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <p
+  <div
     ref={ref}
     className={mergeClasses("text-sm text-muted-foreground", className)}
     {...props}
