c6c8169531dcc6a06c5037a9c52296d37522150e
nounspaceryan <ryan@nounspace.com>
2024-07-29 15:27:16 -0500

    User Search (#284)

---
+++ src/common/components/organisms/SearchModal.tsx
@@ -0,0 +1,50 @@
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useImperativeHandle,
+} from "react";
+import { Dialog, DialogContent } from "@/common/components/atoms/dialog";
+
+import SearchAutocompleteInput from "@/common/components/organisms/SearchAutocompleteInput";
+
+const SearchModal = React.forwardRef((props, ref) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    focus: handleFocus,
+  }));
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        setIsFocused((open) => !open);
+      }
+      if (e.key === "Escape") {
+        setIsFocused(false);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <Dialog open={isFocused} onOpenChange={setIsFocused}>
+      <DialogContent className="p-0 border-none">
+        <SearchAutocompleteInput onSelect={handleBlur} />
+      </DialogContent>
+    </Dialog>
+  );
+});
+
+SearchModal.displayName = "SearchModal";
+
+export default SearchModal;
