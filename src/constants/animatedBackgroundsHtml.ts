cc6b976ed05080009a2272df03940d221933e578
Willy Ogorzaly <willy@ogorzaly.com>
2024-08-12 14:21:40 -0500

    Update Square Grid animated background code
    Replaced CSS variables with constants and reformatted

--- src/constants/animatedBackgroundsHtml.ts
+++ src/constants/animatedBackgroundsHtml.ts
@@ -71,7 +71,74 @@ body {
 </style>
 `;
 
-export const squareGrid = `<html lang="en"><head><style>html{--s:100px;--c1:#C3CCAF;--c2:#67434F;--_s:calc(2*var(--s)) calc(2*var(--s));--_g:var(--_s) conic-gradient(at 40% 40%,#0000 75%,var(--c1) 0);--_p:var(--_s) conic-gradient(at 20% 20%,#0000 75%,var(--c2) 0);background:calc(.9*var(--s)) calc(.9*var(--s))/var(--_p),calc(-.1*var(--s)) calc(-.1*var(--s))/var(--_p),calc(.7*var(--s)) calc(.7*var(--s))/var(--_g),calc(-.3*var(--s)) calc(-.3*var(--s))/var(--_g),conic-gradient(from 90deg at 20% 20%,var(--c2) 25%,var(--c1) 0) 0 0/var(--s) var(--s);animation:m 3s infinite}@keyframes m{0%{background-position:calc(.9*var(--s)) calc(.9*var(--s)),calc(-.1*var(--s)) calc(-.1*var(--s)),calc(.7*var(--s)) calc(.7*var(--s)),calc(-.3*var(--s)) calc(-.3*var(--s)),0 0}25%{background-position:calc(1.9*var(--s)) calc(.9*var(--s)),calc(-1.1*var(--s)) calc(-.1*var(--s)),calc(1.7*var(--s)) calc(.7*var(--s)),calc(-1.3*var(--s)) calc(-.3*var(--s)),0 0}50%{background-position:calc(1.9*var(--s)) calc(-.1*var(--s)),calc(-1.1*var(--s)) calc(.9*var(--s)),calc(1.7*var(--s)) calc(-.3*var(--s)),calc(-1.3*var(--s)) calc(.7*var(--s)),0 0}75%{background-position:calc(2.9*var(--s)) calc(-.1*var(--s)),calc(-2.1*var(--s)) calc(.9*var(--s)),calc(2.7*var(--s)) calc(-.3*var(--s)),calc(-2.3*var(--s)) calc(.7*var(--s)),0 0}100%{background-position:calc(2.9*var(--s)) calc(-1.1*var(--s)),calc(-2.1*var(--s)) calc(1.9*var(--s)),calc(2.7*var(--s)) calc(-1.3*var(--s)),calc(-2.3*var(--s)) calc(1.7*var(--s)),0 0}}</style></head></body></html>`;
+export const squareGrid = `
+<html lang="en">
+<head>
+<style>
+html {
+  --s: 100px;
+  --c1: #C3CCAF;
+  --c2: #67434F;
+  --_s: calc(2 * 100px) calc(2 * 100px);
+  --_g: calc(2 * 100px) calc(2 * 100px) conic-gradient(at 40% 40%, #0000 75%, #C3CCAF 0);
+  --_p: calc(2 * 100px) calc(2 * 100px) conic-gradient(at 20% 20%, #0000 75%, #67434F 0);
+  background:
+    calc(0.9 * 100px) calc(0.9 * 100px) / var(--_p),
+    calc(-0.1 * 100px) calc(-0.1 * 100px) / var(--_p),
+    calc(0.7 * 100px) calc(0.7 * 100px) / var(--_g),
+    calc(-0.3 * 100px) calc(-0.3 * 100px) / var(--_g),
+    conic-gradient(from 90deg at 20% 20%, #67434F 25%, #C3CCAF 0) 0 0 / 100px 100px;
+  animation: m 3s infinite;
+}
+
+@keyframes m {
+  0% {
+    background-position:
+      calc(0.9 * 100px) calc(0.9 * 100px),
+      calc(-0.1 * 100px) calc(-0.1 * 100px),
+      calc(0.7 * 100px) calc(0.7 * 100px),
+      calc(-0.3 * 100px) calc(-0.3 * 100px),
+      0 0;
+  }
+  25% {
+    background-position:
+      calc(1.9 * 100px) calc(0.9 * 100px),
+      calc(-1.1 * 100px) calc(-0.1 * 100px),
+      calc(1.7 * 100px) calc(0.7 * 100px),
+      calc(-1.3 * 100px) calc(-0.3 * 100px),
+      0 0;
+  }
+  50% {
+    background-position:
+      calc(1.9 * 100px) calc(-0.1 * 100px),
+      calc(-1.1 * 100px) calc(0.9 * 100px),
+      calc(1.7 * 100px) calc(-0.3 * 100px),
+      calc(-1.3 * 100px) calc(0.7 * 100px),
+      0 0;
+  }
+  75% {
+    background-position:
+      calc(2.9 * 100px) calc(-0.1 * 100px),
+      calc(-2.1 * 100px) calc(0.9 * 100px),
+      calc(2.7 * 100px) calc(-0.3 * 100px),
+      calc(-2.3 * 100px) calc(0.7 * 100px),
+      0 0;
+  }
+  100% {
+    background-position:
+      calc(2.9 * 100px) calc(-1.1 * 100px),
+      calc(-2.1 * 100px) calc(1.9 * 100px),
+      calc(2.7 * 100px) calc(-1.3 * 100px),
+      calc(-2.3 * 100px) calc(1.7 * 100px),
+      0 0;
+  }
+}
+</style>
+d>
+<body>
+</body>
+>
+`;
 
 export const gradientAndWave = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Waves Animation</title><style>body{margin:auto;font-family:-apple-system,BlinkMacSystemFont,sans-serif;overflow:auto;background:linear-gradient(315deg,rgba(101,0,94,1) 3%,rgba(60,132,206,1) 38%,rgba(48,238,226,1) 68%,rgba(255,25,25,1) 98%);animation:gradient 15s ease infinite;background-size:400% 400%;background-attachment:fixed}@keyframes gradient{0%{background-position:0 0}50%{background-position:100% 100%}100%{background-position:0 0}}.wave{background:rgb(255 255 255 / 25%);border-radius:1000% 1000% 0 0;position:fixed;width:200%;height:12em;animation:wave 10s -3s linear infinite;transform:translate3d(0,0,0);opacity:.8;bottom:0;left:0;z-index:-1}.wave:nth-of-type(2){bottom:-1.25em;animation:wave 18s linear reverse infinite;opacity:.8}.wave:nth-of-type(3){bottom:-2.5em;animation:wave 20s -1s reverse infinite;opacity:.9}@keyframes wave{2%{transform:translateX(1)}25%{transform:translateX(-25%)}50%{transform:translateX(-50%)}75%{transform:translateX(-25%)}100%{transform:translateX(1)}}</style></head><body><div><div class="wave"></div><div class="wave"></div><div class="wave"></div></div></body></html>`;
 
