d33b8b81df4a241aea7e0efba8822fb0c4970cf4
Jesse Paterson <jesse.r.pat@gmail.com>
2024-08-12 00:41:06 -0700

    editable text

---
+++ src/common/components/atoms/editable-text.tsx
@@ -0,0 +1,41 @@
+import _ from "lodash";
+import { useState } from "react";
+
+const inputValue = (e: any): string => e.target.value;
+
+function isEnterOrEscapeKeyEvent(event: React.KeyboardEvent<HTMLInputElement>) {
+  return event.key === "Enter" || event.key === "Escape";
+}
+
+const EditableText = ({ initialText, updateMethod }) => {
+  const [isEditing, setisEditing] = useState(false);
+  const [text, settext] = useState(initialText);
+
+  const onEditEnd = () => {
+    setisEditing(false);
+    updateMethod(text);
+  };
+
+  return isEditing ? (
+    <input
+      value={text}
+      className="bg-transparent border-2 border-black border-solid"
+      onKeyDown={(event) => {
+        if (isEnterOrEscapeKeyEvent(event)) {
+          event.preventDefault();
+          event.stopPropagation();
+          onEditEnd();
+        }
+      }}
+      onChange={_.flow(inputValue, settext)}
+      onBlur={onEditEnd}
+      autoFocus
+    />
+  ) : (
+    <div className="select-none" onDoubleClick={() => setisEditing(true)}>
+      {text}
+    </div>
+  );
+};
+
+export default EditableText;
