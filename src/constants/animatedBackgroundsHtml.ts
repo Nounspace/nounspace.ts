9de1d5795c58ee3e1aa5869ebf340b56986b1aae
Willy Ogorzaly <willy@ogorzaly.com>
2024-08-12 22:12:16 -0500

    Fix typo in gradient wave animated BG

--- src/constants/animatedBackgroundsHtml.ts
+++ src/constants/animatedBackgroundsHtml.ts
@@ -137,11 +137,11 @@ html {
 `;
 
 export const gradientAndWave = `
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Waves Animation</title>
     <style>
         body {
@@ -153,13 +153,17 @@ export const gradientAndWave = `
             background-size: 400% 400%;
             background-attachment: fixed;
         }
-
         @keyframes gradient {
-            0% { background-position: 0 0; }
-            50% { background-position: 100% 100%; }
-            100% { background-position: 0 0; }
+            0% {
+                background-position: 0% 0%;
+            }
+            50% {
+                background-position: 100% 100%;
+            }
+            100% {
+                background-position: 0% 0%;
+            }
         }
-
         .wave {
             background: rgb(255 255 255 / 25%);
             border-radius: 1000% 1000% 0 0;
@@ -173,32 +177,40 @@ export const gradientAndWave = `
             left: 0;
             z-index: -1;
         }
-
         .wave:nth-of-type(2) {
             bottom: -1.25em;
             animation: wave 18s linear reverse infinite;
             opacity: 0.8;
         }
-
         .wave:nth-of-type(3) {
             bottom: -2.5em;
             animation: wave 20s -1s reverse infinite;
             opacity: 0.9;
         }
-
         @keyframes wave {
-            2% { transform: translateX(1); }
-            25% { transform: translateX(-25%); }
-            50% { transform: translateX(-50%); }
-            75% { transform: translateX(-25%); }
-            100% { transform: translateX(1); }
+            2% {
+                transform: translateX(1);
+            }
+            25% {
+                transform: translateX(-25%);
+            }
+            50% {
+                transform: translateX(-50%);
+            }
+            75% {
+                transform: translateX(-25%);
+            }
+            100% {
+                transform: translateX(1);
+            }
         }
     </style>
 </head>
 <body>
-    <div class="wave"></div>
+    <div>
         <div class="wave"></div>
-        <div class="wave">div>
+        <div class="wave"></div>
+        <div class="wave"></div>
     </div>
 </body>
 </html>
