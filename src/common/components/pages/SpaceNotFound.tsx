1889e93f33ff60e105da390fc4cf1165cb2e6605
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2024-08-15 19:11:15 -0300

    Fix 404 duplicated nav (#330)

--- src/common/components/pages/SpaceNotFound.tsx
+++ src/common/components/pages/SpaceNotFound.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Navigation from "../organisms/Navigation";
-import { noop } from "lodash";
 import Image from "next/image";
 
 export default function SpaceNotFound({ src = "/images/UserNotFound.png" }) {
   return (
     <div className="flex w-full h-full">
-      <div className={"flex mx-auto transition-all duration-100 ease-out"}>
-        <Navigation isEditable={false} enterEditMode={noop} />
-      </div>
-
       <div className="w-full transition-all duration-100 ease-out p-8 flex">
         <div className="col-span-6 md:col-span-4 lg:col-span-3 aspect-video w-full bg-gray-100 relative self-center">
           <Image
