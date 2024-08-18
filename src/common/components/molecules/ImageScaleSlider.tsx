32395dcbd041c61c99b2e88f139544cccef2ba4f
sktbrd <sktbrd.eth@gmail.com>
2024-08-18 18:05:40 -0300

    Enhancement: Galery now can Link

---
+++ src/common/components/molecules/ImageScaleSlider.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Slider } from "@mui/material";
+
+export type WidthSliderProps = {
+  min: number;
+  max: number;
+  step?: number;
+  defaultValue?: number;
+  onChange: (value: number) => void;
+};
+
+const ImageScaleSlider: React.FC<WidthSliderProps> = ({
+  min = 0.2,
+  max = 3,
+  step = 0.1,
+  defaultValue = 1,
+  onChange,
+}) => {
+  return (
+    <div className="ml-3 mr-3">
+      <Slider
+        defaultValue={defaultValue}
+        step={step}
+        min={min}
+        max={max}
+        onChange={(_, value) => onChange(value as number)}
+      />
+    </div>
+  );
+};
+
+export default ImageScaleSlider;
