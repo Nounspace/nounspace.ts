fda0e5666963a0927c0569c0be6b71a604410128
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2024-08-21 22:59:22 -0300

    Fix: Fix itemBackground color breaking the app + reorg (#344)

--- src/common/components/molecules/LinksInput.tsx
+++ src/common/components/molecules/LinksInput.tsx
@@ -1,5 +1,4 @@
 import React, { forwardRef, useCallback, useState, useEffect } from "react";
-import { Link } from "./Links";
 import { FaPlus, FaImage, FaLink } from "react-icons/fa";
 import {
   Avatar,
@@ -9,7 +8,7 @@ import {
 import styled from "styled-components";
 import { FaPencil } from "react-icons/fa6";
 import CSSInput from "@/common/components/molecules/CSSInput";
-
+import { Link } from "@/fidgets/ui/Links";
 export interface LinksInputProps {
   value: Link[];
   onChange?: (value: Link[]) => void;
@@ -126,10 +125,11 @@ const LinksInput = forwardRef<HTMLInputElement, LinksInputProps>(
       onChange?.([
         ...value,
         {
-          text: "Nouns",
-          url: "https://nouns.wtf",
-          avatar: "/images/nouns.svg",
-          description: "Funds ideas",
+          //placeholder values
+          text: "New Link",
+          url: "https://",
+          avatar: "/images/chainEmoji.png",
+          description: "Description",
         },
       ]);
       setLinkStates([
