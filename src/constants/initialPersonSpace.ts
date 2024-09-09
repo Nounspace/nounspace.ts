6d12b40c6d2061edc330c1f04b3670a108550429
Julian Early <julian.is.early@gmail.com>
2024-09-09 08:47:55 -0700

    refactor: move initial space config into a separate const

--- src/constants/initialPersonSpace.ts
+++ src/constants/initialPersonSpace.ts
@@ -1,50 +1,54 @@
 import { SpaceConfig } from "@/common/components/templates/Space";
 import DEFAULT_THEME from "@/common/lib/theme/defaultTheme";
 import { FeedType, FilterType } from "@neynar/nodejs-sdk";
+import { cloneDeep } from "lodash";
+
+export const INITIAL_SPACE_CONFIG_EMPTY: Omit<SpaceConfig, "isEditable"> = {
+  layoutID: "",
+  layoutDetails: {
+    layoutConfig: {
+      layout: [],
+    },
+    layoutFidget: "grid",
+  },
+  theme: DEFAULT_THEME,
+  fidgetInstanceDatums: {},
+  fidgetTrayContents: [],
+};
 
 const createIntialPersonSpaceConfigForFid = (
   fid: number,
 ): Omit<SpaceConfig, "isEditable"> => {
-  return {
-    layoutID: "",
-    layoutDetails: {
-      layoutConfig: {
-        layout: [
-          {
-            w: 5,
-            h: 8,
-            x: 0,
-            y: 0,
-            i: "feed:profile",
-            minW: 4,
-            maxW: 36,
-            minH: 6,
-            maxH: 36,
-            moved: false,
-            static: false,
-          },
-        ],
-      },
-      layoutFidget: "grid",
-    },
-    theme: DEFAULT_THEME,
-    fidgetInstanceDatums: {
-      "feed:profile": {
-        config: {
-          editable: false,
-          settings: {
-            feedType: FeedType.Filter,
-            users: fid,
-            filterType: FilterType.Fids,
-          },
-          data: {},
+  const config = cloneDeep(INITIAL_SPACE_CONFIG_EMPTY);
+  config.fidgetInstanceDatums = {
+    "feed:profile": {
+      config: {
+        editable: false,
+        settings: {
+          feedType: FeedType.Filter,
+          users: fid,
+          filterType: FilterType.Fids,
         },
-        fidgetType: "feed",
-        id: "feed:profile",
+        data: {},
       },
+      fidgetType: "feed",
+      id: "feed:profile",
     },
-    fidgetTrayContents: [],
   };
+  config.layoutDetails.layoutConfig.layout.push({
+    w: 5,
+    h: 8,
+    x: 0,
+    y: 0,
+    i: "feed:profile",
+    minW: 4,
+    maxW: 36,
+    minH: 6,
+    maxH: 36,
+    moved: false,
+    static: false,
+  });
+  return config;
 };
 
 export default createIntialPersonSpaceConfigForFid;
