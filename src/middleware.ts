a648c11763d8dc687c8952a963a190bd6836dad0
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2024-09-19 21:53:04 -0300

    Redirect Users for space.nounspace.com in Mobile when url is /s/spacetoken (#422)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,19 @@ import { NextResponse } from "next/server";
 import { NextRequest, userAgent } from "next/server";
 import MOBILE_REDIRECT_URL from "@/constants/mobileRedirectUrl";
 
-// Mobile redirect
 export async function middleware(request: NextRequest) {
   const { device } = userAgent(request);
+  const url = request.nextUrl;
 
   if (device.type === "mobile") {
-    return NextResponse.redirect(new URL(MOBILE_REDIRECT_URL));
+    if (url.pathname === "/s/spacetoken") {
+      return NextResponse.redirect(new URL("https://space.nounspace.com"));
+    }
+
+    return NextResponse.redirect(new URL(`${MOBILE_REDIRECT_URL}`));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
