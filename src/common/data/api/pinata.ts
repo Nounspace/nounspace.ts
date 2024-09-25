277cb351dc7ade94002fb2c5b9ffb2c168578e23
Julian Early <julian.is.early@gmail.com>
2024-09-25 11:57:57 -0700

    feat: install pinata

---
+++ src/common/data/api/pinata.ts
@@ -0,0 +1,8 @@
+import { PinataSDK } from "pinata";
+
+const pinata = new PinataSDK({
+  pinataJwt: process.env.PINATA_JWT!,
+  pinataGateway: process.env.PINATA_GATEWAY,
+});
+
+export default pinata;
