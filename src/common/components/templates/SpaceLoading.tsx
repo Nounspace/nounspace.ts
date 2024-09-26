a913c7d5cfa34406110442ec640256d0e72bede5
Jesse Paterson <jesse.r.pat@gmail.com>
2024-09-26 16:05:39 -0500

    trying to fix loading screen glitch

--- src/common/components/templates/SpaceLoading.tsx
+++ src/common/components/templates/SpaceLoading.tsx
@@ -25,33 +25,40 @@ export default function SpaceLoading({ profile }: { profile?: ReactNode }) {
 
   return (
     <>
-      <div className="w-full transition-all duration-100 ease-out h-full">
-        <div className={"h-full flex flex-col"}>
-          {!isUndefined(profile) ? <div> {profile} </div> : null}
-          <div className="flex-1 grid-container grow">
-            <div
-              className="relative grid-overlap w-full h-full opacity-50"
-              style={{
-                transition: "background-color 1000ms linear",
-                display: "grid",
-                gridTemplateColumns: `repeat(${cols}, 1fr)`,
-                gridTemplateRows: `repeat(${maxRows}, ${rowHeight}px)`,
-                gridGap: `${margin[0]}px`,
-                rowGap: `${margin[1]}px`,
-                padding: `${containerPadding[0]}px`,
-                background: "rgba(200, 227, 248, 0.3)",
-              }}
-            >
-              {[...Array(cols * maxRows)].map((_, i) => (
+      <div className="user-theme-background w-full h-full relative flex-col">
+        <div className="w-full transition-all duration-100 ease-out">
+          {!isUndefined(profile) ? (
+            <div className="z-50 bg-white h-40">{profile}</div>
+          ) : null}
+          <div className="w-full transition-all duration-100 ease-out h-full">
+            <div className={"h-full flex flex-col"}>
+              {!isUndefined(profile) ? <div> {profile} </div> : null}
+              <div className="flex-1 grid-container grow">
                 <div
-                  className="rounded-lg"
-                  key={i}
+                  className="relative grid-overlap w-full h-full opacity-50"
                   style={{
-                    backgroundColor: "rgba(200, 227, 248, 0.5)",
-                    outline: "1px dashed rgba(200, 227, 248, 0.8)",
+                    transition: "background-color 1000ms linear",
+                    display: "grid",
+                    gridTemplateColumns: `repeat(${cols}, 1fr)`,
+                    gridTemplateRows: `repeat(${maxRows}, ${rowHeight}px)`,
+                    gridGap: `${margin[0]}px`,
+                    rowGap: `${margin[1]}px`,
+                    padding: `${containerPadding[0]}px`,
+                    background: "rgba(200, 227, 248, 0.3)",
                   }}
-                />
-              ))}
+                >
+                  {[...Array(cols * maxRows)].map((_, i) => (
+                    <div
+                      className="rounded-lg"
+                      key={i}
+                      style={{
+                        backgroundColor: "rgba(200, 227, 248, 0.5)",
+                        outline: "1px dashed rgba(200, 227, 248, 0.8)",
+                      }}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
