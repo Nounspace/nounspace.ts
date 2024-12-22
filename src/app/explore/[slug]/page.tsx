fe9bdcf08893d662085e5133cddad70cb5bb43c1
Serubin <serubin@serubin.net>
2024-12-22 16:31:51 -0500

    Add explore slug page to app router

--- src/app/explore/[slug]/page.tsx
+++ src/app/explore/[slug]/page.tsx
@@ -5,7 +5,6 @@ import {
   getAllSlugs,
   getMarkdownFileBySlug,
 } from "@/common/data/explore/loadExploreMarkdown";
-import { GetStaticProps, InferGetStaticPropsType } from "next/types";
 import { isNull, isObject, isUndefined, map } from "lodash";
 import Image from "next/image";
 
@@ -33,47 +32,31 @@ function isExplorePost(maybe: unknown): maybe is ExplorePost {
   );
 }
 
-export async function getStaticPaths() {
+export async function generateStaticParams() {
   const slugs = await getAllSlugs();
-  return {
-    paths: map(slugs, (s) => ({
-      params: {
-        slug: s,
-      },
-    })),
-    fallback: false,
-  };
+  return map(slugs, (s) => ({ slug: s }));
 }
 
-export const getStaticProps = (async ({ params }) => {
-  const post = await getMarkdownFileBySlug(params!.slug as string);
+const getPostOrSlug = async (
+  slug: string,
+): Promise<ExplorePost | SlugOnlyPost> => {
+  const post = await getMarkdownFileBySlug(slug);
   if (isNull(post)) {
     return {
-      props: {
-        post: {
-          slug: params!.slug as string,
-        },
-      },
+      slug,
     };
   }
+
   const processedContent = await remark().use(html).process(post.content);
   const contentHtml = processedContent.toString();
-
   return {
-    props: {
-      post: {
-        ...post,
-        contentHtml,
-      },
-    },
+    ...post,
+    contentHtml,
   };
-}) satisfies GetStaticProps<{
-  post: ExplorePost | SlugOnlyPost;
-}>;
+};
 
-export default function Post({
-  post,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default async function Post({ params }) {
+  const post = await getPostOrSlug(params!.slug as string);
   if (isExplorePost(post)) {
     return (
       <div>
