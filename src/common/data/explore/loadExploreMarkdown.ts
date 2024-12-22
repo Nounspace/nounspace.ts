6e91c67c7390b5248e4a367f217e93000660bc7d
Serubin <serubin@serubin.net>
2024-12-22 16:31:19 -0500

    Improve typing

--- src/common/data/explore/loadExploreMarkdown.ts
+++ src/common/data/explore/loadExploreMarkdown.ts
@@ -1,6 +1,11 @@
 import matter from "gray-matter";
 import supabaseClient from "../database/supabase/clients/server";
 import { endsWith, filter, isNull, map, startsWith } from "lodash";
+import { type FileObject } from "@supabase/storage-js";
+
+export type PostData = {
+  slug: string;
+} & MatterResultData;
 
 type MatterResultData = {
   category: string;
@@ -10,7 +15,7 @@ type MatterResultData = {
   [key: string]: string;
 };
 
-export async function getAllMarkdownFiles() {
+export async function getAllMarkdownFiles(): Promise<PostData[]> {
   const { data, error } = await supabaseClient.storage.from("explore").list();
   if (error) {
     throw error;
@@ -19,12 +24,16 @@ export async function getAllMarkdownFiles() {
       return [];
     }
     return Promise.all(
-      map(data, (d) => getMarkdownFileBySlug(d.name.replace(/\.md$/, ""))),
+      map(data, (d: FileObject) =>
+        getMarkdownFileBySlug(d.name.replace(/\.md$/, "")),
+      ),
     );
   }
 }
 
-export async function getMarkdownFileBySlug(slug: string) {
+export async function getMarkdownFileBySlug(
+  slug: string,
+): Promise<PostData | null> {
   const { data, error } = await supabaseClient.storage
     .from("explore")
     .download(`${slug}.md`);
