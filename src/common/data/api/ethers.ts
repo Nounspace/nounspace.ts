73e8318b5542adf12da1ecb6ef3529ffff44b3fd
Julian Early <julian.is.early@gmail.com>
2024-12-24 14:44:45 -0800

    feat: add some utility functions

--- src/common/data/api/ethers.ts
+++ src/common/data/api/ethers.ts
@@ -1,6 +1,7 @@
 import { AlchemyProvider } from "ethers/providers";
 import { Interface } from "ethers/abi";
 import { Contract } from "ethers/contract";
+import { OwnerType } from "@/pages/t/base/[contractAddress]";
 
 export const baseProvider = new AlchemyProvider(
   "base",
@@ -13,4 +14,26 @@ export async function loadEthersContract(
 ) {
   const abi = await provider.getCode(contractAddress);
   return new Contract(contractAddress, new Interface(abi));
+}
+
+export async function contractOwnerFromContract(contract: Contract) {
+  let ownerId: string | undefined = "";
+  let ownerIdType: OwnerType = "address";
+  const abi = contract.interface;
+  if (abi.hasFunction("fid")) {
+    ownerId = (await contract.fid()) as string;
+    ownerIdType = "fid" as OwnerType;
+  } else if (abi.hasFunction("owner")) {
+    ownerId = (await contract.owner()) as string;
+  } else if (abi.hasFunction("deployer")) {
+    ownerId = (await contract.deployer()) as string;
+  } else {
+    // Finally use contract creator address as a fall back
+    ownerId = contract.deploymentTransaction()?.from;
+  }
+
+  return {
+    ownerId,
+    ownerIdType,
+  };
 }
