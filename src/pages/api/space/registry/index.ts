c4273d97d0d36f828796f1cda6e64692cc97baea
Julian Early <julian.is.early@gmail.com>
2024-12-24 14:45:14 -0800

    feat: update spaceStore and registry end points to support contract address owners

--- src/pages/api/space/registry/index.ts
+++ src/pages/api/space/registry/index.ts
@@ -1,21 +1,47 @@
+import {
+  baseProvider,
+  contractOwnerFromContract,
+  loadEthersContract,
+} from "@/common/data/api/ethers";
 import requestHandler, {
   NounspaceResponse,
 } from "@/common/data/api/requestHandler";
 import supabaseClient from "@/common/data/database/supabase/clients/server";
+import { loadOnwingIdentitiesForAddress } from "@/common/data/database/supabase/serverHelpers";
 import { isSignable, validateSignable } from "@/common/lib/signedFiles";
-import { findIndex, first, isArray, isUndefined } from "lodash";
+import {
+  findIndex,
+  first,
+  includes,
+  isArray,
+  isNil,
+  isUndefined,
+} from "lodash";
 import { NextApiRequest, NextApiResponse } from "next/types";
 
-export type SpaceRegistration = {
+interface SpaceRegistrationBase {
   spaceName: string;
   identityPublicKey: string;
-  fid: number;
   signature: string;
   timestamp: string;
-};
+}
 
-type SpaceInfo = SpaceRegistration & {
+export interface SpaceRegistrationContract extends SpaceRegistrationBase {
+  contractAddress: string;
+}
+
+export interface SpaceRegistrationFid extends SpaceRegistrationBase {
+  fid: number;
+}
+
+export type SpaceRegistration =
+  | SpaceRegistrationContract
+  | SpaceRegistrationFid;
+
+type SpaceInfo = SpaceRegistrationBase & {
   spaceId: string;
+  fid: number | null;
+  contractAddress: string | null;
 };
 
 function isSpaceRegistration(maybe: unknown): maybe is SpaceRegistration {
@@ -24,8 +50,13 @@ function isSpaceRegistration(maybe: unknown): maybe is SpaceRegistration {
   }
   return (
     typeof maybe["spaceName"] === "string" &&
-    typeof maybe["fid"] === "number" &&
     typeof maybe["timestamp"] === "string"
+  );
+}
+
+function isSpaceRegistrationFid(maybe: unknown): maybe is SpaceRegistrationFid {
+  return (
+    isSpaceRegistration(maybe) && typeof maybe["contractAddress"] == "string"
   );
 }
 
@@ -46,6 +77,20 @@ async function identityCanRegisterForFid(identity: string, fid: number) {
     data !== null &&
     findIndex(data, (i) => i.identityPublicKey === identity) !== -1
   );
+}
+
+async function identityCanRegisterForContract(
+  identity: string,
+  contractAddress: string,
+) {
+  const contract = await loadEthersContract(baseProvider, contractAddress);
+  const { ownerId, ownerIdType } = await contractOwnerFromContract(contract);
+  if (isNil(ownerId)) {
+    return false;
+  } else if (ownerIdType === "fid") {
+    return identityCanRegisterForFid(identity, parseInt(ownerId));
+  }
+  return includes(await loadOnwingIdentitiesForAddress(ownerId), identity);
 }
 
 // Handles the registration of a new space name to requesting identity
@@ -75,33 +120,38 @@ async function registerNewSpace(
     });
     return;
   }
-  if (
-    !(await identityCanRegisterForFid(
-      registration.identityPublicKey,
-      registration.fid,
-    ))
-  ) {
-    res.status(400).json({
-      result: "error",
-      error: {
-        message: `Identity ${registration.identityPublicKey} cannot manage spaces for fid ${registration.fid}`,
-      },
-    });
-    return;
+  if (isSpaceRegistrationFid(registration)) {
+    if (
+      !(await identityCanRegisterForFid(
+        registration.identityPublicKey,
+        registration.fid,
+      ))
+    ) {
+      res.status(400).json({
+        result: "error",
+        error: {
+          message: `Identity ${registration.identityPublicKey} cannot manage spaces for fid ${registration.fid}`,
+        },
+      });
+      return;
+    }
+  } else {
+    if (
+      !(await identityCanRegisterForContract(
+        registration.identityPublicKey,
+        registration.contractAddress,
+      ))
+    ) {
+      res.status(400).json({
+        result: "error",
+        error: {
+          message: `Identity ${registration.identityPublicKey} cannot manage spaces for contract ${registration.contractAddress}`,
+        },
+      });
+      return;
+    }
   }
-  const { data } = await supabaseClient
-    .from("spaceRegistrations")
-    .select("spaceId")
-    .eq("fid", registration.fid)
-    .eq("spaceName", registration.spaceName);
-  if (data && data.length > 0) {
-    res.status(400).json({
-      result: "error",
-      error: {
-        message: `fid ${registration.fid} already has a space named ${registration.spaceName}`,
-      },
-    });
-  }
+
   const { data: result, error } = await supabaseClient
     .from("spaceRegistrations")
     .insert([registration])
@@ -123,6 +173,7 @@ async function registerNewSpace(
 
 // Returns a list of the spaces that the requesting identity
 // owns and can modify, including updates and renaming
+// Does not check for spaces that are for contracts, as that ownership changes easily
 async function listModifiableSpaces(
   req: NextApiRequest,
   res: NextApiResponse<ModifiableSpacesResponse>,
