bd548f4093f8bac0b74065b8c19f8faca0eef390
Serubin <serubin@serubin.net>
2024-12-28 23:33:09 -0500

    Add 'use client' where needed
    Co-authored-by:  Jesse Paterson <jesse.r.pat@gmail.com>

--- src/common/providers/Privy.tsx
+++ src/common/providers/Privy.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { PrivyClientConfig, PrivyProvider } from "@privy-io/react-auth";
 
--- src/common/providers/Query.tsx
+++ src/common/providers/Query.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
