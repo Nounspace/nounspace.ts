44934a92cf2ea736ced0745b4ac94048fe18a3b9
sktbrd <sktbrd.eth@gmail.com>
2025-01-01 16:11:38 -0300

    Add keyword filtering to useGetCasts and implement useGetCastsByKeyword hook

--- src/common/data/queries/farcaster.ts
+++ src/common/data/queries/farcaster.ts
@@ -77,24 +77,48 @@ export const useGetCasts = ({
   return useInfiniteQuery({
     queryKey: ["channelCasts", feedType, fid, filterType, fids, channel],
     staleTime: 1000 * 60 * 1,
-
     queryFn: async ({ pageParam: cursor }) => {
+      const params: any = {
+        fid: fid === -1 ? 456830 : fid,
+        viewer_fid: fid === -1 ? 456830 : fid,
+        feedType,
+        cursor,
+        channel_id: channel,
+        fids,
+        filter_type: filterType,
+      };
       const { data } = await axiosBackend.get<FeedResponse>(
         "/api/farcaster/neynar/feed",
-        {
-          params: {
-            fid: fid === -1 ? 456830 : fid,
-            viewer_fid: fid === -1 ? 456830 : fid,
-            feedType,
-            cursor,
-            channel_id: channel,
-            fids,
-            filter_type: filterType, // Add underscore as neynar wants it for the query
-          },
-        },
+        { params },
       );
-
       return data;
+    },
+    initialPageParam: undefined as string | undefined,
+    getNextPageParam: (lastPage, _allPages, _lastPageParam, _allPageParams) =>
+      !isUndefined(lastPage) && !isUndefined(lastPage.next)
+        ? lastPage.next.cursor
+        : undefined,
+  });
+};
+
+export const useGetCastsByKeyword = ({ keyword }: { keyword: string }) => {
+  return useInfiniteQuery({
+    queryKey: ["keywordCasts", keyword],
+    staleTime: 1000 * 60 * 1,
+    queryFn: async ({ pageParam: cursor }) => {
+      try {
+        const url = `https://api.neynar.com/v2/farcaster/cast/search?q=${keyword}&priority_mode=false&limit=25&cursor=${cursor}`;
+        const apiKey = process.env.NEYNAR_API_KEY;
+        const { data } = await axiosBackend.get<FeedResponse>(url, {
+          headers: {
+            "x-api-key": apiKey,
+          },
+        });
+        return data;
+      } catch (error) {
+        console.error("Error fetching casts by keyword:", error);
+        throw error;
+      }
     },
     initialPageParam: undefined as string | undefined,
     getNextPageParam: (lastPage, _allPages, _lastPageParam, _allPageParams) =>
