41f8465d5b803fe9a83934bf05a04c3072facc9e
Julian Early <julian.is.early@gmail.com>
2025-01-04 14:06:55 -0800

    refactor + feat: add etherscan api info and reorder files to have better naming

--- src/constants/optimismChainClient.ts
+++ src/constants/optimismChainClient.ts
@@ -1,6 +1,6 @@
 import { createPublicClient } from "viem";
 import { optimism } from "viem/chains";
-import { optimismHttp } from "./chains";
+import { optimismHttp } from "./alchemyChainUrls";
 
 export const optimismChaninClient = createPublicClient({
   chain: optimism,
