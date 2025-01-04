a2c649f7869fb2c4c101a3757854dbcc7fd66b7d
Julian Early <julian.is.early@gmail.com>
2025-01-04 14:06:55 -0800

    refactor + feat: add etherscan api info and reorder files to have better naming

--- src/common/providers/Wagmi.tsx
+++ src/common/providers/Wagmi.tsx
@@ -4,7 +4,11 @@ import { Chain, Transport } from "viem";
 import { createConfig } from "wagmi";
 import { WagmiProvider } from "@privy-io/wagmi";
 import type { CreateConfigParameters } from "wagmi";
-import { baseHttp, mainnetHttp, optimismHttp } from "@/constants/chains";
+import {
+  baseHttp,
+  mainnetHttp,
+  optimismHttp,
+} from "@/constants/alchemyChainUrls";
 import { base, mainnet, optimism } from "viem/chains";
 
 const wagmiParams: CreateConfigParameters<
