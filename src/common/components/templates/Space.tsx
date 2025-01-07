d5c3187ec33ac6dc026e729a8b67667af75ade1f
Jesse Paterson <jesse.r.pat@gmail.com>
2025-01-06 18:17:47 -0600

    cleaning merge conflicts

--- src/common/components/templates/Space.tsx
+++ src/common/components/templates/Space.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { ReactNode, useEffect } from "react";
 import {
   FidgetConfig,
--- src/common/lib/theme/UserThemeProvider.tsx
+++ src/common/lib/theme/UserThemeProvider.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect } from "react";
 import defaultTheme from "@/common/lib/theme/defaultTheme";
 import setGlobalStyleProperty from "@/common/lib/utils/setGlobalStyleProperty";
