c3c382cab505f42cf042746edb8ff563453a27c0
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2025-01-07 03:00:47 -0300

    feat: add clanker tokens tab

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import { groupBy } from "lodash";
-import Image from "next/image";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/common/components/atoms/tabs";
 import {
   getAllMarkdownFiles,
   PostData,
 } from "@/common/data/explore/loadExploreMarkdown";
+import {
+  tabContentClasses,
+  tabListClasses,
+  tabTriggerClasses,
+} from "@/common/lib/theme/helpers";
+import { groupBy } from "lodash";
+import Image from "next/image";
 import ExploreCard from "./ExploreCard";
+import { TokensGrid } from "./Tokens";
 
 const categories = [
   { title: "Nounish", image: "/images/explore-icons/nounish.png" },
@@ -24,51 +35,88 @@ export default async function Explore() {
   const groupedPosts = groupBy(posts, (post: PostData) => post?.category);
 
   return (
-    <div className="min-h-screen max-w-screen h-screen w-screen">
-      <div className="flex w-full h-full">
-        {/* Main Content */}
-        <div className="w-full transition-all duration-100 ease-out h-full p-5 grid grid-rows-[auto_1fr]">
-          <div className="min-h-48 rounded-lg relative overflow-hidden grid grid-cols-1 grid-rows-1 place-content-center">
-            <Image
-              src="/images/rainforest.png"
-              alt="Rainforest"
-              fill
-              className="col-span-1 row-span-1 object-cover object-bottom"
+    <div className="min-h-screen max-w-screen max-h-screen h-screen w-screen p-5 overflow-y-scroll">
+      <Tabs defaultValue="tokens" className="max-h-full">
+        <TabsList className={tabListClasses}>
+          <TabsTrigger value="tokens" className={tabTriggerClasses}>
+            Tokens
+          </TabsTrigger>
+          <TabsTrigger value="spaces" className={tabTriggerClasses}>
+            Spaces
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="tokens" className={tabContentClasses}>
+          <div className="transition-all duration-100 ease-out max-h-full overflow-y-scroll grid grid-rows-[auto_1fr]">
+            <ExploreHeader
+              title="Explore Clanker Tokens"
+              image="/images/clanker_galaxy.png"
             />
-            <div className="col-span-1 row-span-1 z-10 text-center font-bold text-white grid place-content-center text-4xl">
-              Explore Featured Spaces
+            <TokensGrid />
+          </div>
+        </TabsContent>
+        <TabsContent value="spaces" className={tabContentClasses}>
+          <div className="flex w-full h-full">
+            <div className="w-full transition-all duration-100 ease-out h-full grid grid-rows-[auto_1fr]">
+              <ExploreHeader
+                title="Explore Featured Spaces"
+                image="/images/rainforest.png"
+              />
+              <CategoriesGrid
+                categories={categories}
+                groupedPosts={groupedPosts}
+              />
             </div>
           </div>
-          <div className="grid grid-cols-1 gap-8 mt-5 overflow-auto">
-            {categories.map(({ title, image }) => {
-              if (!groupedPosts[title]) return null;
-
-              return (
-                <div
-                  key={title}
-                  className="flex flex-col gap-4 border-b border-b-slate-200 pb-8 last:border-b-0 last:pb-0"
-                >
-                  <h2 className="text-2xl font-bold flex flex-row gap-4 items-center">
-                    <Image
-                      src={image}
-                      alt={title}
-                      width={48}
-                      height={48}
-                      className="shrink-0"
-                    />
-                    {title}
-                  </h2>
-                  <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-                    {groupedPosts[title].map((post: PostData, i: number) => (
-                      <ExploreCard key={`${post.slug}-${i}`} post={post} />
-                    ))}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
+const ExploreHeader = ({ title, image }) => {
+  return (
+    <div className="min-h-48 rounded-lg relative overflow-hidden grid grid-cols-1 grid-rows-1 place-content-center">
+      <Image
+        src={image}
+        alt={title}
+        fill
+        className="col-span-1 row-span-1 object-cover object-bottom"
+      />
+      <div className="col-span-1 row-span-1 z-10 text-center font-bold text-white grid place-content-center text-4xl">
+        {title}
+      </div>
+    </div>
+  );
+};
+
+const CategoriesGrid = ({ categories, groupedPosts }) => {
+  return (
+    <div className="grid grid-cols-1 gap-8 mt-5 overflow-auto">
+      {categories.map(({ title, image }) => {
+        if (!groupedPosts[title]) return null;
+
+        return (
+          <div
+            key={title}
+            className="flex flex-col gap-4 border-b border-b-slate-200 pb-8 last:border-b-0 last:pb-0"
+          >
+            <h2 className="text-2xl font-bold flex flex-row gap-4 items-center">
+              <Image
+                src={image}
+                alt={title}
+                width={48}
+                height={48}
+                className="shrink-0"
+              />
+              {title}
+            </h2>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+              {groupedPosts[title].map((post: PostData, i: number) => (
+                <ExploreCard key={`${post.slug}-${i}`} post={post} />
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
