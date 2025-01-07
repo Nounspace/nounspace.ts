b94bb54b34f69f1f3d0626e67e9a88519709018e
sktbrd <sktbrd.eth@gmail.com>
2025-01-07 19:23:30 -0300

    Remove overflow y

--- src/pages/t/base/[contractAddress]/TokenDataHeader.tsx
+++ src/pages/t/base/[contractAddress]/TokenDataHeader.tsx
@@ -112,7 +112,10 @@ const TokenTabBarHeader: React.FC<TokenTabBarHeaderProps> = ({
   };
 
   return (
-    <div className="flex items-center justify-between px-4 py-2 w-full">
+    <div
+      className="flex items-center justify-between px-4 py-2 w-full"
+      style={{ overflowY: "hidden" }}
+    >
       {/* Avatar and Token Details */}
       <div className="flex items-center space-x-4">
         {/* Avatar */}
