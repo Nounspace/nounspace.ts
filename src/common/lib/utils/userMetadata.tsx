dbbcc9067b5bae0cb21ed1d5a78ab1eb295cac31
Serubin <serubin@serubin.net>
2025-01-06 22:49:59 -0500

    Add metadata

--- src/common/lib/utils/userMetadata.tsx
+++ src/common/lib/utils/userMetadata.tsx
@@ -1,5 +1,6 @@
 import { WEBSITE_URL } from "@/constants/app";
-import React from "react";
+import { merge } from "lodash";
+import { Metadata } from "next";
 
 export type UserMetadata = {
   fid?: number;
@@ -9,17 +10,15 @@ export type UserMetadata = {
   bio?: string;
 };
 
-// TODO @serubin convert this to app router nextjs
-export const UserMetadata = ({
-  userMetadata,
-}: {
-  userMetadata: UserMetadata | null;
-}) => {
+export const getUserMetadataStructure = (
+  userMetadata: UserMetadata,
+): Metadata => {
   if (!userMetadata) {
-    return null;
+    return {};
   }
 
   const { username, displayName, pfpUrl, bio } = userMetadata;
+
   const title = `${displayName} (@${username}) on Nounspace`;
   const spaceUrl = `https://nounspace.com/s/${username}`;
 
@@ -29,28 +28,41 @@ export const UserMetadata = ({
 
   const ogImageUrl = `${WEBSITE_URL}/api/metadata/spaces?username=${username}&displayName=${encodedDisplayName}&pfpUrl=${encodedPfpUrl}&bio=${encodedBio}`;
 
-  return (
-    <>
-      <title>{title}</title>
-      <meta property="og:title" content={title} />
-      <meta name="twitter:title" content={title} />
-      <meta property="twitter:domain" content="https://nounspace.com/" />
-      <meta property="og:url" content={spaceUrl} />
-      <meta property="twitter:url" content={spaceUrl} />
-      {bio && (
-        <>
-          <meta name="description" content={bio} />
-          <meta property="og:description" content={bio} />
-          <meta name="twitter:description" content={bio} />
-        </>
-      )}
-      {pfpUrl && (
-        <>
-          <meta name="twitter:card" content={pfpUrl} />
-          <meta property="og:image" content={ogImageUrl} />
-          <meta name="twitter:image" content={ogImageUrl} />
-        </>
-      )}
-    </>
-  );
+  const metadata = {
+    title,
+    openGraph: {
+      title,
+      url: spaceUrl,
+    },
+    twitter: {
+      title,
+      domain: "https://nounspace.com/",
+      url: spaceUrl,
+    },
+  };
+
+  if (bio) {
+    merge(metadata, {
+      description: bio,
+      openGraph: {
+        description: bio,
+      },
+      twitter: {
+        description: bio,
+      },
+    });
+  }
+  if (pfpUrl) {
+    merge(metadata, {
+      twitter: {
+        card: pfpUrl,
+        image: ogImageUrl,
+      },
+      openGraph: {
+        image: ogImageUrl,
+      },
+    });
+  }
+
+  return metadata;
 };
