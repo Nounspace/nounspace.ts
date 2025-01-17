ce70a3f5c09a95e31a155dbeaf0708079281e5be
sktbrd <sktbrd.eth@gmail.com>
2025-01-17 20:51:58 -0300

    feat: add React import to contract page component

--- src/pages/t/base/[contractAddress]/[tabName].tsx
+++ src/pages/t/base/[contractAddress]/[tabName].tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import ContractPrimarySpace, { ContractSpacePageProps } from ".";
 import { loadContractData } from "@/common/data/loaders/contractPagePropsLoader";
