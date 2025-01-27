40f81d77846eec037d06641090f234e4dfe9bec9
sktbrd <sktbrd.eth@gmail.com>
2025-01-27 16:52:41 -0300

    enhance format marketcap function to include billions 🚀

--- src/common/lib/utils/formatNumber.ts
+++ src/common/lib/utils/formatNumber.ts
@@ -1,5 +1,7 @@
 export function formatNumber(value: number): string {
-  if (value >= 1e6) {
+  if (value >= 1e9) {
+    return (value / 1e9).toFixed(2) + "B";
+  } else if (value >= 1e6) {
     return (value / 1e6).toFixed(2) + "M";
   } else if (value >= 1e3) {
     return (value / 1e3).toFixed(2) + "K";
