349767e1306888a3a8f061bba30770d0697dd61a
r4topunk <gustavo-kuhl@hotmail.com>
2025-01-30 18:14:31 -0300

    fix: allow overide tabs on storage

--- src/pages/api/space/registry/[spaceId]/tabs/index.ts
+++ src/pages/api/space/registry/[spaceId]/tabs/index.ts
@@ -127,6 +127,7 @@ async function registerNewSpaceTab(
     .upload(
       `${registration.spaceId}/tabs/${registration.tabName}`,
       new Blob([stringify(uploadedFile)], { type: "application/json" }),
+      { upsert: true },
     );
   if (!isNull(error)) {
     console.error("Error uploading file:", error);
