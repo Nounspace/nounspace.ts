800d1fd84fc4f2bcb725ccfad70b7d29b7afe9e6
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2025-01-30 18:53:12 -0300

    update renderEmbedurl prop as a build fix

--- src/fidgets/farcaster/components/CastRow.tsx
+++ src/fidgets/farcaster/components/CastRow.tsx
@@ -154,7 +154,7 @@ const CastEmbeds = ({ cast, onSelectCast }) => {
               }
             }}
           >
-            {renderEmbedForUrl(embedData)}
+            {renderEmbedForUrl(embedData, false)}
           </div>
         );
       })}
