ca873712f69dc66830156d938ec24395d0e66242
Ricardo Ferrari <rferrariwd@gmail.com>
2025-02-11 00:09:01 -0300

    venice: choose best sys prompt

--- src/app/api/venice/route.ts
+++ src/app/api/venice/route.ts
@@ -2,8 +2,13 @@ import neynar from "@/common/data/api/neynar";
 
 const DEBUG_PROMPTS = false;
 const VENICE_API_KEY = process.env.VENICE_API_KEY;
-var VENICE_MODEL = "llama-3.2-3b";
+
+const VENICE_MODEL = "llama-3.2-3b";
 // var VENICE_MODEL = "deepseek-r1-671b";
+
+const MODEL_TEMPERATURE_DETERMINISTIC = 0.2;
+const MODEL_TEMPERATURE_CREATIVE = 0.6;
+const MODEL_TEMPERATURE_ALUCINATE = 0.9;
 
 const SYSTEM_PROMPT2 = `
 You are a social media expert specializing in crafting highly engaging, concise Twitter posts (“banger tweets”). Review the following information:
@@ -22,8 +27,11 @@ Using the user's Bio and past tweets to inform style and voice, write a single t
 2. Reflects the user's unique tone or perspective based on their bio.
 3. Incorporates key ideas from the User Input.
 4. Resonates with a broad audience and is likely to get high engagement (likes, retweets, comments).
+5. Ensure the tweet is polished, powerful, and follows best practices for high-engagement tweets. Do not include any hashtags, usernames, or additional commentary—only the final tweet text as your output.
 
-Ensure the tweet is polished, powerful, and follows best practices for high-engagement tweets. Do not include any hashtags, usernames, or additional commentary—only the final tweet text as your output.
+# Guidelines:
+DO NOT add new context, opinions, or unrelated details.
+DO NOT include quotes in the response.
 `;
 
 const SYSTEM_PROMPT = `You are a social media expert.
@@ -35,7 +43,6 @@ Respond only with the improved tweet text, without any introduction, explanation
 # Guidelines:
 DO NOT add new context, opinions, or unrelated details.
 DO NOT include quotes in the response.
-
 `;
 
 const ENHANCE_PROMPT = `
@@ -53,7 +60,8 @@ const CREATE_PROMPT = `Create a new creative and engaging tweet.
 # Guidelines:
 DO NOT add new context, opinions, or unrelated details.
 DO NOT include quotes in the response.
-`;
+
+# TWEET:`;
 
 
 export async function POST(request: Request) {
@@ -62,39 +70,24 @@ export async function POST(request: Request) {
   }
 
   const res = await request.json();
-
-  const userCast = res.text;
-  if (!userCast) {
-    // return new Response("Text is missing", { status: 400 });
-    // if no input, create a new cast from thin air
-  }
-
+  const userCast = res.text || "";
   const userFid = res.fid;
+
   if (!userFid) {
     return new Response("User fid is missing", { status: 400 });
   }
 
   const userCasts = await neynar.fetchPopularCastsByUser(userFid);
-
   const userBio = userCasts.casts?.[0].author.profile.bio.text || "";
 
-  let exampleCastsText = "";
-  if (userCasts?.casts?.length) {
-    userCasts.casts.forEach((cast: any) => {
-      const likesCount = cast.reactions?.likes?.length || 0;
-      const recastsCount = cast.reactions?.recasts?.length || 0;
-      // exampleCastsText += `Text: ${cast.text}\nLikes: ${likesCount}\nRecasts: ${recastsCount}\n\n`;
-      exampleCastsText += `Text: ${cast.text}`;
-    });
-  }
+  let exampleCastsText = userCasts.casts?.length
+    ? userCasts.casts.map(cast => `Text: ${cast.text}`).join("\n")
+    : "";
 
   try {
-    let PROMPT = ENHANCE_PROMPT;
-    if (userCast.trim().length === 0) {
-      PROMPT = CREATE_PROMPT;
-    }
+    const PROMPT = userCast.trim().length === 0 ? CREATE_PROMPT : ENHANCE_PROMPT;
 
-    const options = {
+    const getOptions = (messages: any) => ({
       method: "POST",
       headers: {
         Authorization: "Bearer " + VENICE_API_KEY,
@@ -102,113 +95,70 @@ export async function POST(request: Request) {
       },
       body: JSON.stringify({
         model: VENICE_MODEL,
-        temperature: 0.6,     // close to 0.00 is more deterministic, close to 1.00 is more creative
-        messages: [
-          {
-            role: "system",
-
-            content: SYSTEM_PROMPT2
-              .replace("{USER_BIO}", userBio)
-              .replace("{USER_INPUT}", userCast)
-              .replace("{USER_TWEETS}", exampleCastsText),
-
-            //             content: SYSTEM_PROMPT + `
-            // User bio: ${userBio}
-            // <post_examples>
-            //     ${exampleCastsText}
-            // </post_examples>`,                   // system prompt
-          },
-          //           {
-          //             role: "assistant",      // assistant prompt
-          //             content: `
-          // User bio: ${userBio}
-          // <post_examples>
-          // ${exampleCastsText}
-          // </post_examples>`,
-          //           },
-          {
-            role: "user",
-            content: PROMPT + userCast, // user prompt
-          },
-        ],
+        temperature: MODEL_TEMPERATURE_CREATIVE,
+        messages,
         venice_parameters: {
           include_venice_system_prompt: false,
         },
       }),
-    };
+    });
 
-
-    //
-    // debug
-    // ---
-    //
-    if (DEBUG_PROMPTS) {
-
-      const options2 = {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + VENICE_API_KEY,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          model: VENICE_MODEL,
-          temperature: 0.6,     // close to 0.00 is more deterministic, close to 1.00 is more creative
-          messages: [
-            {
-              role: "system",
-              content: SYSTEM_PROMPT + `
-User bio: ${userBio}
-<post_examples>
-    ${exampleCastsText}
-</post_examples>`,                   // system prompt
-            },
-            {
-              role: "user",
-              content: PROMPT + userCast, // user prompt
-            },
-          ],
-          venice_parameters: {
-            include_venice_system_prompt: false,
-          },
-        }),
-      };
-
-      const fetchResponse2 = await fetch(
-        "https://api.venice.ai/api/v1/chat/completions",
-        options2,
-      );
-      const result2 = await fetchResponse2.json();
-      var choice = result2.choices[0].message.content;
-      if ("deepseek-r1-671b" == VENICE_MODEL)
-        choice = choice.replace(/<think>[\s\S]*?<\/think>\s*\n*/g, '');
-      console.warn("\nchoice2: " + choice);
-
-      //
-      // debug
-      // ---
-      //
-    }
-
-
-    // console.dir("options", options.body);
+    const messages = [{
+      role: "system",
+      content: SYSTEM_PROMPT + `
+          User bio: ${userBio}
+          <post_examples>
+            ${exampleCastsText}
+          </post_examples>`,
+    },
+    {
+      role: "user",
+      content: PROMPT + userCast,
+    }];
 
     const fetchResponse = await fetch(
       "https://api.venice.ai/api/v1/chat/completions",
-      options,
+      getOptions(messages),
     );
 
     const result = await fetchResponse.json();
-    var choice = result.choices[0].message.content;
+    let choice = result.choices[0].message.content;
 
-    if ("deepseek-r1-671b" == VENICE_MODEL)
-      choice = choice.replace(/<think>[\s\S]*?<\/think>\s*\n*/g, '');
+    // if (VENICE_MODEL === "deepseek-r1-671b")
+    choice = choice.replace(/<\/think>.*?<\/think>/gs, '');
 
-    if(DEBUG_PROMPTS)
+    if (DEBUG_PROMPTS) {
+      const messages2 = [{
+        role: "system",
+        content: SYSTEM_PROMPT2
+          .replace("{USER_BIO}", userBio)
+          .replace("{USER_INPUT}", userCast)
+          .replace("{USER_TWEETS}", exampleCastsText),
+      },
+      {
+        role: "user",
+        content: PROMPT + userCast,
+      }];
+
+      const fetchResponse2 = await fetch(
+        "https://api.venice.ai/api/v1/chat/completions",
+        getOptions(messages2),
+      );
+
+      const result2 = await fetchResponse2.json();
+      let choice2 = result2.choices[0].message.content;
+
+      // if (VENICE_MODEL === "deepseek-r1-671b")
+      choice2 = choice2.replace(/<\/think>.*?<\/think>/gs, '');
+
       console.log("\nchoice1: " + choice);
+      console.log("\nchoice2: " + choice2);
+    }
 
     return Response.json({ response: choice });
+
   } catch (error) {
-    console.error("Error fetching data:", error);
-    throw new Error("Failed to fetch data");
+    // console.error("Error fetching data:", error);
+    throw new Error("Failed to fetch data: " + error);
   }
 }
