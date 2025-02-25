1df55dc66a8748e3535bfc48e1960e23cb5bacc9
r4topunk <gustavo-kuhl@hotmail.com>
2025-02-25 15:58:38 -0300

    refactor: delete unused configs

--- src/app/api/venice/route.ts
+++ src/app/api/venice/route.ts
@@ -1,28 +1,29 @@
 import neynar from "@/common/data/api/neynar";
 
-import { SYSTEM_PROMPT, ENHANCE_PROMPT, CREATE_PROMPT } from './prompts'
 import {
-  USERS_CASTS_CHRONOLOGICALLY,
-  USE_USER_PAST_TWEETS, MAX_TRENDING_TWEETS,
-  VENICE_API_KEY, VENICE_MODEL,
-  MODEL_TEMPERATURE_DETERMINISTIC, MODEL_TEMPERATURE_CREATIVE, MODEL_TEMPERATURE_ALUCINATE,
+  DEBUG_PROMPTS,
+  MAX_TRENDING_TWEETS,
+  MODEL_TEMPERATURE_CREATIVE,
   TODAY_TIME_DATE,
-  DEBUG_PROMPTS
-} from './config'
+  USERS_CASTS_CHRONOLOGICALLY,
+  USE_USER_PAST_TWEETS,
+  VENICE_API_KEY,
+  VENICE_MODEL,
+} from "./config";
+import { CREATE_PROMPT, ENHANCE_PROMPT, SYSTEM_PROMPT } from "./prompts";
 
 //
 // Process Trending Casts array and return a string with the top MAX_TRENDING_TWEETS casts
 //
 function processTrendingCasts(casts: any) {
-  let trendingCasts = casts.casts
-    .map(cast => {
-      const username = cast.author.username;
-      const text = cast.text;
-      return `<trending_tweet>\n@${username}: ${text}\n</trending_tweet>\n\n`;
-    });
+  let trendingCasts = casts.casts.map((cast) => {
+    const username = cast.author.username;
+    const text = cast.text;
+    return `<trending_tweet>\n@${username}: ${text}\n</trending_tweet>\n\n`;
+  });
 
-  trendingCasts = trendingCasts.slice(0, MAX_TRENDING_TWEETS)
-  return trendingCasts.join('');
+  trendingCasts = trendingCasts.slice(0, MAX_TRENDING_TWEETS);
+  return trendingCasts.join("");
 }
 
 export async function POST(request: Request) {
@@ -39,16 +40,16 @@ export async function POST(request: Request) {
 
   // get values
 
-    // if USE_USER_PAST_TWEETS
+  // if USE_USER_PAST_TWEETS
   // get user past casts as examples
   let user_past_tweets;
   let userCasts: any;
   if (USE_USER_PAST_TWEETS) {
-    if(USERS_CASTS_CHRONOLOGICALLY) {
+    if (USERS_CASTS_CHRONOLOGICALLY) {
       userCasts = await neynar.fetchAllCastsCreatedByUser(userFid, {
         viewerFid: userFid,
-        limit: 5}
-      );
+        limit: 5,
+      });
       userCasts = userCasts.result;
       // exampleCastsText = userCasts.result.casts?.length
       // ? userCasts.result.casts.map(cast => `<tweet>${cast.text}</tweet>\n`).join("\n")
@@ -58,7 +59,9 @@ export async function POST(request: Request) {
     }
 
     const exampleCastsText = userCasts.casts?.length
-      ? userCasts.casts.map(cast => `<tweet>${cast.text}</tweet>\n`).join("\n")
+      ? userCasts.casts
+          .map((cast) => `<tweet>${cast.text}</tweet>\n`)
+          .join("\n")
       : "";
 
     user_past_tweets = `
@@ -67,7 +70,7 @@ export async function POST(request: Request) {
 ${exampleCastsText}
 </USER_PAST_TWEETS>
 `;
-}
+  }
 
   const currentUser = await neynar.fetchBulkUsers([userFid]);
   const userName = currentUser.users[0].username || "";
@@ -79,24 +82,28 @@ ${exampleCastsText}
   // generate or enahance casts
   try {
     // select prompt for enhance or create cast
-    const PROMPT = userCast.trim().length === 0 ? CREATE_PROMPT : ENHANCE_PROMPT;
+    const PROMPT =
+      userCast.trim().length === 0 ? CREATE_PROMPT : ENHANCE_PROMPT;
 
     // setup the system prompt with variables
-    const system_prompt = SYSTEM_PROMPT
-      .replace('{USER_NAME}', userName)
-      .replace('{TODAY_TIME_DATE}', TODAY_TIME_DATE)
+    const system_prompt = SYSTEM_PROMPT.replace("{USER_NAME}", userName)
+      .replace("{TODAY_TIME_DATE}", TODAY_TIME_DATE)
       .replace("{USER_BIO}", userBio);
     // .replace("{USER_TWEETS}", exampleCastsText);
 
     // setup the user prompt with variables
-    const user_prompt = PROMPT
-      .replace("{TRENDING_FEED}", trendingCasts)
-      .replace("{USER_TWEETS}", user_past_tweets || "")
-      + userCast;
+    const user_prompt =
+      PROMPT.replace("{TRENDING_FEED}", trendingCasts).replace(
+        "{USER_TWEETS}",
+        user_past_tweets || "",
+      ) + userCast;
 
     // use for debug
-    if (DEBUG_PROMPTS)
-      console.log(`\n\n---------SYSTEM-----------`); console.log(system_prompt);    console.log(`\n\n---------USER-----------`); console.log(user_prompt); console.log(`--------------------`);
+    if (DEBUG_PROMPTS) console.log(`\n\n---------SYSTEM-----------`);
+    console.log(system_prompt);
+    console.log(`\n\n---------USER-----------`);
+    console.log(user_prompt);
+    console.log(`--------------------`);
 
     // build the venice model options
     const getOptions = (messages: any) => ({
@@ -106,7 +113,6 @@ ${exampleCastsText}
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-
         model: VENICE_MODEL,
         temperature: MODEL_TEMPERATURE_CREATIVE,
         messages,
@@ -119,18 +125,20 @@ ${exampleCastsText}
     // build model system and user prompts
     const messages = [
       { role: "system", content: system_prompt },
-      { role: "user", content: user_prompt, }
+      { role: "user", content: user_prompt },
     ];
 
-    const fetchResponse = await fetch("https://api.venice.ai/api/v1/chat/completions",
+    const fetchResponse = await fetch(
+      "https://api.venice.ai/api/v1/chat/completions",
       getOptions(messages),
     );
     const response = await fetchResponse.json();
 
     // process model response
     let result = response.choices[0].message.content;
-    result = result.replace(/<\/think>.*?<\/think>/gs, '')    // remove <think> tags
-      .replace(/^['"]|['"]$/g, '');           // remove quotes marks
+    result = result
+      .replace(/<\/think>.*?<\/think>/gs, "") // remove <think> tags
+      .replace(/^['"]|['"]$/g, ""); // remove quotes marks
 
     //return to frontend clear response
     return Response.json({ response: result });
