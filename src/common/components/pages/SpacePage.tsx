07376921e4906ae8d11f5123efe5f347ffa26b76
sktbrd <sktbrd.eth@gmail.com>
2025-03-18 16:26:37 -0300

    framesv2: remove unused FrameV2Fidget component from SpacePage

--- src/common/components/pages/SpacePage.tsx
+++ src/common/components/pages/SpacePage.tsx
@@ -4,7 +4,6 @@ import { isUndefined } from "lodash";
 import SpaceLoading from "@/common/components/templates/SpaceLoading";
 import { useSidebarContext } from "@/common/components/organisms/Sidebar";
 
-import FrameV2Fidget from "@/fidgets/framesv2/framev2";
 
 export type SpacePageArgs = {
   config?: SpaceConfig;
@@ -39,12 +38,6 @@ export default function SpacePage({
         loading ? (
         <SpaceLoading profile={profile} tabBar={tabBar} inEditMode={editMode} />
       ) : (
-        // <FrameV2Fidget searchParams={{ 
-        //   url: "https://f.bracket.game/", 
-        //   specification:"farcaster_v2", 
-        //   actions:"true" 
-        //   }} />
-
         <Space
           config={config}
           saveConfig={saveConfig}
