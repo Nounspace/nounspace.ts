2894a55011264a14336c723fc50deed9dd09170b
Jesse Paterson <jesse.r.pat@gmail.com>
2025-03-20 16:58:57 -0600

    fixed ProfileSpace tab import

--- src/app/(spaces)/homebase/[tabname]/page.tsx
+++ src/app/(spaces)/homebase/[tabname]/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { useParams } from "next/navigation";
 import PrivateSpace from "../PrivateSpace";
 
-const HomebaseTab = () => {
+const HomebaseTabPage = () => {
   const params = useParams<{ tabname: string }>();
   const tabName = decodeURIComponent(params?.tabname ?? "");
 
   return <PrivateSpace tabName={tabName} />;
 };
 
-export default HomebaseTab;
+export default HomebaseTabPage;
