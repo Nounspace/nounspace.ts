d23f03dba43710c9f9ace1bc76a74a3987c2747c
r4to <r4topunk.eth@gmail.com>
2025-03-27 14:35:03 -0300

    fix: editor layout scroll bug

--- src/common/components/organisms/EditorPanel.tsx
+++ src/common/components/organisms/EditorPanel.tsx
@@ -118,7 +118,7 @@ export const EditorPanel: React.FC<EditorPanelProps> = ({
   return (
     <aside
       id="logo-sidebar"
-      className="h-full flex-row flex bg-white transition-transform -translate-x-full sm:translate-x-0"
+      className="h-screen flex-row flex bg-white transition-transform -translate-x-full sm:translate-x-0"
       aria-label="Sidebar"
     >
       <div className="flex-1 w-[270px] h-full pt-12 flex-col flex px-4 py-4 overflow-y-hidden border-r">
