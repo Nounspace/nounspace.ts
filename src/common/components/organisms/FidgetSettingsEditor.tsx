8d0dcc9c96ba9eada0716cb00af33fb83809000d
r4to <r4topunk.eth@gmail.com>
2025-04-02 02:26:09 -0300

    feat: add display name hint support to FidgetSettingsEditor
    - Integrated display name hint tooltip in FidgetSettingsRow.
    - Updated TextInput to handle props more efficiently.

--- src/common/components/organisms/FidgetSettingsEditor.tsx
+++ src/common/components/organisms/FidgetSettingsEditor.tsx
@@ -5,7 +5,7 @@ import {
   FidgetFieldConfig,
 } from "@/common/fidgets";
 import BackArrowIcon from "../atoms/icons/BackArrow";
-import { FaTrashCan } from "react-icons/fa6";
+import { FaTrashCan, FaCircleInfo } from "react-icons/fa6";
 import { Button } from "@/common/components/atoms/button";
 import {
   Tabs,
@@ -79,17 +79,30 @@ export const FidgetSettingsRow: React.FC<FidgetSettingsRowProps> = ({
       )}
       id={id}
     >
-      <div className="md:mb-0 md:w-2/3">
+      <div className="md:mb-0 md:w-full flex items-center justify-between gap-2">
         <label className="capitalize text-sm font-medium text-gray-900 dark:text-white">
           {field.displayName || field.fieldName}
         </label>
+        {field.displayNameHint && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <div className="flex items-center">
+                  <FaCircleInfo color="#D1D5DB" />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <div className="max-w-44">{field.displayNameHint}</div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
       </div>
       <div>
         <InputComponent
           id={id}
           value={value}
           onChange={onChange}
-          displayNameHint={field.displayNameHint}
           className={mergeClasses(
             "!h-9 !rounded-md font-medium !shadow-none",
             !isValid && "border-red-500"
