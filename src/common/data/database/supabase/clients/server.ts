f1c9da932f90c4bc97cda5d3ff005a60ce4408a7
Jesse Paterson <jesse.r.pat@gmail.com>
2025-04-11 22:06:51 -0500

    trying out invalidating the cache on server requests

--- src/common/data/database/supabase/clients/server.ts
+++ src/common/data/database/supabase/clients/server.ts
@@ -4,6 +4,15 @@ import { Database as SupabaseDataBaseType } from "@/supabase/database";
 const supabaseClient = createClient<SupabaseDataBaseType>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+    },
+  }
 );
 
 export default supabaseClient;
+
+
+
