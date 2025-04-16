452798596e4f05f26282ee1f48ad3b267b819e48
sktbrd <sktbrd.eth@gmail.com>
2025-04-16 15:43:19 -0300

    fix: update frames.js dependency to version 0.22.0 and update related imports in route.ts

--- src/app/frames/route.ts
+++ src/app/frames/route.ts
@@ -1,83 +1,81 @@
-// import { POST as handlePOSTRequest } from "@frames.js/render/next";
-// import { type NextRequest } from "next/server";
-// import { getAction } from "../../fidgets/framesv2/actions/getAction";
-// import { persistMockResponsesForDebugHubRequests } from "../../fidgets/framesv2/lib/mock-hub-utils";
-// import { parseFramesWithReports } from "frames.js/parseFramesWithReports";
-// // import { parseFramesWithReports } from "./frames.js/src/parseFramesWithReports";
-// import { isSpecificationValid,
-//   type CastActionDefinitionResponse,
-//   type FrameDefinitionResponse
-//  } from "../../fidgets/framesv2/lib/utils";
+import { POST as handlePOSTRequest } from "@frames.js/render/next";
+import { type NextRequest } from "next/server";
+import { getAction } from "../../fidgets/framesv2/actions/getAction";
+import { parseFramesWithReports } from "frames.js/parseFramesWithReports";
+// import { parseFramesWithReports } from "./frames.js/src/parseFramesWithReports";
+import { isSpecificationValid,
+  type CastActionDefinitionResponse,
+  type FrameDefinitionResponse } from "frames.js/parseFramesWithReports";
 
-// /** Proxies fetching a frame through a backend to avoid CORS issues and preserve user IP privacy */
-// export async function GET(request: NextRequest): Promise<Response> {
-//   const url = request.nextUrl.searchParams.get("url");
-//   const specification =
-//     request.nextUrl.searchParams.get("specification") ?? "farcaster_v2";
-//   const shouldParseActions = request.nextUrl.searchParams.get("actions");
+/** Proxies fetching a frame through a backend to avoid CORS issues and preserve user IP privacy */
+export async function GET(request: NextRequest): Promise<Response> {
+  const url = request.nextUrl.searchParams.get("url");
+  const specification =
+    request.nextUrl.searchParams.get("specification") ?? "farcaster_v2";
+  const shouldParseActions = request.nextUrl.searchParams.get("actions");
 
-//   if (!url) {
-//     return Response.json({ message: "Invalid URL" }, { status: 400 });
-//   }
+  if (!url) {
+    return Response.json({ message: "Invalid URL" }, { status: 400 });
+  }
 
-//   if (!isSpecificationValid(specification)) {
-//     return Response.json({ message: "Invalid specification" }, { status: 400 });
-//   }
+  if (!isSpecificationValid(specification)) {
+    return Response.json({ message: "Invalid specification" }, { status: 400 });
+  }
 
-//   try {
-//     const urlRes = await fetch(url);
+  try {
+    const urlRes = await fetch(url);
 
-//     // If content type is JSON it could be an action
-//     if (
-//       urlRes.headers.get("content-type")?.includes("application/json") &&
-//       specification === "farcaster" &&
-//       shouldParseActions
-//     ) {
-//       const json = (await urlRes.json()) as object;
+    // If content type is JSON it could be an action
+    if (
+      urlRes.headers.get("content-type")?.includes("application/json") &&
+      specification === "farcaster" &&
+      shouldParseActions
+    ) {
+      const json = (await urlRes.json()) as object;
 
-//       const result = getAction({ json, specification });
+      const result = getAction({ json, specification });
 
-//       return Response.json({
-//         ...result,
-//         type: "action",
-//         url,
-//       } satisfies CastActionDefinitionResponse);
-//     }
+      return Response.json({
+        ...result,
+        type: "action",
+        url,
+      } satisfies CastActionDefinitionResponse);
+    }
 
-//     const html = await urlRes.text();
+    const html = await urlRes.text();
 
-//     const parseResult = await parseFramesWithReports({
-//       html,
-//       frameUrl: url,
-//       fallbackPostUrl: url,
-//       fromRequestMethod: "GET",
-//       parseSettings: {
-//         farcaster_v2: {
-//           parseManifest: true,
-//           strict: false,
-//         },
-//       },
-//     });
+    const parseResult = await parseFramesWithReports({
+      html,
+      frameUrl: url,
+      fallbackPostUrl: url,
+      fromRequestMethod: "GET",
+      parseSettings: {
+        farcaster_v2: {
+          parseManifest: true,
+          strict: false,
+        },
+      },
+    });
 
-//     return Response.json({
-//       ...parseResult,
-//       type: "frame",
-//     } satisfies FrameDefinitionResponse);
-//   } catch (err) {
-//     // eslint-disable-next-line no-console -- provide feedback to the developer
-//     console.error(err);
+    return Response.json({
+      ...parseResult,
+      type: "frame",
+    } satisfies FrameDefinitionResponse);
+  } catch (err) {
+    // eslint-disable-next-line no-console -- provide feedback to the developer
+    console.error(err);
 
-//     if (err instanceof Error) {
-//       return Response.json({ message: err.message }, { status: 500 });
-//     }
+    if (err instanceof Error) {
+      return Response.json({ message: err.message }, { status: 500 });
+    }
 
-//     return Response.json({ message: err }, { status: 500 });
-//   }
-// }
+    return Response.json({ message: err }, { status: 500 });
+  }
+}
 
-// /** Proxies frame actions to avoid CORS issues and preserve user IP privacy */
-// export async function POST(req: NextRequest): Promise<Response> {
-//   // await persistMockResponsesForDebugHubRequests(req);
+/** Proxies frame actions to avoid CORS issues and preserve user IP privacy */
+export async function POST(req: NextRequest): Promise<Response> {
+  // await persistMockResponsesForDebugHubRequests(req);
 
-//   return handlePOSTRequest(req);
-// }
+  return handlePOSTRequest(req);
+}
