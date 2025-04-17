9c900a60e7bff1ad249998b97666c46a86196107
sktbrd <sktbrd.eth@gmail.com>
2025-04-17 20:52:48 -0300

    [WIP] - shrunkable navigation bar

---
+++ src/common/components/atoms/icons/SearchIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SearchIcon = () => (
+  <svg
+    className="w-6 h-6 text-gray-800 dark:text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth="2"
+      d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+    />
+  </svg>
+);
+
+export default SearchIcon;
