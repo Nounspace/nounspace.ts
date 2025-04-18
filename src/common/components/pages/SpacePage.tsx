c85c7fea09183aff58227ae27eb5d6cbc4b4e2ec
Ricardo Ferrari <rferrariwd@gmail.com>
2025-04-18 00:04:43 -0300

    fix: spacepage

--- src/common/components/pages/SpacePage.tsx
+++ src/common/components/pages/SpacePage.tsx
@@ -3,7 +3,6 @@ import Space, { SpaceConfig, SpaceConfigSaveDetails } from "../templates/Space";
 import { isUndefined } from "lodash";
 import SpaceLoading from "@/common/components/templates/SpaceLoading";
 import { useSidebarContext } from "@/common/components/organisms/Sidebar";
-import FrameV2Fidget from "@/fidgets/framesv2/framev2";
 
 
 export type SpacePageArgs = {
@@ -39,27 +38,19 @@ export default function SpacePage({
         loading ? (
         <SpaceLoading profile={profile} tabBar={tabBar} inEditMode={editMode} />
       ) : (
-        <FrameV2Fidget searchParams={{ 
-          // url: "https://04e0-2804-14d-5c5e-67cc-5046-5ea8-6517-c54b.ngrok-free.app/frames/hello/vaipraonde", 
-          // url: "https://www.skatehive.app/post/hive-173115/@samuelvelizsk8/fegxlhrj",
-          url: "https://www.builderscore.xyz/",
-          specification:"farcaster_v2", 
-          actions:"true" 
-          }} />
-
-        // <Space
-        //   config={config}
-        //   saveConfig={saveConfig}
-        //   commitConfig={commitConfig}
-        //   resetConfig={resetConfig}
-        //   tabBar={tabBar}
-        //   profile={profile}
-        //   feed={feed}
-        //   setEditMode={setEditMode}
-        //   editMode={editMode}
-        //   setSidebarEditable={setSidebarEditable}
-        //   portalRef={portalRef}
-        // />
+        <Space
+          config={config}
+          saveConfig={saveConfig}
+          commitConfig={commitConfig}
+          resetConfig={resetConfig}
+          tabBar={tabBar}
+          profile={profile}
+          feed={feed}
+          setEditMode={setEditMode}
+          editMode={editMode}
+          setSidebarEditable={setSidebarEditable}
+          portalRef={portalRef}
+        />
       )}
     </>
   );
