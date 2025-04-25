c75162ecfc829b75f3a957d4037da6b269baba44
Ricardo Ferrari <rferrariwd@gmail.com>
2025-04-24 22:06:07 -0300

    fix: farcaster neynar publishMessage

--- src/pages/api/farcaster/neynar/publishMessage.ts
+++ src/pages/api/farcaster/neynar/publishMessage.ts
@@ -1,13 +1,18 @@
 import neynar from "@/common/data/api/neynar";
 import requestHandler from "@/common/data/api/requestHandler";
+// import { MessageData } from "@farcaster/core";
 import { isAxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
+
 async function publishMessage(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const result = await neynar.publishMessageToFarcaster(req.body);
+    const data = { ...req.body };
+    const message = { body: data };
+    const result = await neynar.publishMessageToFarcaster(message);
     res.status(200).json(result);
-  } catch (e) {
+    console.log(result)
+  } catch (e: any) {
     if (isAxiosError(e)) {
       res.status(e.status || 500).json(e.response?.data);
     } else {
