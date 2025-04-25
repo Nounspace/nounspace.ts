80bcbdc4d4c2bfb44f87b6ce9b7d5f5d9c12e1a3
Ricardo Ferrari <rferrariwd@gmail.com>
2025-04-25 00:52:44 -0300

    fix: message publish data

--- src/common/data/api/neynar.ts
+++ src/common/data/api/neynar.ts
@@ -1,3 +1,3 @@
-import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { NeynarV2APIClient } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 
-export default new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
+export default new NeynarV2APIClient(process.env.NEYNAR_API_KEY!);
--- src/pages/api/farcaster/neynar/publishMessage.ts
+++ src/pages/api/farcaster/neynar/publishMessage.ts
@@ -1,18 +1,21 @@
 import neynar from "@/common/data/api/neynar";
 import requestHandler from "@/common/data/api/requestHandler";
-// import { MessageData } from "@farcaster/core";
 import { isAxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
 async function publishMessage(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const data = { ...req.body };
-    const message = { body: data };
-    const result = await neynar.publishMessageToFarcaster(message);
-    res.status(200).json(result);
-    console.log(result)
+    const message = {
+      ...req.body.data
+    };
+    const response = await neynar.publishMessageToFarcaster(message);
+    res.status(200).json(response);
   } catch (e: any) {
+    if (e.response?.data) {
+      console.error("response.data")
+      console.dir(e.response.data, { depth: null });
+    }
     if (isAxiosError(e)) {
       res.status(e.status || 500).json(e.response?.data);
     } else {
