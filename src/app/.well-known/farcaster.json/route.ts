24512d71211d94fabd40abc542e023ae592e8a44
r4to <r4topunk.eth@gmail.com>
2025-04-29 16:35:32 -0300

    ♻️ refactor: comment out accountAssociation in GET config
    Removed the accountAssociation object from the GET route's config
    for cleaner code and to avoid unnecessary exposure of sensitive data.

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,14 +4,11 @@ console.log("WEBSITE_URL", WEBSITE_URL);
 
 export async function GET() {
   const config = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjE5NjMyOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDMwM0JlMDNiMkExQjVlMkViQjQzZEQ0NkEyYWFDZjk1YzJhQTVBODQifQ",
-      payload:
-        "eyJkb21haW4iOiJlY29tbWVyY2UtdmFyaWFuY2UtYnJvb2tseW4tZmVycnkudHJ5Y2xvdWRmbGFyZS5jb20ifQ",
-      signature:
-        "MHg5NzRjY2JkMmJiZjEyOGM0ZjBlOWY5NDZhYmEyZDBlMzk2ZGQ3MzU0ZmE4NjBkNGM1MzQ0MGEwMzZmNzdiMjZiMjlkNzczNGFjYmYzMjU0N2ZjZjQ1ODExZDk1NGFiMWRlMGY3MDUyYmY4NTA3ODgxYjdjM2VkNTU1NDI1MzEzYjFi",
-    },
+    // accountAssociation: {
+      // header: "",
+      // payload: "",
+      // signature: "",
+    // },
     frame: {
       version: "1",
       name: "Nounspace",
