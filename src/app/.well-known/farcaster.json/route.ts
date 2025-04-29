075c53c3fd8340e390dd221a8c693b917c10fb8e
r4to <r4topunk.eth@gmail.com>
2025-04-29 16:11:15 -0300

    ✨ feat: update Nounspace configuration to use WEBSITE_URL constant
    Replaced direct usage of process.env.NEXT_PUBLIC_URL with
    WEBSITE_URL constant for improved consistency and maintainability.
    Updated account association and frame properties accordingly.

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,19 +1,25 @@
-export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_URL;
+import { WEBSITE_URL } from "../../../constants/app";
 
+console.log("WEBSITE_URL", WEBSITE_URL);
+
+export async function GET() {
   const config = {
     accountAssociation: {
-      header: "",
-      payload: "",
-      signature: "",
+      header:
+        "eyJmaWQiOjE5NjMyOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDMwM0JlMDNiMkExQjVlMkViQjQzZEQ0NkEyYWFDZjk1YzJhQTVBODQifQ",
+      payload:
+        "eyJkb21haW4iOiJlY29tbWVyY2UtdmFyaWFuY2UtYnJvb2tseW4tZmVycnkudHJ5Y2xvdWRmbGFyZS5jb20ifQ",
+      signature:
+        "MHg5NzRjY2JkMmJiZjEyOGM0ZjBlOWY5NDZhYmEyZDBlMzk2ZGQ3MzU0ZmE4NjBkNGM1MzQ0MGEwMzZmNzdiMjZiMjlkNzczNGFjYmYzMjU0N2ZjZjQ1ODExZDk1NGFiMWRlMGY3MDUyYmY4NTA3ODgxYjdjM2VkNTU1NDI1MzEzYjFi",
     },
     frame: {
       version: "1",
       name: "Nounspace",
-      homeUrl: appUrl,
-      iconUrl: `${appUrl}/images/frames/icon.png`,
-      splashImageUrl: `${appUrl}/images/frames/splash.png`,
-      splashBackgroundColor: "#1C1C1C",
+      homeUrl: WEBSITE_URL,
+      iconUrl: `${WEBSITE_URL}/images/frames/icon.png`,
+      splashImageUrl: `${WEBSITE_URL}/images/frames/splash.png`,
+      webhookUrl: `${WEBSITE_URL}/api/farcaster/webhook`,
+      splashBackgroundColor: "#FFFFFF",
     },
   };
 
