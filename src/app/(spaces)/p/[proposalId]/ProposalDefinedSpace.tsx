47046e31442a98189cf36d49ea1d129a84fc8ee5
r4to <r4topunk.eth@gmail.com>
2025-05-01 21:20:11 -0300

    ♻️ refactor: remove mobile responsiveness logic from ProposalDefinedSpace
    Eliminated unused state and effect hooks related to mobile detection.
    This simplifies the component and focuses on rendering the desktop version.

--- src/app/(spaces)/p/[proposalId]/ProposalDefinedSpace.tsx
+++ src/app/(spaces)/p/[proposalId]/ProposalDefinedSpace.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 import { ProposalData } from "@/fidgets/community/nouns-dao";
 
 export interface ProposalPageSpaceProps {
@@ -14,30 +13,10 @@ export interface ProposalPageSpaceProps {
   owningIdentities?: string[];
 }
 const ProposalDefinedSpace = (props: ProposalPageSpaceProps) => {
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <>
-      {/* {isMobile ? (
-        <MobileContractDefinedSpace {...props} />
-      ) : ( */}
-      <div className="w-full">
-        <DynamicDesktopContractDefinedSpace {...props} />
-      </div>
-      {/* )} */}
-    </>
+    <div className="w-full">
+      <DynamicDesktopContractDefinedSpace {...props} />
+    </div>
   );
 };
 
