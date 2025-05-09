0226d70c2d60b5bd8c3121bac24c927496aa72cc
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-09 16:32:22 -0400

    Update onboarding guide
    Updated and fixed broken GIFs and made minor tweaks to copy

--- src/constants/intialHomebase.ts
+++ src/constants/intialHomebase.ts
@@ -1,23 +1,21 @@
 import { SpaceConfig } from "@/app/(spaces)/Space";
 import DEFAULT_THEME from "@/common/lib/theme/defaultTheme";
 const tutorialText = `
- ## To start customizing, click the paintbrush in the bottom left next to 'Cast' ⬋🖌️⬋
+ ## To start customizing, click the paintbrush in the bottom left ⬋🖌️⬋
 
 ### Add Fidgets
-From customization mode, click the big blue **+** button. Then, drag a Fidget to an open spot on the grid. Finally, click Save so you can scroll down here for more instructions. 
+From customization mode, click the blue **+** button. Then, drag a Fidget to an open spot on the grid. Finally, click Save and scroll down here for more instructions. 
 
-![DragFidget-ezgif.com-loop-count](https://hackmd.io/_uploads/S1MTNDTsC.gif)
-
-scroll down for more ⬇️ ⬇️ ⬇️ 
+![Add Fidget2](https://hackmd.io/_uploads/B15DHk2exl.gif)
 
 ### Customize Fidgets
-Click any Fidget on the grid to open its settings. In addition to configuring each Fidget's settings, you can customize its look in the 'Style' tab. Any Fidget style parameters set to "Theme" inherit their look from the theme of their Tab.  
+From customization mode, click any Fidget on the grid to open its settings. In addition to customizing each Fidget's settings, you can customize its look in the 'Style' tab. Any Fidget style settings set to "Theme" inherit their look from the theme of their Tab.  
 
 ![EditFidget-ezgif.com-loop-count](https://hackmd.io/_uploads/r1pTND6s0.gif)
 
 ### Arrange Fidgets
 - **Move:** Click and drag fidgets from the middle to move them
-![move fidget](https://nounspace.mypinata.cloud/ipfs/QmYWvdpdiyKwjVAqjhcFTBkiTUnc8rF4p2EGg3C4sTRsr6)
+![move fidget](https://space.mypinata.cloud/ipfs/QmYWvdpdiyKwjVAqjhcFTBkiTUnc8rF4p2EGg3C4sTRsr6)
 - **Resize:** Click and drag fidgets' edges to resize them
 ![2024-08-28 21.05.59](https://hackmd.io/_uploads/ryxADUpjC.gif)
 - **Stash in Fidget Tray:** Click a fidget then click the ⇱ icon above it to save it in your Fidget Tray for later.
@@ -29,19 +27,22 @@ Click any Fidget on the grid to open its settings. In addition to configuring ea
 - **Templates:** One quick and easy option is to select a template. Then if you'd like, you can further customize to make it your own.
 - **Style:** Set a background color or gradient for each Tab, or set the default color, border, and shadows for all Fidgets on the selected Tab.
 - **Fonts:** Set the default header and body fonts for Fidgets on your space.
-- **Code:** Add HTML/CSS to fully customize your space's background. 
+- **Code:** Add HTML/CSS to fully customize your space's background, or generate a custom background with a prompt. 
 
-![Edit Theme](https://hackmd.io/_uploads/Sk7sWw6sC.gif)
+![Edit Theme2](https://hackmd.io/_uploads/HJNNpRiglx.gif)
 
 ### Customize Music
 Last but not least, search for or paste the link to any song or playlist on YouTube to play it for yourself on your homebase or on your space for your friends.
+
+![customize music](https://hackmd.io/_uploads/rkvjARsxll.gif)
+
 
 ### Homebase vs. Space
 **Your Homebase** is a private space that only you can see.
 
 **Your Space** is your public profile that everyone can see.
 
-You can use the same Fidgets and tricks to customize them both. Use your **Homebase** to access the content, communities, and functionality that you love, and use your **Space** to share the content and functionality you love with your friends.
+You can use the same tricks and Fidgets to customize them both. Use your **Homebase** to access the content, communities, and functionality you love, and use your **Space** to share the content and functionality you love with your friends.
 
 `;
 const onboardingFidgetID = "text:onboarding";
