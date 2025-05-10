6ee79554d6ea9841890cf6249f1c33c35b82d26c
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-10 14:24:12 -0500

    Update onboarding guide (#710)

--- src/constants/intialHomebase.ts
+++ src/constants/intialHomebase.ts
@@ -1,55 +1,64 @@
 import { SpaceConfig } from "@/app/(spaces)/Space";
 import DEFAULT_THEME from "@/common/lib/theme/defaultTheme";
 const tutorialText = `
- ## To start customizing, click the paintbrush in the bottom left next to 'Cast' ⬋🖌️⬋
+### 🖌️ Click the paintbrush in the bottom-left corner to open Customization Mode
 
 ### Add Fidgets
-From customization mode, click the big blue **+** button. Then, drag a Fidget to an open spot on the grid. Finally, click Save so you can scroll down here for more instructions. 
+1. Click the blue **+** button.
+2. Drag a Fidget to an open spot on the grid.
+3. Click Save
 
-![DragFidget-ezgif.com-loop-count](https://hackmd.io/_uploads/S1MTNDTsC.gif)
+(after saving, scroll down here for more instructions)
 
-scroll down for more ⬇️ ⬇️ ⬇️ 
+![Add Fidget2](https://space.mypinata.cloud/ipfs/bafkreiczpd2bzyoboj6uxr65kta5cmg3bziveq2nz5egx4fuxr2nmkthru)
 
 ### Customize Fidgets
-Click any Fidget on the grid to open its settings. In addition to configuring each Fidget's settings, you can customize its look in the 'Style' tab. Any Fidget style parameters set to "Theme" inherit their look from the theme of their Tab.  
+1. From customization mode, click any Fidget on the grid to open its settings.
+2. Click 'Style' to customize a fidget's look. Any Fidget styles set to "Theme" inherit their look from the Tab's theme.  
 
-![EditFidget-ezgif.com-loop-count](https://hackmd.io/_uploads/r1pTND6s0.gif)
+![EditFidget](https://space.mypinata.cloud/ipfs/bafybeihcjkbcljxr4ttgt6xcxmsc4m4qvw62hkolifmd3t5pdc7kvj5nji)
 
 ### Arrange Fidgets
-- **Move:** Click and drag fidgets from the middle to move them
-![move fidget](https://nounspace.mypinata.cloud/ipfs/QmYWvdpdiyKwjVAqjhcFTBkiTUnc8rF4p2EGg3C4sTRsr6)
-- **Resize:** Click and drag fidgets' edges to resize them
-![2024-08-28 21.05.59](https://hackmd.io/_uploads/ryxADUpjC.gif)
-- **Stash in Fidget Tray:** Click a fidget then click the ⇱ icon above it to save it in your Fidget Tray for later.
-![image](https://hackmd.io/_uploads/Syz8wUajC.png)
-- **Delete:** Click a fidget then click the X icon above it to delete it forever.
-![image](https://hackmd.io/_uploads/SyhwvLpoR.png)
+- **Move:** Drag from the center
+![move fidget](https://space.mypinata.cloud/ipfs/QmYWvdpdiyKwjVAqjhcFTBkiTUnc8rF4p2EGg3C4sTRsr6)
+- **Resize:** Drag from an edge or corner
+![Resize Fidget](https://space.mypinata.cloud/ipfs/bafybeifmssfizx5xjmmqyc6wwqxgs2xdhhbdtnvldtj276mfdul3eacmwu)
+- **Stash in Fidget Tray:** Click a fidget then click ⇱ to save it for later.
+![image](https://space.mypinata.cloud/ipfs/bafkreigy7ymnuwertvr6bn4bmwfe3vu3vvw4r6ekkzv6prs4zwlibsjtya)
+- **Delete:** Click a fidget then click X it to delete it forever.
+![image](https://space.mypinata.cloud/ipfs/bafkreieucvjlovm7wq5ftcvvvcv52sujnqqjwji5epoigb2ycumhmgrtfy)
 
 ### Customize Theme
-- **Templates:** One quick and easy option is to select a template. Then if you'd like, you can further customize to make it your own.
-- **Style:** Set a background color or gradient for each Tab, or set the default color, border, and shadows for all Fidgets on the selected Tab.
-- **Fonts:** Set the default header and body fonts for Fidgets on your space.
-- **Code:** Add HTML/CSS to fully customize your space's background. 
+- **Templates:** Select a pre-made Theme. Then, customize it further to make it your own.
+- **Style:** Set a background color for the Tab, or set the default styles for all Fidgets on the Tab.
+- **Fonts:** Set the default header and body fonts for Fidgets on the Tab.
+- **Code:** Add HTML/CSS to fully customize the Tab's background, or generate a custom background with a prompt. 
 
-![Edit Theme](https://hackmd.io/_uploads/Sk7sWw6sC.gif)
+![Edit Theme2](https://space.mypinata.cloud/ipfs/bafybeietizt4vgyaiv62ytn25ztanusjmbcw6iwm3v2gedcpipr6koxh3e)
 
 ### Customize Music
-Last but not least, search for or paste the link to any song or playlist on YouTube to play it for yourself on your homebase or on your space for your friends.
+Add a soundtrack to each Tab. Search for or paste the link to any song or playlist on YouTube, or select a music NFT.
+
+![customize music](https://space.mypinata.cloud/ipfs/bafkreigtslrp3kjj42gp25bxd5nubs47qx22ivj3pkm3tc7j3fbqt3b5hy)
 
 ### Homebase vs. Space
-**Your Homebase** is a private space that only you can see.
-
 **Your Space** is your public profile that everyone can see.
+**Your Homebase** is a private dashboard that only you can see.
 
-You can use the same Fidgets and tricks to customize them both. Use your **Homebase** to access the content, communities, and functionality that you love, and use your **Space** to share the content and functionality you love with your friends.
+You can use the same tricks and Fidgets to customize them both. Use your **Homebase** to access the content, communities, and functionality you love, and use your **Space** to share the content and functionality you love with your friends.
 
+### Questions or feedback?
+
+Tag [@nounspacetom](https://nounspace.com/s/nounspacetom) in a cast or join our [Discord](https://discord.gg/H8EYnEmj6q).
+
+### Happy customizing!
 `;
 const onboardingFidgetID = "text:onboarding";
 const onboardingFidgetConfig = {
   config: {
     editable: true,
     settings: {
-      title: "Welcome to Nounspace! 🚀 👾",
+      title: "",
       text: tutorialText,
       urlColor: "blue",
       fontFamily: "Londrina Solid",
