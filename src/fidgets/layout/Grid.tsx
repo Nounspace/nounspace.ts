6d3fca88df31da6aecc7017c3ca0f2feb722d2a9
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-19 18:15:22 -0500

    Fix use of app store context in grid

--- src/fidgets/layout/Grid.tsx
+++ src/fidgets/layout/Grid.tsx
@@ -5,6 +5,7 @@ import React, {
   useState,
   useCallback,
   useRef,
+  useContext,
 } from "react";
 import useWindowSize from "@/common/lib/hooks/useWindowSize";
 import RGL, { WidthProvider } from "react-grid-layout";
@@ -34,6 +35,7 @@ import AddFidgetIcon from "@/common/components/atoms/icons/AddFidget";
 import FidgetSettingsEditor from "@/common/components/organisms/FidgetSettingsEditor";
 import { debounce } from "lodash";
 import { updateFidgetInstanceDatums } from "./updateFidgetInstanceDatums";
+import { AppStoreContext } from "@/common/data/stores/app";
 
 export const resizeDirections = ["s", "w", "e", "n", "sw", "nw", "se", "ne"];
 export type ResizeDirection = (typeof resizeDirections)[number];
@@ -136,6 +138,10 @@ const Grid: LayoutFidget<GridLayoutProps> = ({
   hasFeed,
   fid,
 }) => {
+  const store = useContext(AppStoreContext);
+  if (!store) {
+    throw new Error("AppStoreContext is not available");
+  }
   // State to handle selecting, dragging, and Grid edit functionality
   const [element, setElement] = useState<HTMLDivElement | null>(
     portalRef.current,
@@ -394,6 +400,13 @@ const Grid: LayoutFidget<GridLayoutProps> = ({
   const addFidgetToGrid = (fidget: FidgetBundle): boolean => {
     const { fidgetType, id } = fidget;
 
+    const existingLayout =
+      store.getState().homebase.homebaseConfig?.layoutDetails.layoutConfig
+        .layout ?? layoutConfig.layout;
+    const existingDatums =
+      store.getState().homebase.homebaseConfig?.fidgetInstanceDatums ??
+      fidgetInstanceDatums;
+
     // Prevent duplicates
     const alreadyPlaced = layoutConfig.layout.some((item) => item.i === id);
     if (alreadyPlaced) {
@@ -443,8 +456,8 @@ const Grid: LayoutFidget<GridLayoutProps> = ({
 
           // Save both layout and fidgetInstanceDatums in a single operation
           debouncedSaveConfig({
-            layoutConfig: { layout: [...layoutConfig.layout, newItem] },
-            fidgetInstanceDatums: { ...fidgetInstanceDatums, [id]: fidget },
+            layoutConfig: { layout: [...existingLayout, newItem] },
+            fidgetInstanceDatums: { ...existingDatums, [id]: fidget },
           });
 
           analytics.track(AnalyticsEvent.ADD_FIDGET, {
