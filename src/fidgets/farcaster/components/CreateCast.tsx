1daab5083d29224ad83498ca895f31a1e9967f72
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-20 15:59:40 -0500

    fix embed type import

--- src/fidgets/farcaster/components/CreateCast.tsx
+++ src/fidgets/farcaster/components/CreateCast.tsx
@@ -5,6 +5,7 @@ import {
   FarcasterNetwork,
   CastAddBody,
 } from "@farcaster/core";
+import { Embed } from "@mod-protocol/core";
 
 import { useEditor, EditorContent } from "@mod-protocol/react-editor";
 import {
@@ -151,7 +152,7 @@ const CreateCast: React.FC<CreateCastProps> = ({
         e.preventDefault();
         const url = await uploadImage(file);
         if (url) {
-          addEmbed({ url });
+          addEmbed({ url } as Embed);
         }
       }
     }
