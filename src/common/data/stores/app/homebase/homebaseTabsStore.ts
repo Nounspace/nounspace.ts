17aec9e23e382d4c9aace7f382039a5e7b949da0
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-20 14:48:59 -0500

    Filter permanent tabs when saving homebase order

--- src/common/data/stores/app/homebase/homebaseTabsStore.ts
+++ src/common/data/stores/app/homebase/homebaseTabsStore.ts
@@ -116,13 +116,11 @@ export const createHomeBaseTabStoreFunc = (
 ): HomeBaseTabStore => ({
   ...homeBaseStoreDefaults,
   updateTabOrdering(newOrdering, commit = false) {
-    // console.log('Updating tab ordering:', {
-    //   newOrder: newOrdering,
-    //   commit
-    // });
-    
+    const filtered = newOrdering.filter((name, index) => {
+      return name !== "Feed" && newOrdering.indexOf(name) === index;
+    });
     set((draft) => {
-      draft.homebase.tabOrdering.local = newOrdering;
+      draft.homebase.tabOrdering.local = filtered;
     }, "updateTabOrdering");
     if (commit) {
       get().homebase.commitTabOrderingToDatabase();
@@ -173,7 +171,11 @@ export const createHomeBaseTabStoreFunc = (
     }
   },
   commitTabOrderingToDatabase: debounce(async () => {
-    const localCopy = cloneDeep(get().homebase.tabOrdering.local);
+    const localCopy = cloneDeep(
+      get().homebase.tabOrdering.local.filter((name, i, arr) =>
+        name !== "Feed" && arr.indexOf(name) === i,
+      ),
+    );
     if (localCopy) {
       // console.log('Committing tab ordering to database:', {
       //   tabCount: localCopy.length,
