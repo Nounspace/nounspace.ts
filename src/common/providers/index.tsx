4832cfcb104e1db79ce95f91d07499864a4aff78
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-20 09:02:11 -0500

    fix: reset scroll on mobile page change

--- src/common/providers/index.tsx
+++ src/common/providers/index.tsx
@@ -9,6 +9,7 @@ import { AppStoreProvider } from "@/common/data/stores/app";
 import UserThemeProvider from "@/common/lib/theme/UserThemeProvider";
 import LoggedInStateProvider from "./LoggedInStateProvider";
 import AnalyticsProvider from "./AnalyticsProvider";
+import MobileScrollToTopProvider from "./MobileScrollToTopProvider";
 import VersionCheckProivder from "./VersionCheckProvider";
 import { SidebarContextProvider } from "@/common/components/organisms/Sidebar";
 import { ToastProvider } from "../components/atoms/Toast";
@@ -27,9 +28,11 @@ export default function Providers({ children }: { children: React.ReactNode }) {
                     <LoggedInStateProvider>
                       <SidebarContextProvider>
                         <AnalyticsProvider>
-                          <MiniAppSdkProvider>
-                            <ToastProvider>{children}</ToastProvider>
-                          </MiniAppSdkProvider>
+                          <MobileScrollToTopProvider>
+                            <MiniAppSdkProvider>
+                              <ToastProvider>{children}</ToastProvider>
+                            </MiniAppSdkProvider>
+                          </MobileScrollToTopProvider>
                         </AnalyticsProvider>
                       </SidebarContextProvider>
                     </LoggedInStateProvider>
