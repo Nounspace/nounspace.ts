22200c33f99a1363c0289fbad61d00a27657b53a
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-23 16:02:36 -0500

    increase padding

--- src/pages/api/metadata/token.tsx
+++ src/pages/api/metadata/token.tsx
@@ -86,7 +86,7 @@ const TokenCard = ({ data }: { data: TokenCardData }) => {
           display: "flex",
           flexDirection: "column",
           gap: "12px",
-          paddingLeft: "60px",
+          paddingLeft: "80px",
         }}
       >
         <span style={{ fontSize: "56px", fontWeight: "bold" }}>{data.name}</span>
