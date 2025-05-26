6ddda16c29378f7694117839a67e09ceadeaea89
Willy Ogorzaly <willy@ogorzaly.com>
2025-05-26 14:24:12 -0500

    fix space config hooks

---
+++ src/common/data/queries/spaceConfig.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAppStore } from "../stores/app";
+
+export const useHomebaseTabConfig = (tabName: string) => {
+  const loadTab = useAppStore((state) => state.homebase.loadHomebaseTab);
+  return useQuery({
+    queryKey: ["homebase-tab-config", tabName],
+    suspense: true,
+    queryFn: () => loadTab(tabName),
+  });
+};
+
+export const useSpaceTabConfig = (spaceId: string | null, tabName: string) => {
+  const loadTab = useAppStore((state) => state.space.loadSpaceTab);
+  return useQuery({
+    queryKey: ["space-tab-config", spaceId, tabName],
+    enabled: !!spaceId,
+    suspense: true,
+    queryFn: () => (spaceId ? loadTab(spaceId, tabName) : Promise.resolve()),
+  });
+};
