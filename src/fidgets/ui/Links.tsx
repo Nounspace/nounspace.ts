b68c2391f8610e58bcac635fd86fe107e479e903
Willy Ogorzaly <willy@ogorzaly.com>
2025-06-05 12:01:03 -0500

    Refine Links hover scaling

--- src/fidgets/ui/Links.tsx
+++ src/fidgets/ui/Links.tsx
@@ -339,13 +339,14 @@ export const Links: React.FC<FidgetArgs<LinkFidgetSettings>> = ({
               href={link.url}
               target="_blank"
               rel="noopener noreferrer"
+              className="block"
             >
               <CardContent
 
                 className={
                   isGridView
-                    ? "p-4 flex flex-col items-start justify-between m-1 transform-gpu transition-transform duration-150 ease-out will-change-transform hover:scale-105"
-                    : "p-2 flex items-center justify-between m-2 transform-gpu transition-transform duration-150 ease-out will-change-transform hover:scale-105"
+                    ? "relative p-4 flex flex-col items-start justify-between m-1 transform-gpu transition-transform duration-150 ease-out will-change-transform hover:scale-[1.02]"
+                    : "relative p-2 flex items-center justify-between m-2 transform-gpu transition-transform duration-150 ease-out will-change-transform hover:scale-[1.02]"
                 }
                 key={index}
               >
