e838d196dfde148e24c841868aa4b495ae3c3f61
Jesse Paterson <jesse.r.pat@gmail.com>
2025-06-06 17:14:39 -0500

    got rid of redundant app folder

--- app/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Inter } from 'next/font/google';
-
-export const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
-  fallback: ['system-ui', 'arial'],
-  variable: '--font-inter',
-}); 
