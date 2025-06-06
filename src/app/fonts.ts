d4bbb4792f82cb1b058ed9126d137b5ca991f5c9
Jesse Paterson <jesse.r.pat@gmail.com>
2025-06-06 18:15:06 -0500

    post upgrade

---
+++ src/app/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+  fallback: ['system-ui', 'arial'],
+  variable: '--font-inter',
+}); 
