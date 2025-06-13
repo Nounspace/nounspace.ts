98eaf294b2443e70c4f13e1daf32207120cbaaa5
Willy Ogorzaly <willy@ogorzaly.com>
2025-06-13 03:40:40 -0500

    Add client directive to CastRow (#1144)

--- src/fidgets/farcaster/components/CastRow.tsx
+++ src/fidgets/farcaster/components/CastRow.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useCallback, useMemo } from "react";
 import { Properties } from "csstype";
 import { mergeClasses as classNames } from "@/common/lib/utils/mergeClasses";
