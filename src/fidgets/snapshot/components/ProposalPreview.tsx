efc123878e12d278ea70561fbfc79270c8bb7374
sktbrd <sktbrd.eth@gmail.com>
2025-06-14 14:03:06 -0300

    refactor: Memoize sanitization schema in ProposalPreview for performance optimization

--- src/fidgets/snapshot/components/ProposalPreview.tsx
+++ src/fidgets/snapshot/components/ProposalPreview.tsx
@@ -13,50 +13,53 @@ interface ProposalPreviewProps {
 const ProposalPreview: React.FC<ProposalPreviewProps> = memo(({ body }) => {
   const markdownComponents = useMemo(() => MarkdownRenderers(), []);
 
-  // Create a strict sanitization schema
-  const sanitizeSchema = {
-    ...defaultSchema,
-    attributes: {
-      ...defaultSchema.attributes,
-      // Remove dangerous attributes
-      "*": ["className", "id"],
-    },
-    tagNames: [
-      // Allow only safe HTML tags
-      "p",
-      "br",
-      "strong",
-      "em",
-      "u",
-      "s",
-      "del",
-      "ins",
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "ul",
-      "ol",
-      "li",
-      "blockquote",
-      "pre",
-      "code",
-      "a",
-      "img",
-      "table",
-      "thead",
-      "tbody",
-      "tr",
-      "th",
-      "td",
-    ],
-    protocols: {
-      href: ["http", "https", "mailto"],
-      src: ["http", "https"],
-    },
-  };
+  // Create a strict sanitization schema, memoized to avoid recreation on every render
+  const sanitizeSchema = useMemo(
+    () => ({
+      ...defaultSchema,
+      attributes: {
+        ...defaultSchema.attributes,
+        // Merge with default global attributes instead of replacing them
+        "*": [...(defaultSchema.attributes?.["*"] || []), "className", "id"],
+      },
+      tagNames: [
+        // Allow only safe HTML tags
+        "p",
+        "br",
+        "strong",
+        "em",
+        "u",
+        "s",
+        "del",
+        "ins",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "ul",
+        "ol",
+        "li",
+        "blockquote",
+        "pre",
+        "code",
+        "a",
+        "img",
+        "table",
+        "thead",
+        "tbody",
+        "tr",
+        "th",
+        "td",
+      ],
+      protocols: {
+        href: ["http", "https", "mailto"],
+        src: ["http", "https"],
+      },
+    }),
+    []
+  );
 
   return (
     <ReactMarkdown
