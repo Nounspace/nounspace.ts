52fe7fb51d81ace198b22da6fa64287c41416db6
Willy Ogorzaly <willy@ogorzaly.com>
2025-06-16 16:58:17 +0800

    Remove logo tooltip and adjust position

--- src/common/components/molecules/BrandHeader.tsx
+++ src/common/components/molecules/BrandHeader.tsx
@@ -1,52 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../atoms/tooltip";
-import { Tooltip, TooltipArrow } from "@radix-ui/react-tooltip";
-import { FaExternalLinkAlt } from "react-icons/fa";
-import { Londrina_Solid } from "next/font/google";
-
-const Londrina = Londrina_Solid({ subsets: ["latin"], weight: "400" });
 
 const BrandHeader = () => {
   return (
     <>
-      <TooltipProvider>
-        <Tooltip>
-          <Link
-            href="/home"
-            className="flex items-center ps-2.5"
-            rel="noopener noreferrer"
-          >
-            <TooltipTrigger asChild>
-              <Image
-                src="/images/noggles.svg"
-                className="h-13 me-3 "
-                alt="Nounspace Logo"
-                width={60}
-                height={40}
-              />
-            </TooltipTrigger>
-          </Link>
-          <TooltipContent className=" bg-gray-200 font-black" side="left">
-            <TooltipArrow className="fill-gray-200" />
-            <div className="flex flex-col gap-1">
-              <a
-                className={`text-black text-base ${Londrina.className}`}
-                href="https://nouns.wtf"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                wtf is nouns? <FaExternalLinkAlt className="inline ml-1 mb-1" />
-              </a>
-            </div>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <Link
+        href="/home"
+        className="flex items-center ps-2.5 -mt-[15px]"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/images/noggles.svg"
+          className="h-13 me-3"
+          alt="Nounspace Logo"
+          width={60}
+          height={40}
+        />
+      </Link>
 
       {false && (
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
