b269ca51899ff4dfafb710f6dd367c0d00ead9c8
Jesse Paterson <jesse.r.pat@gmail.com>
2025-06-17 15:19:28 +0800

    cleaning up stubs

--- src/app/(spaces)/homebase/[tabname]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import React from "react";
-
-const HomebaseTabPage = () => null;
-
-export default HomebaseTabPage; 
--- src/app/(spaces)/homebase/c/[caster]/[castHash]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import React from "react";
-
-const HomebaseCastPage = () => null;
-
-export default HomebaseCastPage; 
