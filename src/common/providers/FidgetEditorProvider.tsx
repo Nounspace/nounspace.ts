221817c3b8e814cfd422096f5600ac163f6b195d
Willy Ogorzaly <willy@ogorzaly.com>
2025-06-28 16:00:25 +0800

    share fidget editor state

---
+++ src/common/providers/FidgetEditorProvider.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+
+export interface FidgetEditorContextType {
+  selectedFidgetID: string;
+  setSelectedFidgetID: (id: string) => void;
+  currentFidgetSettings: React.ReactNode;
+  setCurrentFidgetSettings: (node: React.ReactNode) => void;
+}
+
+const FidgetEditorContext = createContext<FidgetEditorContextType | null>(null);
+
+export const FidgetEditorProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [selectedFidgetID, setSelectedFidgetID] = useState("");
+  const [currentFidgetSettings, setCurrentFidgetSettings] = useState<React.ReactNode>(<></>);
+
+  return (
+    <FidgetEditorContext.Provider
+      value={{
+        selectedFidgetID,
+        setSelectedFidgetID,
+        currentFidgetSettings,
+        setCurrentFidgetSettings,
+      }}
+    >
+      {children}
+    </FidgetEditorContext.Provider>
+  );
+};
+
+export const useFidgetEditorContext = () => useContext(FidgetEditorContext);
