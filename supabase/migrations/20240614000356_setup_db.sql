51c5627832717b397204fffc7b821ab49f4cae3a
sktbrd <sktbrd.eth@gmail.com>
2025-06-30 21:50:52 +0800

    fix: add missing "use client" directive and remove unused pgsodium extension

--- supabase/migrations/20240614000356_setup_db.sql
+++ supabase/migrations/20240614000356_setup_db.sql
@@ -12,8 +12,6 @@ SET row_security = off;
 
 CREATE EXTENSION IF NOT EXISTS "pg_net" WITH SCHEMA "extensions";
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
-
 COMMENT ON SCHEMA "public" IS 'standard public schema';
 
 CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
