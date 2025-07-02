0bd2ddef871fc4c847c38c4123cc4367ad40a570
Willy Ogorzaly <willy@ogorzaly.com>
2025-07-02 13:30:25 +0800

    Fix loading of saved token space

--- src/app/(spaces)/t/[network]/[contractAddress]/utils.ts
+++ src/app/(spaces)/t/[network]/[contractAddress]/utils.ts
@@ -2,6 +2,7 @@ import createSupabaseServerClient from "@/common/data/database/supabase/clients/
 import { SignedFile, isSignedFile } from "@/common/lib/signedFiles";
 import { SpaceConfig } from "@/app/(spaces)/Space";
 import { unstable_noStore as noStore } from "next/cache";
+import axios from "axios";
 
 export async function getSpaceTabConfig(
   spaceId: string,
@@ -9,15 +10,22 @@ export async function getSpaceTabConfig(
 ): Promise<Omit<SpaceConfig, "isEditable"> | null> {
   noStore();
   try {
-    const { data, error } = await createSupabaseServerClient()
-      .storage
+    const supabase = createSupabaseServerClient();
+    const {
+      data: { publicUrl },
+    } = await supabase.storage
       .from("spaces")
-      .download(`${spaceId}/tabs/${tabName}`);
+      .getPublicUrl(`${spaceId}/tabs/${tabName}`);
 
-    if (error || !data) {
-      console.warn(`No space config found for ${spaceId}/${tabName}:`, error);
+    if (!publicUrl) {
+      console.warn(`No space config found for ${spaceId}/${tabName}`);
       return null;
     }
+
+    const t = Math.random().toString(36).substring(2);
+    const urlWithParam = `${publicUrl}?t=${t}`;
+
+    const { data } = await axios.get<Blob>(urlWithParam, { responseType: "blob" });
 
     const fileText = await data.text();
     let parsed: unknown;
