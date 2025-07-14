e9a645a5a25091594ecbea3d94e52a8770d46b6c
Willy Ogorzaly <willy@ogorzaly.com>
2025-07-14 14:42:48 -0600

    feat: add channel space pages

---
+++ src/app/(spaces)/channel/[channelName]/ChannelSpace.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { isArray, isNil } from "lodash";
+import SpaceNotFound from "@/app/(spaces)/SpaceNotFound";
+import createInitialChannelSpaceConfigForName from "@/constants/initialChannelSpace";
+import PublicSpace from "../../PublicSpace";
+
+export interface ChannelSpaceProps {
+  spaceOwnerFid: number | null;
+  spaceId: string | null;
+  tabName: string | string[] | null | undefined;
+  channelName: string;
+}
+
+const ChannelSpace: React.FC<ChannelSpaceProps> = ({
+  spaceOwnerFid,
+  spaceId,
+  tabName,
+  channelName,
+}) => {
+  if (isNil(spaceOwnerFid)) {
+    return <SpaceNotFound />;
+  }
+
+  const INITIAL_CHANNEL_SPACE_CONFIG = createInitialChannelSpaceConfigForName(
+    channelName,
+  );
+
+  const getSpacePageUrl = (tab: string) => `/channel/${channelName}/${tab}`;
+
+  return (
+    <PublicSpace
+      spaceId={spaceId}
+      tabName={isArray(tabName) ? tabName[0] : tabName ?? "Profile"}
+      initialConfig={INITIAL_CHANNEL_SPACE_CONFIG}
+      getSpacePageUrl={getSpacePageUrl}
+      spaceOwnerFid={spaceOwnerFid}
+      pageType="profile"
+    />
+  );
+};
+
+export default ChannelSpace;
