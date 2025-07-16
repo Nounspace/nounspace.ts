aa3a3d502d84ea3f399199189fd702f22a6f98fe
Willy Ogorzaly <willy@ogorzaly.com>
2025-07-16 11:39:35 -0600

    fix signer uuid hook

--- src/common/lib/hooks/useNeynarSignerUUID.ts
+++ src/common/lib/hooks/useNeynarSignerUUID.ts
@@ -2,18 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import axiosBackend from "@/common/data/api/backend";
 
 export function useNeynarSignerUUID(fid?: number): string | undefined {
-  const { data } = useQuery(
-    ["neynar-signer-uuid", fid],
-    async () => {
+  const { data } = useQuery({
+    queryKey: ["neynar-signer-uuid", fid],
+    enabled: !!fid && fid > 0,
+    queryFn: async () => {
       if (!fid || fid <= 0) return undefined;
       const response = await axiosBackend.get<{ signer_uuid: string }>(
         "/api/farcaster/neynar/signer",
-        { params: { fid } }
+        { params: { fid } },
       );
       return response.data.signer_uuid;
     },
-    { enabled: !!fid && fid > 0 }
-  );
+  });
 
   return data;
 }
