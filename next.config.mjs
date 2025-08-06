d4785534a6e4203930490beed9914815ed896808
Willy Ogorzaly <willy@ogorzaly.com>
2025-08-06 15:04:44 -0600

    chore: allow youtube scripts

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const withBundleAnalyzer = bundlerAnalyzer({
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com https://www.youtube.com/iframe_api https://auth.privy.nounspace.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com https://www.youtube.com https://www.youtube.com/iframe_api https://auth.privy.nounspace.com;
     style-src 'self' 'unsafe-inline' https://i.ytimg.com https://mint.highlight.xyz;
     img-src 'self' blob: data: https:;
     font-src 'self' https:;
