c9967961b5c28a8988751f4dfa56bd052b000cfa
jhonattan2121 <devferri22@gmail.com>
2025-08-20 21:44:10 -0300

    fix: Remove streaming meta tags to ensure MP4 video display in Farcaster
    - Removed twitter:player:stream meta tags that were causing .m3u8 streaming interpretation
    - Simplified Farcaster Frame meta tags for direct MP4 video embedding
    - Ensured video appears as native MP4 player instead of streaming link
    - Fixed video embedding to show proper video player in Farcaster posts

--- src/pages/video/walrus/[blobId].tsx
+++ src/pages/video/walrus/[blobId].tsx
@@ -20,32 +20,32 @@ export default function WalrusVideoPage({ blobId, videoUrl, baseUrl, videoMimeTy
         <title>{pageTitle}</title>
         <meta name="description" content={description} />
 
-        {/* Open Graph meta tags for social media */}
-        <meta property="og:type" content="video.other" />
+        {/* Open Graph meta tags for social media - Updated for video */}
+        <meta property="og:type" content="video" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={description} />
         <meta property="og:url" content={pageUrl} />
-        <meta property="og:video" content={videoUrl} />
         <meta property="og:video:url" content={videoUrl} />
         <meta property="og:video:secure_url" content={videoUrl} />
-        <meta property="og:video:type" content={videoMimeType} />
+        <meta property="og:video:type" content="video/mp4" />
         <meta property="og:video:width" content="1280" />
         <meta property="og:video:height" content="720" />
-        <meta property="og:image" content={`${baseUrl}/images/nounspace_logo.png`} />
+        <meta property="og:image" content={`${baseUrl}/api/walrus/thumbnail/${blobId}`} />
 
-        {/* Twitter Card meta tags */}
+        {/* Twitter Card meta tags for video */}
         <meta name="twitter:card" content="player" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:player" content={videoUrl} />
         <meta name="twitter:player:width" content="1280" />
         <meta name="twitter:player:height" content="720" />
+        <meta name="twitter:image" content={`${baseUrl}/api/walrus/thumbnail/${blobId}`} />
 
-        {/* Farcaster Frame meta tags */}
+        {/* Farcaster Frame meta tags for direct video */}
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:video" content={videoUrl} />
-        <meta property="fc:frame:video:type" content={videoMimeType} />
-        <meta property="fc:frame:image" content={`${baseUrl}/images/nounspace_logo.png`} />
+        <meta property="fc:frame:video:type" content="video/mp4" />
+        <meta property="fc:frame:image" content={`${baseUrl}/api/walrus/thumbnail/${blobId}`} />
 
         {/* Canonical URL */}
         <link rel="canonical" href={pageUrl} />
