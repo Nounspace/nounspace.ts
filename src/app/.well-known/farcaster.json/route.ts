3452a0096d3524763422f65aa0c9a796866505bf
Jesse Paterson <jesse.r.pat@gmail.com>
2025-08-28 14:02:58 -0500

    Manifest setup

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,24 +1,39 @@
-import { WEBSITE_URL } from "../../../constants/app";
+import { APP_NAME, APP_ICON, APP_SUBTITLE, APP_DESCRIPTION, APP_SPLASH_IMAGE, SPLASH_BACKGROUND_COLOR, APP_PRIMARY_CATEGORY, APP_HERO_IMAGE, APP_TAGLINE, APP_OG_TITLE, APP_OG_DESCRIPTION, APP_OG_IMAGE } from '../../../constants/app';
 
-console.log("WEBSITE_URL", WEBSITE_URL);
+function withValidProperties(properties: Record<string, undefined | string | string[]>) {
+  return Object.fromEntries(
+    Object.entries(properties).filter(([_, value]) => (Array.isArray(value) ? value.length > 0 : !!value))
+  );
+}
 
 export async function GET() {
-  const config = {
-    // accountAssociation: {
-      // header: "",
-      // payload: "",
-      // signature: "",
-    // },
-    frame: {
-      version: "1",
-      name: "Nounspace",
-      homeUrl: WEBSITE_URL,
-      iconUrl: `${WEBSITE_URL}/images/frames/icon.png`,
-      splashImageUrl: `${WEBSITE_URL}/images/frames/splash.png`,
-      webhookUrl: `${WEBSITE_URL}/api/farcaster/webhook`,
-      splashBackgroundColor: "#FFFFFF",
+  const URL = process.env.NEXT_PUBLIC_URL as string;
+  return Response.json({
+    accountAssociation: {
+      header: process.env.FARCASTER_HEADER,
+      payload: process.env.FARCASTER_PAYLOAD,
+      signature: process.env.FARCASTER_SIGNATURE,
     },
-  };
-
-  return Response.json(config);
+    frame: withValidProperties({
+      version: '1',
+      name: APP_NAME || process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      subtitle: APP_SUBTITLE || process.env.NEXT_PUBLIC_APP_SUBTITLE,
+      description: APP_DESCRIPTION || process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+      screenshotUrls: [],
+      iconUrl: APP_ICON || process.env.NEXT_PUBLIC_APP_ICON,
+      splashImageUrl: APP_SPLASH_IMAGE || process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
+      splashBackgroundColor: SPLASH_BACKGROUND_COLOR || process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+      homeUrl: URL,
+      webhookUrl: `${URL}/api/webhook`,
+      primaryCategory: APP_PRIMARY_CATEGORY || process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
+      tags: [],
+      heroImageUrl: APP_HERO_IMAGE || process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+      tagline: APP_TAGLINE || process.env.NEXT_PUBLIC_APP_TAGLINE,
+      ogTitle: APP_OG_TITLE || process.env.NEXT_PUBLIC_APP_OG_TITLE,
+      ogDescription: APP_OG_DESCRIPTION || process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
+      ogImageUrl: APP_OG_IMAGE || process.env.NEXT_PUBLIC_APP_OG_IMAGE,
+      // use only while testing
+      // noindex: 'true',
+    }),
+  });
 }
--- src/constants/app.ts
+++ src/constants/app.ts
@@ -8,3 +8,16 @@ export const WEBSITE_URL =
 export const APP_FID = process.env.NEXT_PUBLIC_APP_FID
   ? Number(process.env.NEXT_PUBLIC_APP_FID)
   : undefined;
+
+export const APP_NAME = 'Nounspace'
+export const APP_ICON = 'https://www.nounspace.com/images/frames/icon.png'
+export const APP_SUBTITLE = 'The Nounish Homepage'
+export const APP_DESCRIPTION = 'A social hub for all things Nouns'
+export const APP_SPLASH_IMAGE = 'https://www.nounspace.com/images/frames/splash.png'
+export const SPLASH_BACKGROUND_COLOR = '#FFFFFF'
+export const APP_PRIMARY_CATEGORY = 'social'
+export const APP_HERO_IMAGE = 'https://your-app.vercel.app/og.png'
+export const APP_TAGLINE = 'The Nounish Homepage'
+export const APP_OG_TITLE = 'Nounspace'
+export const APP_OG_DESCRIPTION = 'A social hub for all things Nouns'
+export const APP_OG_IMAGE = 'https://www.nounspace.com/images/icon-192x192.png'
