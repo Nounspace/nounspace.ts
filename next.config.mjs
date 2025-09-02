cfeb47fc14bcbe822c3438984c0f36fd66f87490
Willy Ogorzaly <willy@ogorzaly.com>
2025-09-02 12:19:54 -0500

    Disable React strict mode to prevent nav remount flicker

--- next.config.mjs
+++ next.config.mjs
@@ -46,6 +46,7 @@ const cspHeader = `
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   // output: 'export', // Outputs a Single-Page Application (SPA).
   // distDir: './dist', // Changes the build output directory to `./dist/`.
   transpilePackages: [
