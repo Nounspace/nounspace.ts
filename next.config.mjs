3014a38d704389b11213f74dd2265c91bcdc0a46
Willy Ogorzaly <willy@ogorzaly.com>
2025-09-02 13:32:24 -0500

    re-enable reactStrictMode

--- next.config.mjs
+++ next.config.mjs
@@ -46,7 +46,6 @@ const cspHeader = `
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   // output: 'export', // Outputs a Single-Page Application (SPA).
   // distDir: './dist', // Changes the build output directory to `./dist/`.
   transpilePackages: [
