5c66b4d1da864fb2f0d54a0d57536fd860fc2e43
Jesse Paterson <jesse.r.pat@gmail.com>
2025-10-22 18:10:01 -0500

    cut out fidget specific data and trimmed config

--- src/constants/spaceToken.ts
+++ src/constants/spaceToken.ts
@@ -1,1 +1,6 @@
-export const SPACE_CONTRACT_ADDR = "0x48c6740bcf807d6c47c864faeea15ed4da3910ab";
+import { loadSystemConfig } from "@/config";
+
+// Load system configuration
+const config = loadSystemConfig();
+
+export const SPACE_CONTRACT_ADDR = config.community.contracts.space;
