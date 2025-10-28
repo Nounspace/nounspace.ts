b857728cfa8cef8a76fbfe75773dc04a4d9a5401
Willy Ogorzaly <willy@ogorzaly.com>
2025-10-28 12:47:04 -0500

    Close mobile nav after search selection

--- src/common/components/organisms/SearchModal.tsx
+++ src/common/components/organisms/SearchModal.tsx
@@ -8,42 +8,61 @@ import { Dialog, DialogContent } from "@/common/components/atoms/dialog";
 
 import SearchAutocompleteInput from "@/common/components/organisms/SearchAutocompleteInput";
 
-const SearchModal = React.forwardRef((props, ref) => {
-  const [isFocused, setIsFocused] = useState(false);
+export type SearchModalHandle = {
+  focus: () => void;
+};
 
-  const handleFocus = useCallback(() => {
-    setIsFocused(true);
-  }, []);
+type SearchModalProps = {
+  onResultSelect?: () => void;
+};
 
-  const handleBlur = useCallback(() => {
-    setIsFocused(false);
-  }, []);
+const SearchModal = React.forwardRef<SearchModalHandle, SearchModalProps>(
+  ({ onResultSelect }, ref) => {
+    const [isFocused, setIsFocused] = useState(false);
 
-  useImperativeHandle(ref, () => ({
-    focus: handleFocus,
-  }));
+    const handleFocus = useCallback(() => {
+      setIsFocused(true);
+    }, []);
 
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        setIsFocused((open) => !open);
-      }
-      if (e.key === "Escape") {
-        setIsFocused(false);
-      }
-    };
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-  }, []);
+    const handleBlur = useCallback(() => {
+      setIsFocused(false);
+    }, []);
 
-  return (
-    <Dialog open={isFocused} onOpenChange={setIsFocused}>
-      <DialogContent className="p-0 border-none">
-        <SearchAutocompleteInput onSelect={handleBlur} />
-      </DialogContent>
-    </Dialog>
-  );
-});
+    const handleSelect = useCallback(() => {
+      handleBlur();
+      onResultSelect?.();
+    }, [handleBlur, onResultSelect]);
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        focus: handleFocus,
+      }),
+      [handleFocus],
+    );
+
+    useEffect(() => {
+      const down = (e: KeyboardEvent) => {
+        if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+          setIsFocused((open) => !open);
+        }
+        if (e.key === "Escape") {
+          setIsFocused(false);
+        }
+      };
+      document.addEventListener("keydown", down);
+      return () => document.removeEventListener("keydown", down);
+    }, []);
+
+    return (
+      <Dialog open={isFocused} onOpenChange={setIsFocused}>
+        <DialogContent className="p-0 border-none">
+          <SearchAutocompleteInput onSelect={handleSelect} />
+        </DialogContent>
+      </Dialog>
+    );
+  },
+);
 
 SearchModal.displayName = "SearchModal";
 
