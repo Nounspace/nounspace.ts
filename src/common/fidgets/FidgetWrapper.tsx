ac478785f3bf0bdb10165fac9d91c5bd1076ea33
Jhonattan Ferri <81554200+Jhonattan2121@users.noreply.github.com>
2025-10-29 02:37:22 -0300

    Update FidgetWrapper.tsx

--- src/common/fidgets/FidgetWrapper.tsx
+++ src/common/fidgets/FidgetWrapper.tsx
@@ -1,247 +1,121 @@
-"use client";
+import { FidgetArgs, FidgetInstanceData, FidgetModule } from "@/common/fidgets";
+import { CompleteFidgets } from "@/fidgets";
+import { isUndefined } from "lodash";
+import React, { Dispatch, SetStateAction, useCallback, useMemo } from "react";
+import { FixedSizeList } from "react-window";
+import { Card, CardContent } from "../atoms/card";
+import BackArrowIcon from "../atoms/icons/BackArrow";
 
-import { Card, CardContent } from "@/common/components/atoms/card";
-import CSSInput from "@/common/components/molecules/CSSInput";
-import ScopedStyles from "@/common/components/molecules/ScopedStyles";
-import { useAppStore } from "@/common/data/stores/app";
-import { reduce } from "lodash";
-import React from "react";
-import { FaX } from "react-icons/fa6";
-import { toast } from "sonner";
-import {
-  FidgetArgs,
-  FidgetBundle,
-  FidgetConfig,
-  FidgetData,
-  FidgetProperties,
-  FidgetRenderContext,
-  FidgetSettings,
-} from ".";
-import GrabHandleIcon from "../components/atoms/icons/GrabHandle";
-import StashIcon from "../components/atoms/icons/Stash";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../components/atoms/tooltip";
-import FidgetSettingsEditor from "../components/organisms/FidgetSettingsEditor";
-
-export type FidgetWrapperProps = {
-  fidget: React.FC<FidgetArgs>;
-  bundle: FidgetBundle;
-  context?: FidgetRenderContext;
-  saveConfig: (conf: FidgetConfig) => Promise<void>;
-  setCurrentFidgetSettings: (currentFidgetSettings: React.ReactNode) => void;
-  setSelectedFidgetID: (selectedFidgetID: string) => void;
-  selectedFidgetID: string;
-  removeFidget: (fidgetId: string) => void;
-  minimizeFidget: (fidgetId: string) => void;
-  borderRadius?: string;
-};
-
-export const getSettingsWithDefaults = (
-  settings: FidgetSettings,
-  config: FidgetProperties,
-): FidgetSettings => {
-  return reduce(
-    config.fields,
-    (acc, f) => ({
-      ...acc,
-      [f.fieldName]:
-        settings && typeof settings === 'object' && f.fieldName in settings
-          ? settings[f.fieldName]
-          : f.default || undefined,
-    }),
-    {},
-  );
-};
-
-/// Custom memoization to avoid unnecessary re-renders
- function areEqual(prevProps: FidgetWrapperProps, nextProps: FidgetWrapperProps) {
-  if (prevProps.bundle.id !== nextProps.bundle.id) return false;
-  if (prevProps.selectedFidgetID !== nextProps.selectedFidgetID) return false;
-  if (prevProps.borderRadius !== nextProps.borderRadius) return false;
-  const a = prevProps.bundle.config;
-  const b = nextProps.bundle.config;
-  if (a.editable !== b.editable) return false;
-  if (a.settings !== b.settings) return false;
-  if (a.data !== b.data) return false;
-  if (prevProps.bundle.properties !== nextProps.bundle.properties) return false;
-  if (prevProps.fidget !== nextProps.fidget) return false;
-  if (prevProps.context?.theme !== nextProps.context?.theme) return false;
-  return true;
+export interface FidgetPickerProps {
+  addFidget: (fidgetId: string, fidget: FidgetModule<any>) => void;
+  setExternalDraggedItem: Dispatch<
+    SetStateAction<{ i: string; w: number; h: number } | undefined>
+  >;
+  setCurrentlyDragging: React.Dispatch<React.SetStateAction<boolean>>;
+  generateFidgetInstance(
+    fidgetId: string,
+    fidget: FidgetModule<FidgetArgs>,
+  ): FidgetInstanceData;
+  setIsPickingFidget: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const FidgetWrapper = React.memo(function FidgetWrapper({
-  fidget,
-  bundle,
-  saveConfig,
-  setCurrentFidgetSettings,
-  setSelectedFidgetID,
-  selectedFidgetID,
-  removeFidget,
-  minimizeFidget,
-  borderRadius,
-}: FidgetWrapperProps) {
-  const { homebaseConfig } = useAppStore((state) => ({
-    homebaseConfig: state.homebase.homebaseConfig,
-  }));
+const ITEM_HEIGHT = 56;
 
-  function onClickEdit() {
-    setSelectedFidgetID(bundle.id);
-    setCurrentFidgetSettings(
-      <FidgetSettingsEditor
-        fidgetId={bundle.id}
-        properties={bundle.properties}
-        settings={settingsWithDefaults}
-        onSave={onSave}
-        unselect={unselect}
-        removeFidget={removeFidget}
-      />,
-    );
-  }
-
-  const Fidget = fidget;
-
-  const saveData = (data: FidgetData) => {
-    return saveConfig({
-      ...bundle.config,
-      data,
-    });
-  };
-
-  const settingsWithDefaults = getSettingsWithDefaults(
-    bundle.config.settings,
-    bundle.properties,
+export const FidgetPicker: React.FC<FidgetPickerProps> = ({
+  addFidget,
+  setExternalDraggedItem,
+  setCurrentlyDragging,
+  generateFidgetInstance,
+  setIsPickingFidget,
+}) => {
+  const fidgetEntries = useMemo(
+    () => Object.entries(CompleteFidgets).filter(([, fidgetModule]) => !isUndefined(fidgetModule)) as
+     [string, FidgetModule<FidgetArgs>][],
+    [CompleteFidgets]
   );
 
-  const onSave = async (
-    newSettings: FidgetSettings,
-    shouldUnselect?: boolean,
-  ) => {
-    try {
-      await saveConfig({
-        ...bundle.config,
-        settings: newSettings,
+  const handleDragStart = useCallback(
+    (e: React.DragEvent, fidgetId: string, fidgetModule: FidgetModule<FidgetArgs>) => {
+      setCurrentlyDragging(true);
+      e.dataTransfer.setData(
+        "text/plain",
+        JSON.stringify(generateFidgetInstance(fidgetId, fidgetModule)),
+      );
+      setExternalDraggedItem({
+        i: fidgetId,
+        w: fidgetModule.properties.size.minWidth,
+        h: fidgetModule.properties.size.minHeight,
       });
-    } catch (e) {
-      toast.error("Failed to save fidget settings", { duration: 1000 });
-    }
+    },
+    [setCurrentlyDragging, setExternalDraggedItem, generateFidgetInstance]
+  );
 
-    if (shouldUnselect) {
-      unselect();
-    }
-  };
+  const handleClick = useCallback(
+    (fidgetId: string, fidgetModule: FidgetModule<FidgetArgs>) => {
+      addFidget(fidgetId, fidgetModule);
+    },
+    [addFidget]
+  );
 
-  function unselect() {
-    setSelectedFidgetID("");
-    setCurrentFidgetSettings(<></>);
-  }
-
-  const userStyles = bundle.properties.fields
-    .filter((f) => f.inputSelector === CSSInput)
-    .map((f) => settingsWithDefaults[f.fieldName]);
-
-  return (
-    <>
+  const RowComponent: React.FC<{ index: number; style: React.CSSProperties }> = ({ index, style }) => {
+    const [fidgetId, fidgetModule] = fidgetEntries[index];
+    return (
       <div
-        className={
-          selectedFidgetID === bundle.id
-            ? "absolute -mt-7 opacity-80 transition-opacity ease-in flex flex-row h-6 z-50"
-            : "absolute -mt-7 opacity-0 transition-opacity ease-in flex flex-row h-6 z-50"
-        }
-        data-fidget-controls
+        key={fidgetModule.properties.fidgetName}
+        className="z-20 droppable-element flex justify-center items-center transition-transform duration-300"
+        draggable={true}
+        style={style}
+        onDragStart={(e) => handleDragStart(e, fidgetId, fidgetModule)}
       >
-        <Card className="h-full grabbable rounded-lg w-6 flex items-center justify-center bg-[#F3F4F6] hover:bg-sky-100 text-[#1C64F2]">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div className="flex items-center gap-1">
-                  <GrabHandleIcon />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>Drag to Move</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </Card>
         <button
-          onClick={() => {
-            minimizeFidget(bundle.id);
-          }}
+          key={fidgetId}
+          className="group w-full h-10 flex items-center gap-2 p-1 bg-transparent transform-gpu transition-transform will-change-transform hover:scale-[1.04]"
+          onClick={() => handleClick(fidgetId, fidgetModule)}
         >
-          <Card className="h-full rounded-lg ml-1 w-6 flex items-center justify-center bg-[#F3F4F6] hover:bg-sky-100 text-[#1C64F2]">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <div className="flex items-center gap-1">
-                    <StashIcon />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent>Stash in Fidget Tray</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </Card>
-        </button>
-        <button
-          onClick={() => {
-            removeFidget(bundle.id);
-          }}
-        >
-          <Card className="h-full rounded-lg ml-1 w-6 flex items-center justify-center bg-[#F3F4F6] hover:bg-red-100 text-[#1C64F2]">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <FaX className="w-5/12" />
-                </TooltipTrigger>
-                <TooltipContent>Remove Fidget</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+          <Card className="w-full h-full bg-[#F3F4F6] flex items-center p-1 rounded-lg">
+            <CardContent className="overflow-hidden flex items-center gap-2 p-0 pl-2">
+              <div className="flex items-center justify-center w-5 h-5">
+                <span
+                  className="text-sm leading-none text-black group-hover:text-black"
+                  role="img"
+                  aria-label={fidgetModule.properties.fidgetName}
+                >
+                  {String.fromCodePoint(fidgetModule.properties.icon)}
+                </span>
+              </div>
+              <span className="text-xs text-black text-left leading-none group-hover:text-black">
+                {fidgetModule.properties.fidgetName}
+              </span>
+            </CardContent>
           </Card>
         </button>
       </div>
-      <Card
-        className={
-          selectedFidgetID === bundle.id
-            ? "size-full border-solid border-sky-600 border-4 overflow-hidden"
-            : "size-full overflow-hidden"
-        }
-        style={{
-          background: settingsWithDefaults.useDefaultColors
-            ? homebaseConfig?.theme?.properties.fidgetBackground
-            : settingsWithDefaults.background,
-          borderColor: settingsWithDefaults.useDefaultColors
-            ? homebaseConfig?.theme?.properties.fidgetBorderColor
-            : settingsWithDefaults.fidgetBorderColor,
-          borderWidth: settingsWithDefaults.useDefaultColors
-            ? homebaseConfig?.theme?.properties.fidgetBorderWidth
-            : settingsWithDefaults.fidgetBorderWidth,
-          boxShadow: settingsWithDefaults.useDefaultColors
-            ? homebaseConfig?.theme?.properties.fidgetShadow
-            : settingsWithDefaults.fidgetShadow,
-          borderRadius: borderRadius ?? homebaseConfig?.theme?.properties.fidgetBorderRadius ?? "12px",
-        }}
-      >
-        {bundle.config.editable && (
-          <button
-            onMouseDown={onClickEdit}
-            className="items-center justify-center opacity-0 hover:opacity-50 duration-500 absolute inset-0 z-30 flex bg-slate-400 bg-opacity-50 rounded-md"
-          ></button>
-        )}
-        <ScopedStyles cssStyles={userStyles} className="size-full">
-          <CardContent className="size-full p-0">
-            <Fidget
-              {...{
-                settings: settingsWithDefaults,
-                data: bundle.config.data,
-                saveData,
-              }}
-            />
-          </CardContent>
-        </ScopedStyles>
-      </Card>
-    </>
-  );
-}, areEqual);
+    );
+  };
 
-export { FidgetWrapper };
+  const Row = React.memo(RowComponent);
+  Row.displayName = "Row";
+
+  return (
+    <div className="h-full overflow-y-auto">
+      <div className="flex pb-4 m-2 items-center">
+        <button onClick={() => setIsPickingFidget(false)} className="my-auto">
+          <BackArrowIcon />
+        </button>
+        <h1 className="capitalize text-lg pl-4">Add Fidget</h1>
+      </div>
+      <section id="Fidgets" className="mx-auto grid grid-cols-1 gap-1">
+        <FixedSizeList
+          height={400}
+          itemCount={fidgetEntries.length}
+          itemSize={ITEM_HEIGHT}
+          width={"100%"}
+        >
+          {Row}
+        </FixedSizeList>
+      </section>
+    </div>
+  );
+};
+
+export default FidgetPicker;
