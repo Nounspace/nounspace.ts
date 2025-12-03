9e2555451a4f5979d13a537e2585606df5c452dc
willyogo <willy@ogorzaly.com>
2025-12-03 15:03:40 -0600

    Update Yarn install state and PWA page

--- src/app/pwa/page.tsx
+++ src/app/pwa/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import { subscribeUser, unsubscribeUser, sendNotification } from './actions'
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
 
@@ -13,7 +13,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i)
   }
-  return outputArray
+  return outputArray.buffer
 }
 
 function PushNotificationManager() {
