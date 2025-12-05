2077f1107681a053e2a3fea61455bb2f6b39fd6f
sktbrd <sktbrd.eth@gmail.com>
2025-12-05 15:01:00 -0300

    zora fix2

--- src/fidgets/zora/ZoraCoins.tsx
+++ src/fidgets/zora/ZoraCoins.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import TextInput from "@/common/components/molecules/TextInput";
-import SwitchButton from "@/common/components/molecules/SwitchButton";
 import {
   FidgetArgs,
   FidgetModule,
